20f84b87133a0a26679b6f3ab2808aa09ac815a7
Arthur <olehno.arturs@gmail.com>
2025-05-19 15:43:41 +0300

    Completed 4th & 5th activity Completed Task1

--- src/test/java/selenium/sample/Sample4Task.java
+++ src/test/java/selenium/sample/Sample4Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample4Task {
     WebDriver driver;
@@ -29,6 +33,32 @@ public class Sample4Task {
     @Test
     public void enterNumber() throws Exception {
 //         TODO:
+
+        WebElement textField = driver.findElement(By.id("number"));
+        WebElement clearResultButton = driver.findElement(By.id("clear_result_button_number"));
+        WebElement resultNumber = driver.findElement(By.id("result_number"));
+        WebElement resultButton = driver.findElement(By.id("result_button_number"));
+
+        String textFieldNumber = "5";
+        String expectedResult = "You entered number: \"5\"";
+
+        textField.clear();
+
+        textField.sendKeys(textFieldNumber);
+
+        assertFalse(clearResultButton.isEnabled());
+        assertFalse(resultNumber.isDisplayed());
+
+        resultButton.click();
+
+        assertTrue(resultNumber.isDisplayed());
+        assertEquals(expectedResult, resultNumber.getText());
+        assertTrue(clearResultButton.isEnabled());
+
+        clearResultButton.click();
+
+        assertEquals("", resultNumber.getText());
+        assertFalse(resultNumber.isDisplayed());
 //        enter a number under "Number"
 //        check that button is not clickable "Clear Result"
 //        check that text is not displayed
@@ -42,6 +72,12 @@ public class Sample4Task {
 
     @Test
     public void clickOnLink() throws Exception {
+
+        assertEquals("https://acctabootcamp.github.io/site/examples/actions", driver.getCurrentUrl());
+        driver.findElement(By.id("homepage_link")).click();
+        assertFalse(driver.getCurrentUrl().equals(base_url));
+        assertEquals("https://acctabootcamp.github.io/site/", driver.getCurrentUrl());
+
 //         TODO:
 //        check current url is base_url
 //        click on "This is a link to Homepage"
--- src/test/java/selenium/tasks/Task1.java
+++ src/test/java/selenium/tasks/Task1.java
@@ -3,10 +3,14 @@ package selenium.tasks;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
+import java.text.DecimalFormat;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Task1 {
     WebDriver driver;
@@ -27,12 +31,30 @@ public class Task1 {
 
     @Test
     public void errorOnText() {
+
+        String text = "test";
+        String expectedMessage = "Please enter a number";
+
+        driver.findElement(By.id("numb")).sendKeys(text);
+        driver.findElement(By.className("w3-orange")).click();
+
+        assertEquals(expectedMessage, driver.findElement(By.id("ch1_error")).getText());
+
 //        TODO
 //        enter a text instead of a number, check that correct error is seen
     }
 
     @Test
     public void errorOnNumberTooSmall() {
+
+        String text = "17";
+        String expectedMessage = "Number is too small";
+
+        driver.findElement(By.id("numb")).sendKeys(text);
+        driver.findElement(By.className("w3-orange")).click();
+
+        assertEquals(expectedMessage, driver.findElement(By.id("ch1_error")).getText());
+
 //        BUG: if I enter number 49 or 42 no errors where seen
 //        TODO
 //        enter number which is too small (positive number below 50), check that correct error is seen
@@ -41,6 +63,14 @@ public class Task1 {
     @Test
     public void errorOnNumberTooBig() {
 
+        String text = "117";
+        String expectedMessage = "Number is too big";
+
+        driver.findElement(By.id("numb")).sendKeys(text);
+        driver.findElement(By.className("w3-orange")).click();
+
+        assertEquals(expectedMessage, driver.findElement(By.id("ch1_error")).getText());
+
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
@@ -48,6 +78,18 @@ public class Task1 {
 
     @Test
     public void correctSquareRoot() {
+
+        String text = "56.25";
+        double calcResult = Math.sqrt(Double.parseDouble(text));
+        DecimalFormat df = new DecimalFormat("#.00");
+        String formattedNumber = df.format(calcResult);
+        String fullMessageText = "Square root of " + text + " is " + formattedNumber;
+
+        driver.findElement(By.id("numb")).sendKeys(text);
+        driver.findElement(By.className("w3-orange")).click();
+
+        assertEquals(fullMessageText, driver.switchTo().alert().getText());
+
 //        TODO
 //        enter a number between 50 and 100 digit in the input, then press submit
 //        and check that no error is seen and that square root is calculated correctly
