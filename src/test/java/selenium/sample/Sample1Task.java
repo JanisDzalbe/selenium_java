9c722dd8b109b7cc5dc61e0092b4f01ec35509e5
penktastal <arnas.matuolis@gmail.com>
2025-05-19 09:47:45 +0300

    Initial commit / First activity

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -1,12 +1,26 @@
 package selenium.sample;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Sample1Task {
 
     @Test
     public void goToHomepage() throws Exception {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://acctabootcamp.github.io/site/index2.html");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+        // messing around w/ assertions, they probably shouldn't be here
+        Assertions.assertEquals("Home Page for this site version 2", driver.getTitle());
+        Assertions.assertEquals("https://acctabootcamp.github.io/site/index2.html", driver.getCurrentUrl());
+
+        driver.quit();
 //        TODO:
 //         define driver
 //         go to https://acctabootcamp.github.io/site/index2.html
