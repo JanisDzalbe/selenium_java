6efe8416f34a33a7b5d8cb3c39791b161d3732eb
Daniils Grigorjevs <daniilsgrigorjevs1@gmails.com>
2025-05-19 09:48:47 +0300

    Sample1Task is done

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -2,8 +2,16 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+
+
 
 public class Sample1Task {
+    static String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
 
     @Test
     public void goToHomepage() throws Exception {
@@ -13,5 +21,16 @@ public class Sample1Task {
 //         get title of page
 //         get URL of current page
 //         close browser
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://acctabootcamp.github.io/site/");
+        System.out.println(driver.findElement(By.id("h1")).getText());
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        Thread.sleep(5000);
+        driver.quit();
+
     }
 }
