56557ad5cedde53016dc5afd8ff20e89a851ae8a
Damian <damian.karalkevic@gmail.com>
2025-05-19 09:59:36 +0300

    Completed Sample1Task

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -2,6 +2,9 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import selenium.utility.BootcampUtils;
 
 public class Sample1Task {
 
@@ -13,5 +16,10 @@ public class Sample1Task {
 //         get title of page
 //         get URL of current page
 //         close browser
+        WebDriver driver = BootcampUtils.initializeChromeDriver();
+        driver.get("https://acctabootcamp.github.io/site/index2.html");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
     }
 }
