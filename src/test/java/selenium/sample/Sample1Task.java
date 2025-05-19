76887dcad53712cf5bd9221198ec755be5a35b2a
Arthur <olehno.arturs@gmail.com>
2025-05-19 11:42:08 +0300

    Completed first & second activity

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -2,11 +2,25 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import selenium.utility.BootcampUtils;
 
 public class Sample1Task {
 
     @Test
     public void goToHomepage() throws Exception {
+
+        WebDriver driver = BootcampUtils.initializeChromeDriver();
+        driver.get("https://acctabootcamp.github.io/site/index2.html");
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+        Thread.sleep(5000);
+
+        driver.quit();
+
 //        TODO:
 //         define driver
 //         go to https://acctabootcamp.github.io/site/index2.html
--- src/test/java/selenium/sample/Sample2Task.java
+++ src/test/java/selenium/sample/Sample2Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
 
 public class Sample2Task {
     WebDriver driver;
@@ -27,24 +31,46 @@ public class Sample2Task {
 
     @Test
     public void findElementByID() throws Exception {
+        System.out.println(driver.findElement(By.id("heading_2")).getText());
 //         TODO:
 //         get text "Heading 2 text" using id
     }
 
     @Test
     public void findElementByName() throws Exception {
+
+        WebElement button = driver.findElement(By.name("randomButton2"));
+        System.out.println(button.getAttribute("id"));
+        System.out.println(button.getAttribute("value"));
+
+//        System.out.println(driver.findElement(By.name("randomButton2")).getAttribute("value"));
+//        System.out.println(driver.findElement(By.name("randomButton2")).getAttribute("id"));
+
 //         TODO:
 //         get attribute "id" and "value" of button "This is also a button" using name
     }
 
     @Test
     public void findElementByClassFirst() throws Exception {
+
+        System.out.println(driver.findElements(By.className("test")).get(0).getText());
+
 //         TODO:
 //         get first text of class "test" (should be "Test Text 1")
     }
 
     @Test
     public void findElementByClassAll() throws Exception {
+
+        List<WebElement> testElements = driver.findElements(By.className("test"));
+        System.out.println(driver.findElements(By.className("test")).size());
+
+        for (WebElement testElement: testElements) {
+            System.out.println(testElement.getText());
+        }
+
+        System.out.println(driver.findElements(By.className("test")).get(2).getText());
+
 //         TODO:
 //         get size text of class "test" (should be 5)
 //         get text of class "test"
