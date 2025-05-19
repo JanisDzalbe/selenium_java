604fa76f08904da03713498655ac833a83a697bf
Germans Veidemans <germans.veidemans@edu.rtu.lv>
2025-05-19 20:42:45 +0300

    Finished Task1 commit

--- src/test/java/selenium/tasks/Task1.java
+++ src/test/java/selenium/tasks/Task1.java
@@ -3,10 +3,16 @@ package selenium.tasks;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Task1 {
     WebDriver driver;
@@ -29,6 +35,17 @@ public class Task1 {
     public void errorOnText() {
 //        TODO
 //        enter a text instead of a number, check that correct error is seen
+
+        WebElement input = driver.findElement(By.id("numb"));
+        WebElement submit = driver.findElement(By.className("w3-btn"));
+        WebElement error = driver.findElement(By.id("ch1_error"));
+
+        input.clear();
+        input.sendKeys("heeeeellooooo");
+        submit.click();
+
+        assertTrue(error.isDisplayed());
+        assertEquals("Please enter a number", error.getText());
     }
 
     @Test
@@ -36,6 +53,17 @@ public class Task1 {
 //        BUG: if I enter number 49 or 42 no errors where seen
 //        TODO
 //        enter number which is too small (positive number below 50), check that correct error is seen
+
+        WebElement input = driver.findElement(By.id("numb"));
+        WebElement submit = driver.findElement(By.className("w3-btn"));
+
+        input.clear();
+        input.sendKeys("42");
+        submit.click();
+
+        WebElement error = driver.findElement(By.id("ch1_error"));
+        assertTrue(error.isDisplayed());
+        assertEquals("Number must be between 50 and 100", error.getText().trim());
     }
 
     @Test
@@ -44,6 +72,17 @@ public class Task1 {
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
+
+        WebElement input = driver.findElement(By.id("numb"));
+        WebElement submit = driver.findElement(By.className("w3-btn"));
+
+        input.clear();
+        input.sendKeys("666");
+        submit.click();
+
+        WebElement error = driver.findElement(By.id("ch1_error"));
+        assertTrue(error.isDisplayed());
+        assertEquals("Number must be between 50 and 100", error.getText().trim());
     }
 
     @Test
@@ -52,5 +91,23 @@ public class Task1 {
 //        enter a number between 50 and 100 digit in the input, then press submit
 //        and check that no error is seen and that square root is calculated correctly
 //        NOTE: input value is hardcoded, but square root used in assertions should be calculated in code
+
+        int inputValue = 81;
+        WebElement input = driver.findElement(By.id("numb"));
+        WebElement submit = driver.findElement(By.className("w3-btn"));
+
+        input.clear();
+        input.sendKeys(String.valueOf(inputValue));
+        submit.click();
+
+
+        Alert alert = driver.switchTo().alert();
+
+        double sqrtValue = Math.sqrt(inputValue);
+
+        String expectedAlertText = String.format("Square root of %d is %.2f", inputValue, sqrtValue);
+        assertEquals(expectedAlertText, alert.getText());
+        alert.accept();
+
     }
 }
