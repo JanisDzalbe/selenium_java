eaf8df48e086bd14a95a299708e1b19ee51237f6
EgleOrintaite <egleorintaite@gmail.com>
2025-05-19 14:03:33 +0300

    Sample4Task done

--- src/test/java/selenium/sample/Sample4Task.java
+++ src/test/java/selenium/sample/Sample4Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample4Task {
     WebDriver driver;
@@ -28,7 +32,7 @@ public class Sample4Task {
 
     @Test
     public void enterNumber() throws Exception {
-//         TODO:
+//       TODO:
 //        enter a number under "Number"
 //        check that button is not clickable "Clear Result"
 //        check that text is not displayed
@@ -38,14 +42,40 @@ public class Sample4Task {
 //        check that the button "Clear Result" is clickable now
 //        click on "Clear Result"
 //        check that the text is now (""), but it is not displayed
+        WebElement numberForm = driver.findElement(org.openqa.selenium.By.id("number"));
+        WebElement resultButton = driver.findElement(org.openqa.selenium.By.id("result_button_number"));
+        WebElement clearButton = driver.findElement(org.openqa.selenium.By.id("clear_result_button_number"));
+        WebElement resultText = driver.findElement(org.openqa.selenium.By.id("result_number"));
+
+
+        numberForm.clear();
+        numberForm.sendKeys("123");
+
+        assertFalse(clearButton.isEnabled(), "Clear button should not be enabled");
+
+        assertFalse(resultText.isDisplayed(), "Result button should not be displayed");
+        resultButton.click();
+
+        assertTrue(resultText.isDisplayed());
+        assertEquals("You entered number: \"123\"", resultText.getText(), "Entered number should be displayed" );
+
+        assertTrue(clearButton.isEnabled(), "Clear button should be enabled");
+        clearButton.click();
+
+        assertEquals("",resultText.getText(), "Text should be empty now");
+        assertFalse(resultText.isDisplayed());
     }
 
     @Test
     public void clickOnLink() throws Exception {
-//         TODO:
+//       TODO:
 //        check current url is base_url
 //        click on "This is a link to Homepage"
 //        check that current url is not base_url
 //        verify that current url is homepage
+        assertEquals(base_url, driver.getCurrentUrl());
+        driver.findElement(By.linkText("This is a link to Homepage")).click();
+        assertNotEquals(base_url, driver.getCurrentUrl());
+        assertEquals("https://acctabootcamp.github.io/site/", driver.getCurrentUrl());
     }
 }
