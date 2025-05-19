f4fac2ef492dc1d2ac34a530018c8791396cd907
Santana S <santana.voinikevica@gmail.com>
2025-05-19 16:53:49 +0300

    Tasks done: Individual task: 1

--- src/test/java/selenium/tasks/Task1.java
+++ src/test/java/selenium/tasks/Task1.java
@@ -3,19 +3,26 @@ package selenium.tasks;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Task1 {
     WebDriver driver;
 
     @BeforeEach
     public void openPage() {
-
-        String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
-        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
         driver = new ChromeDriver();
         driver.get("https://acctabootcamp.github.io/site/tasks/enter_a_number");
     }
@@ -27,8 +34,19 @@ public class Task1 {
 
     @Test
     public void errorOnText() {
+
 //        TODO
 //        enter a text instead of a number, check that correct error is seen
+        WebElement fieldNumb = driver.findElement(By.id("numb"));
+        WebElement submitButton = driver.findElement(By.className("w3-orange"));
+        WebElement errorMes = driver.findElement(By.id("ch1_error"));
+
+        fieldNumb.clear();
+        fieldNumb.sendKeys("seven");
+        submitButton.click();
+        assertTrue(errorMes.isDisplayed());
+        assertEquals("Please enter a number",errorMes.getText());
+        System.out.println("Error message: " + errorMes.getText());
     }
 
     @Test
@@ -36,6 +54,16 @@ public class Task1 {
 //        BUG: if I enter number 49 or 42 no errors where seen
 //        TODO
 //        enter number which is too small (positive number below 50), check that correct error is seen
+        WebElement fieldNumb = driver.findElement(By.id("numb"));
+        WebElement submitButton = driver.findElement(By.className("w3-orange"));
+        WebElement errorMes = driver.findElement(By.id("ch1_error"));
+
+        fieldNumb.clear();
+        fieldNumb.sendKeys("45");
+        submitButton.click();
+        assertTrue(errorMes.isDisplayed());
+        assertEquals("Number is too small",errorMes.getText());
+        System.out.println("Error message: " + errorMes.getText());
     }
 
     @Test
@@ -44,13 +72,42 @@ public class Task1 {
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
+        WebElement fieldNumb = driver.findElement(By.id("numb"));
+        WebElement submitButton = driver.findElement(By.className("w3-orange"));
+        WebElement errorMes = driver.findElement(By.id("ch1_error"));
+
+        fieldNumb.clear();
+        fieldNumb.sendKeys("337");
+        submitButton.click();
+        assertTrue(errorMes.isDisplayed());
+        assertEquals("Number is too big",errorMes.getText());
+        System.out.println("Error message: " + errorMes.getText());
     }
 
     @Test
     public void correctSquareRoot() {
 //        TODO
-//        enter a number between 50 and 100 digit in the input, then press submit
+//        +enter a number between 50 and 100 digit in the input, then press submit
 //        and check that no error is seen and that square root is calculated correctly
 //        NOTE: input value is hardcoded, but square root used in assertions should be calculated in code
+        WebElement fieldNumb = driver.findElement(By.id("numb"));
+        WebElement submitButton = driver.findElement(By.className("w3-orange"));
+        WebElement errorMes = driver.findElement(By.id("ch1_error"));
+
+
+        int inputNumber = 81;
+        double numberSq = Math.sqrt(inputNumber);
+        String formatted = String.format("%.2f", numberSq);
+        String allertExp = "Square root of " + inputNumber + " is " + formatted;
+
+        fieldNumb.clear();
+        fieldNumb.sendKeys(String.valueOf(inputNumber));
+        submitButton.click();
+//        System.out.println(errorMes.getText());
+        Alert alert = driver.switchTo().alert();
+        String allertText = alert.getText();
+
+        assertEquals(allertExp, allertText);
+        System.out.println("It works");
     }
 }
