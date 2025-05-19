dcb5f477afbb7b9226dabf0971cf4712b5e7b914
penktastal <arnas.matuolis@gmail.com>
2025-05-19 11:05:55 +0300

    Complete Second task

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -5,13 +5,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import selenium.utility.BootcampUtils;
 
 public class Sample1Task {
 
     @Test
     public void goToHomepage() throws Exception {
 
+
         WebDriver driver = new ChromeDriver();
+        driver = BootcampUtils.initializeChromeDriver();
+
         driver.get("https://acctabootcamp.github.io/site/index2.html");
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
--- src/test/java/selenium/sample/Sample2Task.java
+++ src/test/java/selenium/sample/Sample2Task.java
@@ -1,10 +1,15 @@
 package selenium.sample;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
 
 public class Sample2Task {
     WebDriver driver;
@@ -29,18 +34,25 @@ public class Sample2Task {
     public void findElementByID() throws Exception {
 //         TODO:
 //         get text "Heading 2 text" using id
+    System.out.println(driver.findElement(By.id("heading_2")).getText());
     }
 
     @Test
     public void findElementByName() throws Exception {
 //         TODO:
 //         get attribute "id" and "value" of button "This is also a button" using name
+        System.out.println(driver.findElement(By.name("randomButton2")).getDomAttribute("id"));
+        System.out.println(driver.findElement(By.name("randomButton2")).getDomAttribute("value"));
+
     }
 
     @Test
     public void findElementByClassFirst() throws Exception {
 //         TODO:
 //         get first text of class "test" (should be "Test Text 1")
+        System.out.println(driver.findElement(By.className("test")).getText());
+        //dont think it's necessary to make a test here, but I'm doing it for practice :)
+        Assertions.assertEquals("Test Text 1", driver.findElement(By.className("test")).getText());
     }
 
     @Test
@@ -49,5 +61,17 @@ public class Sample2Task {
 //         get size text of class "test" (should be 5)
 //         get text of class "test"
 //         get third text of class "test" (should be "Test Text 4")
+        List<WebElement> classes = driver.findElements(By.className("test"));
+        System.out.println(classes.size());
+        System.out.println(classes.get(0).getText());
+        System.out.println(classes.get(2).getText());
+
+        // once, again, don't think it's neede but I'm doing it because why not :)
+        Assertions.assertEquals(5, classes.size());
+        Assertions.assertEquals("Test Text 4", classes.get(2).getText());
+
+/*        for(WebElement element : classes) {
+            System.out.println(element.getText());
+        }*/
     }
 }
