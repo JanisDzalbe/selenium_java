6050187a0729fc443cf622636bb3f32f1d4cac8f
Brigita Bruškytė <114769087+ClawOfNyx@users.noreply.github.com>
2025-05-19 13:58:02 +0300

    Completed sample task 4

--- src/test/java/selenium/sample/Sample4Task.java
+++ src/test/java/selenium/sample/Sample4Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample4Task {
     WebDriver driver;
@@ -38,6 +42,30 @@ public class Sample4Task {
 //        check that the button "Clear Result" is clickable now
 //        click on "Clear Result"
 //        check that the text is now (""), but it is not displayed
+        WebElement numberInput = driver.findElement(By.id("number"));
+        WebElement resultButton = driver.findElement(By.id("result_button_number"));
+        WebElement clearResultButton = driver.findElement(By.id("clear_result_button_number"));
+        WebElement resultText = driver.findElement(By.id("result_number"));
+
+        String inputValue = "7";
+        numberInput.clear();
+        numberInput.sendKeys(inputValue);
+
+        assertFalse(clearResultButton.isEnabled(), "Clear Result button should be disabled initially");
+        assertFalse(resultText.isDisplayed(), "Result text should not be displayed initially");
+
+        resultButton.click();
+
+        assertTrue(resultText.isDisplayed(), "Result text should be displayed after clicking Result");
+
+        String expectedText = "You entered number: \"" + inputValue + "\"";
+        assertEquals(expectedText, resultText.getText(), "Displayed result text is incorrect");
+        assertTrue(clearResultButton.isEnabled(), "Clear Result button should now be enabled");
+
+        clearResultButton.click();
+
+        assertEquals("", resultText.getText(), "Result text should be empty after clicking Clear Result");
+        assertFalse(resultText.isDisplayed(), "Result text should be hidden after clicking Clear Result");
     }
 
     @Test
@@ -47,5 +75,13 @@ public class Sample4Task {
 //        click on "This is a link to Homepage"
 //        check that current url is not base_url
 //        verify that current url is homepage
+        assertEquals(base_url, driver.getCurrentUrl(), "Current url is not the base url");
+
+        driver.findElement(By.linkText("This is a link to Homepage")).click();
+        assertNotEquals(base_url, driver.getCurrentUrl(), "Current url is the base url");
+
+        String pageHeader = driver.findElement(By.tagName("h1")).getText();
+        assertEquals("This is a home page", pageHeader, "Current url is not the homepage");
+        assertEquals("https://acctabootcamp.github.io/site/", driver.getCurrentUrl(), "Current url is not the homepage" );
     }
 }
