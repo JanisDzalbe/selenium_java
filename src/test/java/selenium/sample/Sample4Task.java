a6486563ebf83ca231673bbb41d3c2c659bb794b
nikita_rjabovs <nrsmm01@gmail.com>
2025-05-19 14:33:38 +0300

    Done 4th, 5th tasks

--- src/test/java/selenium/sample/Sample4Task.java
+++ src/test/java/selenium/sample/Sample4Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample4Task {
     WebDriver driver;
@@ -30,22 +34,46 @@ public class Sample4Task {
     public void enterNumber() throws Exception {
 //         TODO:
 //        enter a number under "Number"
+        WebElement numberInput = driver.findElement(By.id("number"));
+        numberInput.clear();
+        String number = "123";
+        numberInput.sendKeys(number);
 //        check that button is not clickable "Clear Result"
+        WebElement clearButton = driver.findElement(By.id("clear_result_button_number"));
+        assertFalse(clearButton.isEnabled(), "Clear Result button should be disabled initially");
 //        check that text is not displayed
+        WebElement resultText = driver.findElement(By.id("result_number"));
+        assertFalse(resultText.isDisplayed(), "Result text should not be visible initially");
 //        click on "Result" button
+        WebElement resultButton = driver.findElement(By.id("result_button_number"));
+        resultButton.click();
 //        check that text is displayed
+        assertTrue(resultText.isDisplayed(), "Result text should be visible after clicking 'Result'");
 //        check that the correct Text appears ("You entered number: "NUMBER YOU ENTERED"")
+        String expectedMessage = "You entered number: \"" + number + "\"";
+        assertEquals(expectedMessage, resultText.getText(), "Displayed message is incorrect");
 //        check that the button "Clear Result" is clickable now
+        assertTrue(clearButton.isEnabled(), "'Clear Result' button should be enabled after result is shown");
 //        click on "Clear Result"
+        clearButton.click();
 //        check that the text is now (""), but it is not displayed
+        assertFalse(resultText.isDisplayed(), "Result text should be hidden after clearing");
     }
 
     @Test
     public void clickOnLink() throws Exception {
 //         TODO:
 //        check current url is base_url
+        assertEquals(base_url, driver.getCurrentUrl(), "Initial URL should match base_url");
 //        click on "This is a link to Homepage"
+        WebElement link = driver.findElement(By.linkText("This is a link to Homepage"));
+        link.click();
+        Thread.sleep(1000);
 //        check that current url is not base_url
+        String newUrl = driver.getCurrentUrl();
+        assertNotEquals(base_url, newUrl, "URL should change after clicking the homepage link");
 //        verify that current url is homepage
+        String expectedHomeUrl = "https://acctabootcamp.github.io/site/";
+        assertEquals(expectedHomeUrl, newUrl, "Did not redirect to the correct homepage URL");
     }
 }
--- src/test/java/selenium/sample/Sample5Task.java
+++ src/test/java/selenium/sample/Sample5Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample5Task {
     WebDriver driver;
@@ -29,20 +33,33 @@ public class Sample5Task {
     public void goToAlertedPageViaButton() throws Exception {
 //         TODO:
 //        click on "To go to alerted page press Ok. Or stay here" button
+        driver.findElement(By.className("w3-blue")).click();
 //        switch to alert
 //        click ok
+        Alert firstAlert = driver.switchTo().alert();
+        firstAlert.accept();
 //        switch to second alert
+        Alert secondAlert = driver.switchTo().alert();
+        assertEquals("Booooooooo!", secondAlert.getText());
+        secondAlert.accept();
 //        verify alert text
 //        click ok on second alert
 //        verify that the correct page is opened
+        assertEquals("This page is alerted", driver.findElement(By.id("heading")).getText());
+
     }
 
     @Test
     public void doNotGoToAlertedPageViaButton() throws Exception {
 //         TODO:
 //        click on "To go to alerted page press Ok. Or stay here" button
+        driver.findElement(By.className("w3-blue")).click();
 //        switch to alert
 //        click cancel
+        Alert alert = driver.switchTo().alert();
+        alert.dismiss();
 //        verify the text on page
+        String expectedText = "So you desided to say? Good!";
+        assertEquals(expectedText, driver.findElement(By.id("textForAlerts")).getText());
     }
 }
