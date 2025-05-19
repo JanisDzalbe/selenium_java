8bfb598e312e94d9c636f403fd2af588296b7510
Brigita Bruškytė <114769087+ClawOfNyx@users.noreply.github.com>
2025-05-19 09:52:42 +0300

    Completed sample task 1

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -2,6 +2,8 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Sample1Task {
 
@@ -9,9 +11,19 @@ public class Sample1Task {
     public void goToHomepage() throws Exception {
 //        TODO:
 //         define driver
+        System.setProperty("webdriver.chrome.driver", "lib/chromedriver" + new selenium.ChangeToFileExtension().extension());
+        WebDriver driver = new ChromeDriver();
+
 //         go to https://acctabootcamp.github.io/site/index2.html
+        driver.get("https://acctabootcamp.github.io/site/index2.html");
+
 //         get title of page
+        System.out.println(driver.getTitle());
+
 //         get URL of current page
+        System.out.println(driver.getCurrentUrl());
+
 //         close browser
+        driver.quit();
     }
 }
