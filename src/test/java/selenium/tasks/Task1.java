f544bdb4ead7d901194aec442163245e4afe58e5
Brigita Bruškytė <114769087+ClawOfNyx@users.noreply.github.com>
2025-05-19 15:18:14 +0300

    Completed individual task 1

--- src/test/java/selenium/tasks/Task1.java
+++ src/test/java/selenium/tasks/Task1.java
@@ -3,8 +3,12 @@ package selenium.tasks;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 
@@ -29,6 +33,9 @@ public class Task1 {
     public void errorOnText() {
 //        TODO
 //        enter a text instead of a number, check that correct error is seen
+        driver.findElement(By.id("numb")).sendKeys("This is text");
+        driver.findElement(By.className("w3-btn")).click();
+        assertEquals("Please enter a number", driver.findElement(By.className("error")).getText());
     }
 
     @Test
@@ -36,6 +43,9 @@ public class Task1 {
 //        BUG: if I enter number 49 or 42 no errors where seen
 //        TODO
 //        enter number which is too small (positive number below 50), check that correct error is seen
+        driver.findElement(By.id("numb")).sendKeys("17");
+        driver.findElement(By.className("w3-btn")).click();
+        assertEquals("Number is too small", driver.findElement(By.className("error")).getText());
     }
 
     @Test
@@ -44,6 +54,9 @@ public class Task1 {
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
+        driver.findElement(By.id("numb")).sendKeys("177");
+        driver.findElement(By.className("w3-btn")).click();
+        assertEquals("Number is too big", driver.findElement(By.className("error")).getText());
     }
 
     @Test
@@ -52,5 +65,17 @@ public class Task1 {
 //        enter a number between 50 and 100 digit in the input, then press submit
 //        and check that no error is seen and that square root is calculated correctly
 //        NOTE: input value is hardcoded, but square root used in assertions should be calculated in code
+        int number = 67;
+        Double squareRoot = Math.sqrt(number);
+        String expected = String.format("Square root of %d is %.2f", number, squareRoot);
+
+        driver.findElement(By.id("numb")).sendKeys(String.format("%d", number));
+        driver.findElement(By.className("w3-btn")).click();
+
+        Alert alert = driver.switchTo().alert();
+        assertEquals(expected, alert.getText());
+        alert.accept();
+
+        assertEquals("", driver.findElement(By.className("error")).getText());
     }
 }
