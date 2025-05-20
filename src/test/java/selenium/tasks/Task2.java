4fe968fbee1cbbeedf29417188ffa57d07ec5890
Santana S <santana.voinikevica@gmail.com>
2025-05-20 17:03:23 +0300

    Doing task2: +initialFeedbackPage

--- src/test/java/selenium/tasks/Task2.java
+++ src/test/java/selenium/tasks/Task2.java
@@ -3,22 +3,25 @@ package selenium.tasks;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Task2 {
     WebDriver driver;
 
     @BeforeEach
     public void openPage() {
-        String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
-        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
         driver = new ChromeDriver();
         driver.get("https://acctabootcamp.github.io/site/tasks/provide_feedback");
     }
-
     @AfterEach
     public void closeBrowser() {
         driver.quit();
@@ -31,6 +34,35 @@ public class Task2 {
 //         "Don't know" is selected in "Genre"
 //         "Choose your option" in "How do you like us?"
 //         check that the button send is blue with white letters
+        WebElement nameField = driver.findElement(By.id("fb_name"));
+        WebElement ageField = driver.findElement(By.id("fb_age"));
+        WebElement maleRadio = driver.findElement(By.cssSelector("input[value='male']"));
+        WebElement femaleRadio = driver.findElement(By.cssSelector("input[value='female']"));
+        WebElement dK = driver.findElement(By.cssSelector("#fb_form > form > div:nth-child(4) > input:nth-child(6)"));
+        WebElement sendButton = driver.findElement(By.xpath("//*[@type='submit']"));
+
+        assertEquals("", nameField.getAttribute("value"));
+        assertEquals("", ageField.getAttribute("value"));
+
+        List<WebElement> langBoxs = driver.findElements(By.cssSelector("#lang_check"));
+
+        for (WebElement langBox : langBoxs) {
+            assertFalse(langBox.isSelected()); // checkboxes are NOT selected
+            langBox.click();
+        }
+
+        assertFalse(maleRadio.isSelected());
+        assertFalse(femaleRadio.isSelected());
+        assertTrue(dK.isSelected());
+
+        assertTrue(driver.findElement(By.id("like_us")).isDisplayed());
+        Select dropdown = new Select(driver.findElement(By.id("like_us")));
+        assertEquals("Choose your option", dropdown.getFirstSelectedOption().getText());
+
+        assertEquals("",driver.findElement(By.cssSelector("#fb_form > form > div:nth-child(6) > textarea")).getText());
+        assertEquals("rgba(33, 150, 243, 1)", sendButton.getCssValue("background-color"));
+        assertEquals("rgb(255, 255, 255)", sendButton.getCssValue("text-decoration-color"));
+
     }
 
     @Test
@@ -40,6 +72,8 @@ public class Task2 {
 //         check fields are empty or "null"
 //         check button colors
 //         (green with white letter and red with white letters)
+
+
     }
 
     @Test
