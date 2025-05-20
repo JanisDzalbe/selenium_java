57e0f577d27df0d4bf5d7a3e55f6ec5c4c363b77
nikita_rjabovs <nrsmm01@gmail.com>
2025-05-20 14:25:44 +0300

    Sample8Task.java completed

--- src/test/java/selenium/sample/Sample8Task.java
+++ src/test/java/selenium/sample/Sample8Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample8Task {
     WebDriver driver;
@@ -31,5 +35,14 @@ public class Sample8Task {
 //        check the background of top 2 sections
 //        rgba(255, 221, 221, 1);
 //        check h1 element font-size 64px
+
+        WebElement section1 = driver.findElement(By.cssSelector("body > div:nth-child(3) > div:nth-child(1) > div"));
+        assertEquals("rgba(255, 221, 221, 1)", section1.getCssValue("background-color"));
+
+        WebElement section2 = driver.findElement(By.cssSelector("body > div:nth-child(3) > div:nth-child(2) > div"));
+        assertEquals("rgba(255, 255, 204, 1)", section2.getCssValue("background-color"));
+
+        WebElement h1 = driver.findElement(By.cssSelector("body > div.w3-light-grey.w3-padding.w3-margin-bottom.w3-center > h1"));
+        assertEquals("64px", h1.getCssValue("font-size"));
     }
 }
