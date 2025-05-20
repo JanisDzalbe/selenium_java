7fe049cbd9c2556627173956a3bdf0047a059efd
Anastasija <anastas5504@gmail.com>
2025-05-20 17:09:17 +0300

    Completed Sample8Task

--- src/test/java/selenium/sample/Sample8Task.java
+++ src/test/java/selenium/sample/Sample8Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample8Task {
     WebDriver driver;
@@ -31,5 +35,13 @@ public class Sample8Task {
 //        check the background of top 2 sections
 //        rgba(255, 221, 221, 1);
 //        check h1 element font-size 64px
+        WebElement firstSection = driver.findElement(By.className("w3-pale-red"));
+        assertEquals("rgba(255, 221, 221, 1)", firstSection.getCssValue("background-color"));
+
+        WebElement secondSection = driver.findElement(By.className("w3-pale-yellow"));
+        assertEquals("rgba(255, 255, 204, 1)", secondSection.getCssValue("background-color"));
+
+        WebElement h1 = driver.findElement(By.className("w3-jumbo"));
+        assertEquals("64px", h1.getCssValue("font-size"));
     }
 }
