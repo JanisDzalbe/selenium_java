6c4baa8b05cf99a23cfb0d8cc590798d7c4f2265
Brigita Bruškytė <114769087+ClawOfNyx@users.noreply.github.com>
2025-05-21 09:50:23 +0300

    Completed extra 1 task

--- src/test/java/selenium/sample/extra/extra1Task.java
+++ src/test/java/selenium/sample/extra/extra1Task.java
@@ -3,8 +3,15 @@ package selenium.sample.extra;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import selenium.utility.BootcampUtils;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class extra1Task {
     WebDriver driver;
@@ -35,6 +42,16 @@ public class extra1Task {
 //        check that the url now "https://acctabootcamp.github.io/site/examples/po1"
 //        using driver navigation go back to "https://acctabootcamp.github.io/site/examples/po"
 //        check that the page now is "https://acctabootcamp.github.io/site/examples/po"
+        String startingUrl = "https://acctabootcamp.github.io/site/examples/po";
+        String destinationUrl = "https://acctabootcamp.github.io/site/examples/po1";
+
+        driver.get(startingUrl);
+
+        driver.findElement(By.cssSelector(".w3-pale-red a[href='po1']")).click();
+        assertEquals(destinationUrl, driver.getCurrentUrl());
+
+        driver.navigate().back();
+        assertEquals(startingUrl, driver.getCurrentUrl());
     }
 
     @Test
@@ -45,6 +62,19 @@ public class extra1Task {
 //        using driver navigation go back to "https://acctabootcamp.github.io/site/examples/po"
 //        using driver navigation go forward to "https://acctabootcamp.github.io/site/examples/po1"
 //        check that the page now is "https://acctabootcamp.github.io/site/examples/po1"
+        String startingUrl = "https://acctabootcamp.github.io/site/examples/po";
+        String destinationUrl = "https://acctabootcamp.github.io/site/examples/po1";
+
+        driver.get(startingUrl);
+
+        driver.findElement(By.cssSelector(".w3-pale-red a[href='po1']")).click();
+        assertEquals(destinationUrl, driver.getCurrentUrl());
+
+        driver.navigate().back();
+        assertEquals(startingUrl, driver.getCurrentUrl());
+
+        driver.navigate().forward();
+        assertEquals(destinationUrl, driver.getCurrentUrl());
     }
 
     @Test
@@ -55,5 +85,15 @@ public class extra1Task {
 //        check that text "I am here!" is seen
 //        refresh page
 //        check that text "I am here!" is not seen
+        WebDriverWait wait = (WebDriverWait) new WebDriverWait(driver, Duration.ofSeconds(10)).ignoring(StaleElementReferenceException.class);
+        driver.get("https://acctabootcamp.github.io/site/examples/act");
+
+        wait.until(driver -> driver.findElement(By.id("show_text")).isDisplayed());
+        driver.findElement(By.id("show_text")).click();
+        assertTrue(driver.findElement(By.id("show_me")).isDisplayed());
+        assertEquals("I am here!", driver.findElement(By.id("show_me")).getText());
+
+        driver.navigate().refresh();
+        assertFalse(driver.findElement(By.id("show_me")).isDisplayed());
     }
 }
