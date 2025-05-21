c4ae875e7ec047106f345b68b04b24ff0e5d8c24
Germans Veidemans <germans.veidemans@edu.rtu.lv>
2025-05-21 22:30:49 +0300

    Part of Task2 commit

--- src/test/java/selenium/tasks/Task2.java
+++ src/test/java/selenium/tasks/Task2.java
@@ -3,10 +3,16 @@ package selenium.tasks;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Task2 {
     WebDriver driver;
@@ -31,6 +37,29 @@ public class Task2 {
 //         "Don't know" is selected in "Genre"
 //         "Choose your option" in "How do you like us?"
 //         check that the button send is blue with white letters
+        List<WebElement> name_surname = driver.findElements(By.cssSelector(".w3-input[type='text']"));
+
+        for (WebElement box : name_surname) {
+            assertFalse(box.isSelected());
+        }
+
+        List<WebElement> checkBoxes = driver.findElements(By.cssSelector(".w3-check[type='checkbox']"));
+
+        for (WebElement checkBox : checkBoxes) {
+            assertFalse(checkBox.isSelected());
+        }
+
+        WebElement option3 = driver.findElement(By.xpath("(//input[@type='radio'])[3]"));
+        assertTrue(option3.isSelected());
+
+        Select likeUs = new Select(driver.findElement(By.id("like_us")));
+
+        likeUs.selectByVisibleText("Good");
+
+        WebElement send_button = driver.findElement(By.className("w3-blue"));
+        assertEquals("rgba(33, 150, 243, 1)", send_button.getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 255, 1)", send_button.getCssValue("color"));
+
     }
 
     @Test
@@ -40,6 +69,24 @@ public class Task2 {
 //         check fields are empty or "null"
 //         check button colors
 //         (green with white letter and red with white letters)
+
+        driver.findElement(By.className("w3-blue")).click();
+
+        List<WebElement> fields = driver.findElements(By.className("description"));
+
+        for (WebElement field : fields) {
+            String text = field.getText();
+            assertTrue(text.isEmpty() || text.equalsIgnoreCase("null"));
+        }
+
+        WebElement yes_button = driver.findElement(By.className("w3-green"));
+        assertEquals("rgba(76, 175, 80, 1)", yes_button.getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 255, 1)", yes_button.getCssValue("color"));
+
+        WebElement no_button = driver.findElement(By.className("w3-red"));
+        assertEquals("rgba(244, 67, 54, 1)", no_button.getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 255, 1)", no_button.getCssValue("color"));
+
     }
 
     @Test
@@ -49,6 +96,33 @@ public class Task2 {
 //         check fields are filled correctly
 //         check button colors
 //         (green with white letter and red with white letters)
+        String name = "German";
+        String age = "21";
+        String comment = "Hello world!";
+
+        WebElement name_field = driver.findElement(By.id("fb_name"));
+        name_field.sendKeys(name);
+
+        WebElement age_field = driver.findElement(By.id("fb_age"));
+        age_field.sendKeys(age);
+
+        WebElement lang_option = driver.findElement(By.cssSelector(".w3-check[value='English'][type='checkbox']"));
+        lang_option.click();
+        assertTrue(lang_option.isSelected());
+
+        WebElement genre_option = driver.findElement(By.xpath("(//input[@type='radio'])[1]"));
+        genre_option.click();
+        assertTrue(genre_option.isSelected());
+
+        Select likeUs = new Select(driver.findElement(By.id("like_us")));
+        likeUs.selectByVisibleText("Good");
+
+        WebElement comment_field = driver.findElement(By.name("comment"));
+        comment_field.sendKeys(comment);
+
+        driver.findElement(By.className("w3-blue")).click();
+        Thread.sleep(5000);
+
     }
 
     @Test
