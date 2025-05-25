69060d03ca43a8cbd4f406b3438dd5aa967ac61b
Alona_Cjumane <alona.cjumane@hotmail.com>
2025-05-25 16:37:07 +0300

    Completed task 4

--- src/test/java/selenium/sample/Sample4Task.java
+++ src/test/java/selenium/sample/Sample4Task.java
@@ -3,8 +3,14 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample4Task {
     WebDriver driver;
@@ -23,29 +29,69 @@ public class Sample4Task {
     // method which is being run after each test
     @AfterEach
     public void endingTests() throws Exception {
-        driver.quit();
+        //driver.quit();
     }
 
     @Test
     public void enterNumber() throws Exception {
 //         TODO:
+        WebElement numberArea = driver.findElement(By.id("number"));
+        String originalNumber = "5";
+        assertEquals(numberArea.getAttribute("value"), originalNumber);
+        numberArea.clear();
+
 //        enter a number under "Number"
+        String numberNew = "2";
+        numberArea.sendKeys(numberNew);
+
 //        check that button is not clickable "Clear Result"
+        WebElement clearResultButtonNumber = driver.findElement(By.id("clear_result_button_number"));
+        assertFalse(clearResultButtonNumber.isEnabled());
+
 //        check that text is not displayed
+        WebElement textResultNumber = driver.findElement(By.id("result_number"));
+        assertFalse(textResultNumber.isDisplayed());
+
 //        click on "Result" button
+        WebElement resultButtonNumber = driver.findElement(By.id("result_button_number"));
+        assertTrue(resultButtonNumber.isEnabled());
+        resultButtonNumber.click();
+
 //        check that text is displayed
+        assertTrue(textResultNumber.isDisplayed());
+
 //        check that the correct Text appears ("You entered number: "NUMBER YOU ENTERED"")
+        String expectedText = ("You entered number: \"" + numberNew + "\"");
+        assertEquals(expectedText, textResultNumber.getText());
+        assertTrue(textResultNumber.isDisplayed());
+
 //        check that the button "Clear Result" is clickable now
+        assertTrue(clearResultButtonNumber.isEnabled());
+
 //        click on "Clear Result"
+        clearResultButtonNumber.click();
+
 //        check that the text is now (""), but it is not displayed
+        assertEquals("", textResultNumber.getText());
+        assertFalse(textResultNumber.isDisplayed());
+
     }
+
 
     @Test
     public void clickOnLink() throws Exception {
 //         TODO:
 //        check current url is base_url
+        assertEquals(base_url, driver.getCurrentUrl());
+
 //        click on "This is a link to Homepage"
+        driver.findElement(By.id("homepage_link")).click();
+        assertEquals("This is a home page", driver.findElement(By.id("h1")).getText());
+
 //        check that current url is not base_url
+        assertFalse(driver.getCurrentUrl().equals(base_url));
+
 //        verify that current url is homepage
+        assertEquals("https://acctabootcamp.github.io/site/", driver.getCurrentUrl());
     }
 }
