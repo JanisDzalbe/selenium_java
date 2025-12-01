3b8d760156a6c71ad9c1211eaa5a00a3655524f0
xDoots <gladiatus2001@inbox.lv>
2025-12-01 14:05:32 +0200

    Complete Sample4Task

--- src/test/java/selenium/sample/Sample4Task.java
+++ src/test/java/selenium/sample/Sample4Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample4Task {
     WebDriver driver;
@@ -38,6 +42,23 @@ public class Sample4Task {
 //          check that the button "Clear Result" is clickable now
 //          click on "Clear Result"
 //          check that the text is now (""), but it is not displayed
+        WebElement textArea = driver.findElement(By.id("number"));
+        textArea.clear();
+        textArea.sendKeys("150");
+
+        WebElement clearButton = driver.findElement(By.id("clear_result_button_number"));
+        assertFalse(clearButton.isEnabled());
+        WebElement resultText = driver.findElement(By.id("result_number"));
+        assertFalse(resultText.isDisplayed());
+
+        driver.findElement(By.id("result_button_number")).click();
+        assertTrue(resultText.isDisplayed());
+        assertEquals("You entered number: \"150\"", resultText.getText());
+
+        assertTrue(clearButton.isEnabled());
+        clearButton.click();
+        assertEquals("", resultText.getText());
+        assertFalse(resultText.isDisplayed());
     }
 
     @Test
@@ -47,5 +68,9 @@ public class Sample4Task {
 //          click on "This is a link to Homepage"
 //          check that current url is not base_url
 //          verify that current url is homepage
+        assertTrue(driver.getCurrentUrl().equals(base_url));
+        driver.findElement(By.id("homepage_link")).click();
+        assertFalse(driver.getCurrentUrl().equals(base_url));
+        assertTrue(driver.getCurrentUrl().equals("https://janisdzalbe.github.io/example-site/"));
     }
 }
