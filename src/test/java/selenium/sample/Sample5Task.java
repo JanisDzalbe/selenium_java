91a4c04282c7ef2596654f7268c1b4b286e9831f
Krii01 <kzeikare@gmail.com>
2025-12-01 17:06:08 +0200

    Activity 5 and Task1

--- src/test/java/selenium/sample/Sample5Task.java
+++ src/test/java/selenium/sample/Sample5Task.java
@@ -3,8 +3,13 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample5Task {
     WebDriver driver;
@@ -35,6 +40,13 @@ public class Sample5Task {
 //          verify alert text
 //          click ok on second alert
 //          verify that the correct page is opened
+        driver.findElement(By.className("w3-blue")).click();
+        driver.switchTo().alert().accept();
+
+        Alert alert2 = driver.switchTo().alert();
+        assertEquals("Booooooooo!", alert2.getText());
+        alert2.accept();
+        assertEquals("https://janisdzalbe.github.io/example-site/examples/alerted_page", driver.getCurrentUrl());
     }
 
     @Test
@@ -44,5 +56,16 @@ public class Sample5Task {
 //          switch to alert
 //          click cancel
 //          verify the text on page
+//        driver.findElement(By.className("w3-blue")).click();
+//        Alert alert = driver.switchTo().alert();
+//        assertEquals("Want to see an alerted page?!", alert.getText());
+//
+//        alert.dismiss();
+//        assertEquals("So you desided to say? Good!", driver.findElement(By.id("textForAlerts")).getText());
+
+        driver.findElement(By.className("w3-blue")).click();
+        driver.switchTo().alert().dismiss();
+        assertEquals("https://janisdzalbe.github.io/example-site/examples/alerts_popups", driver.getCurrentUrl());
+
     }
 }
--- src/test/java/selenium/tasks/Task1.java
+++ src/test/java/selenium/tasks/Task1.java
@@ -3,10 +3,17 @@ package selenium.tasks;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
+import java.text.DecimalFormat;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Task1 {
     WebDriver driver;
@@ -29,18 +36,33 @@ public class Task1 {
     public void errorOnText() {
 //        TODO
 //         enter a text instead of a number, check that correct error is shown
+        WebElement textInput = driver.findElement(By.id("numb"));
+        textInput.sendKeys("hello");
+        driver.findElement(By.className("w3-btn")).click();
+
+        assertEquals("Please enter a number", driver.findElement(By.id("ch1_error")).getText());
     }
 
     @Test
     public void errorOnNumberTooSmall() {
 //        TODO
 //         enter number which is too small (positive number below 50), check that correct error is shown
+        WebElement numbInput = driver.findElement(By.id("numb"));
+        numbInput.sendKeys("1");
+        driver.findElement(By.className("w3-btn")).click();
+
+        assertEquals("Number is too small", driver.findElement(By.id("ch1_error")).getText());
     }
 
     @Test
     public void errorOnNumberTooBig() {
 //        TODO
 //         enter number which is too big (above 100), check that correct error is shown
+        WebElement numbInput = driver.findElement(By.id("numb"));
+        numbInput.sendKeys("230");
+        driver.findElement(By.className("w3-btn")).click();
+
+        assertEquals("Number is too big", driver.findElement(By.id("ch1_error")).getText());
     }
 
     @Test
@@ -49,5 +71,22 @@ public class Task1 {
 //         enter a number between 50 and 100 digit in the input, then press submit
 //         and check that no error is shown and that square root is calculated correctly
 //         NOTE: input and assertion values have to be defined as variables
+        double inputValue = 81;
+        double expectedResult = Math.sqrt(inputValue);
+        DecimalFormat df = new DecimalFormat("#.00");
+        String actualResult = df.format(expectedResult);
+        String expectedText = "Square root of " + inputValue + " is " + actualResult;
+
+        WebElement numbInput = driver.findElement(By.id("numb"));
+        numbInput.sendKeys(String.valueOf(inputValue));
+        driver.findElement(By.className("w3-btn")).click();
+
+        Alert alert = driver.switchTo().alert();
+
+        String actualText =alert.getText();
+
+        assertEquals(expectedText, actualText);
+
+        alert.accept();
     }
 }
