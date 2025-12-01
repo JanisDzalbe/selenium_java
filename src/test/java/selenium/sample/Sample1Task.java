1c3d0e106d7e9ef3f5f5bc6fb4bdf47055d13d66
Ravindu-DeAlwis <ravinduweerasiriwardhana@gmail.com>
2025-12-01 16:45:32 +0200

    Completed Sample1Task

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -2,6 +2,8 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Sample1Task {
 
@@ -9,9 +11,20 @@ public class Sample1Task {
     public void goToHomepage() throws Exception {
 //        TODO:
 //         define driver
+        System.setProperty(
+                "webdriver.chrome.driver",
+                System.getProperty("user.dir")
+                        + "/lib/chromedriver"
+                        + new selenium.ChangeToFileExtension().extension()
+        );
+        WebDriver driver = new ChromeDriver();
 //         go to https://janisdzalbe.github.io/example-site/index2.html
+        driver.get("https://janisdzalbe.github.io/example-site/index2.html");
 //         get title of page
+        System.out.println(driver.getTitle());
 //         get URL of current page
+        System.out.println(driver.getCurrentUrl());
 //         close browser
+        driver.quit();
     }
 }
