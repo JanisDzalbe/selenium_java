6a27d0b3575e5260ecf37fb54ca4adff6f3968a6
Nahid <nahid.clash76@gmail.com>
2025-12-01 11:11:20 +0200

    Sample2Task done

--- src/test/java/selenium/sample/Sample2Task.java
+++ src/test/java/selenium/sample/Sample2Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
 
 public class Sample2Task {
     WebDriver driver;
@@ -29,25 +33,38 @@ public class Sample2Task {
     public void findElementByID() throws Exception {
 //         TODO:
 //          get text "Heading 2 text" using id
+        System.out.println(driver.findElement(By.id("heading_2")).getText());
     }
 
     @Test
     public void findElementByName() throws Exception {
 //         TODO:
 //          get attribute "id" and "value" of button "This is also a button" using name
+        System.out.println(driver.findElement(By.name("randomButton2")).getAttribute("value"));
+        System.out.println(driver.findElement(By.name("randomButton2")).getAttribute("id"));
+
     }
 
     @Test
     public void findElementByClassFirst() throws Exception {
 //         TODO:
 //          get first text of class "test" (should be "Test Text 1")
+        System.out.println(driver.findElements(By.className("test")).get(0).getText());
     }
 
     @Test
     public void findElementByClassAll() throws Exception {
+        List<WebElement> elements = driver.findElements(By.className("test"));
+        System.out.println(elements.size());
+        for(WebElement element : elements){
+            System.out.println(element.getText());
+        }
+        System.out.println(elements.get(2).getText());
 //         TODO:
 //          get size text of class "test" (should be 5)
+
 //          get text of class "test"
+
 //          get third text of class "test" (should be "Test Text 4")
     }
 }
