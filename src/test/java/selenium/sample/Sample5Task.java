85e058f832e0446b69ce38e1775d8c4fe429bd5d
tuttifrutti7 <daces777@gmail.com>
2025-12-01 14:39:26 +0200

    completed Sample5Task

--- src/test/java/selenium/sample/Sample5Task.java
+++ src/test/java/selenium/sample/Sample5Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample5Task {
     WebDriver driver;
@@ -35,6 +39,15 @@ public class Sample5Task {
 //          verify alert text
 //          click ok on second alert
 //          verify that the correct page is opened
+        driver.findElement(By.className("w3-blue")).click();
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+        alert =  driver.switchTo().alert();
+        assertEquals("Booooooooo!", alert.getText());
+        alert.accept();
+
+        assertEquals("This page is alerted", driver.findElement(By.id("heading")).getText());
     }
 
     @Test
@@ -44,5 +57,10 @@ public class Sample5Task {
 //          switch to alert
 //          click cancel
 //          verify the text on page
+        driver.findElement(By.className("w3-blue")).click();
+        Alert alert = driver.switchTo().alert();
+        alert.dismiss();
+
+        assertEquals("So you desided to say? Good!", driver.findElement(By.id("textForAlerts")).getText());
     }
 }
