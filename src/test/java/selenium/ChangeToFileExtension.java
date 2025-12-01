bca197b8536bba9d7d8bc5416b63407bf42df1aa
Ergashev Nurmukhammad <ergashev.nurik01@mail.ru>
2025-12-01 14:40:34 +0300

    Initializing the project

--- src/test/java/selenium/ChangeToFileExtension.java
+++ src/test/java/selenium/ChangeToFileExtension.java
@@ -1,7 +1,7 @@
 package selenium;
 
 public class ChangeToFileExtension {
-
+    // Initializing the project
     public String extension() {
         System.out.println(System.getProperty("os.name") );
         if (System.getProperty("os.name").contains("Mac") || System.getProperty("os.name").contains("mac"))
