3df2447c8ffd37150af8587364d050851ede5869
julijakuznecova <cicadoidea@gmail.com>
2025-12-01 15:40:01 +0200

    4 task

--- src/test/java/selenium/sample/Sample4Task.java
+++ src/test/java/selenium/sample/Sample4Task.java
@@ -3,24 +3,24 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Sample4Task {
+
     WebDriver driver;
     String base_url = "https://janisdzalbe.github.io/example-site/examples/actions";
 
-    // method which is being run before each test
     @BeforeEach
     public void startingTests() throws Exception {
-        // Initialize driver
         driver = BootcampUtils.initializeChromeDriver();
-
-        //open page:
         driver.get(base_url);
     }
 
-    // method which is being run after each test
     @AfterEach
     public void endingTests() throws Exception {
         driver.quit();
@@ -28,24 +28,36 @@ public class Sample4Task {
 
     @Test
     public void enterNumber() throws Exception {
-//         TODO:
-//          enter a number under "Number"
-//          check that button is not clickable "Clear Result"
-//          check that text is not displayed
-//          click on "Result" button
-//          check that text is displayed
-//          check that the correct Text appears ("You entered number: "NUMBER YOU ENTERED"")
-//          check that the button "Clear Result" is clickable now
-//          click on "Clear Result"
-//          check that the text is now (""), but it is not displayed
+        WebElement numberInput = driver.findElement(By.id("number"));
+        WebElement resultButton = driver.findElement(By.id("result_button_number"));
+        WebElement clearButton = driver.findElement(By.id("clear_result_button_number"));
+        WebElement resultText = driver.findElement(By.id("result_number"));
+
+        assertFalse(resultText.isDisplayed());
+        assertTrue(resultButton.isEnabled());
+        assertFalse(clearButton.isEnabled());
+
+        numberInput.sendKeys("123");
+        resultButton.click();
+
+        assertTrue(resultText.isDisplayed());
+        assertEquals("You entered number: \"123\"", resultText.getText());
+        assertTrue(clearButton.isEnabled());
+
+        clearButton.click();
+
+        assertEquals("", resultText.getText());
+        assertFalse(resultText.isDisplayed());
     }
 
     @Test
     public void clickOnLink() throws Exception {
-//         TODO:
-//          check current url is base_url
-//          click on "This is a link to Homepage"
-//          check that current url is not base_url
-//          verify that current url is homepage
+        assertEquals(base_url, driver.getCurrentUrl());
+
+        WebElement homeLink = driver.findElement(By.linkText("Home"));
+        homeLink.click();
+
+        assertNotEquals(base_url, driver.getCurrentUrl());
+        assertEquals("https://janisdzalbe.github.io/example-site/", driver.getCurrentUrl());
     }
 }
