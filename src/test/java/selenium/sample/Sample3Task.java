d9bffcdd779b463a0e563bf21f94f7d0bb3df74c
Nahid <nahid.clash76@gmail.com>
2025-12-01 12:00:39 +0200

    Sample3Task done

--- src/test/java/selenium/sample/Sample3Task.java
+++ src/test/java/selenium/sample/Sample3Task.java
@@ -3,8 +3,14 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample3Task {
     WebDriver driver;
@@ -27,29 +33,71 @@ public class Sample3Task {
 
     @Test
     public void assertEqualsTask() throws Exception {
-//         TODO:
-//          check how many element with class "test" there are on page (5)
-//          check that value of second button is "This is also a button"
+        // check how many elements with class "test" there are on page (5)
+        int expectedNumberOfTestElements = 5;
+        int actualNumberOfTestElements = driver.findElements(By.className("test")).size();
+        assertEquals(expectedNumberOfTestElements, actualNumberOfTestElements);
+
+        // check that value/text of second button is "This is also a button"
+        List<WebElement> buttons = driver.findElements(By.cssSelector("button, input[type='button']"));
+        WebElement secondButton = buttons.get(1);
+
+        // get label from button (either inner text or value attribute)
+        String secondButtonText = secondButton.getText();
+        if (secondButtonText == null || secondButtonText.isEmpty()) {
+            secondButtonText = secondButton.getAttribute("value");
+        }
+
+        assertEquals("This is also a button", secondButtonText);
     }
 
     @Test
     public void assertTrueTask() throws Exception {
-//         TODO:
-//          check that it is True that value of second button is
-//          "this is Also a Button" if you ignore Caps Locks
-//          fail with custom error message:
+        // check that it is True that value of second button is
+        // "this is Also a Button" if you ignore Caps Locks
+        List<WebElement> buttons = driver.findElements(By.cssSelector("button, input[type='button']"));
+        WebElement secondButton = buttons.get(1);
+
+        String secondButtonText = secondButton.getText();
+        if (secondButtonText == null || secondButtonText.isEmpty()) {
+            secondButtonText = secondButton.getAttribute("value");
+        }
+
+        assertTrue(
+                secondButtonText.equalsIgnoreCase("this is Also a Button"),
+                "Second button text is not equal to 'this is Also a Button' ignoring case"
+        );
     }
 
     @Test
     public void assertFalseTask() throws Exception {
-//         TODO:
-//          check that it is False that value of second button is "This is a button"
+        // check that it is False that value of second button is "This is a button"
+        List<WebElement> buttons = driver.findElements(By.cssSelector("button, input[type='button']"));
+        WebElement secondButton = buttons.get(1);
+
+        String secondButtonText = secondButton.getText();
+        if (secondButtonText == null || secondButtonText.isEmpty()) {
+            secondButtonText = secondButton.getAttribute("value");
+        }
+
+        assertFalse(
+                secondButtonText.equals("This is a button"),
+                "Second button SHOULD NOT be exactly 'This is a button'"
+        );
     }
 
     @Test
     public void failTask() throws Exception {
-//        TODO:
-//         check that none of items with class "test"
-//         contain number 190
+        // check that none of items with class "test" contain number 190
+        List<WebElement> testItems = driver.findElements(By.className("test"));
+
+        for (WebElement item : testItems) {
+            String text = item.getText();
+            if (text != null && text.contains("190")) {
+                // if we ever find 190, we fail with custom error message
+                fail("Found forbidden number '190' in element text: " + text);
+            }
+        }
+        // if loop finishes without fail(), test passes
     }
 }
