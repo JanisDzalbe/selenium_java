7cdd0470a3488937ca1a13476375f2b8bf33af36
beate_scerbane <bscerbane1@gmail.com>
2025-12-01 14:04:43 +0200

    Sample4Task.java

--- src/test/java/selenium/sample/Sample4Task.java
+++ src/test/java/selenium/sample/Sample4Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample4Task {
     WebDriver driver;
@@ -38,6 +42,35 @@ public class Sample4Task {
 //          check that the button "Clear Result" is clickable now
 //          click on "Clear Result"
 //          check that the text is now (""), but it is not displayed
+
+        WebElement numberInput = driver.findElement(By.id("number"));
+        WebElement resultButton = driver.findElement(By.id("result_button_number"));
+        WebElement clearButton = driver.findElement(By.id("clear_result_button_number"));
+        WebElement resultText = driver.findElement(By.id("result_number"));
+
+        String numberToEnter = "10"; //number we will enter
+
+        numberInput.clear(); //clears any previous existing value in the box (default 5)
+
+        numberInput.sendKeys(numberToEnter); //entering the earlier defined 10
+
+        assertFalse(clearButton.isEnabled()); //check that button "Clear Result" is not clickable
+
+        assertFalse(resultText.isDisplayed()); // check that text is not displayed
+
+        resultButton.click(); //click on "Result" button
+
+        assertTrue(resultText.isDisplayed()); // check that text is displayed
+
+        String expectedText = "You entered number: \"" + numberToEnter + "\"";
+        assertEquals(expectedText, resultText.getText());
+
+        assertTrue(clearButton.isEnabled()); //check that the button "Clear Result" is clickable now
+
+        clearButton.click(); // clicking on "Clear Result"
+
+        assertEquals("", resultText.getText()); // Check that the text is now empty (""), but it is not displayed
+        assertFalse(resultText.isDisplayed());
     }
 
     @Test
@@ -47,5 +80,13 @@ public class Sample4Task {
 //          click on "This is a link to Homepage"
 //          check that current url is not base_url
 //          verify that current url is homepage
+
+        assertEquals(base_url, driver.getCurrentUrl()); //check current url is base_url
+
+        driver.findElement(By.linkText("This is a link to Homepage")).click(); // click on homepage
+
+        assertNotEquals(base_url, driver.getCurrentUrl()); //check that current url is not base_url
+
+        assertEquals("https://janisdzalbe.github.io/example-site/", driver.getCurrentUrl()); //verify that current url is homepage
     }
 }
