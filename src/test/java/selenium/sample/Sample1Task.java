5a1f1ebc98cde577ec2a10eab63e81bef3475bb4
julijakuznecova <cicadoidea@gmail.com>
2025-12-01 13:50:42 +0200

    1task

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -1,17 +1,31 @@
 package selenium.sample;
 
-
 import org.junit.jupiter.api.Test;
+import java.io.File;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
 
 public class Sample1Task {
 
+
     @Test
     public void goToHomepage() throws Exception {
-//        TODO:
-//         define driver
-//         go to https://janisdzalbe.github.io/example-site/index2.html
-//         get title of page
-//         get URL of current page
-//         close browser
+
+        // define driver
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + File.separator + "lib" + File.separator + "chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        // go to URL
+        driver.get("https://janisdzalbe.github.io/example-site/index2.html");
+
+        // get title of page
+        System.out.println(driver.getTitle());
+
+        // get URL of current page
+        System.out.println(driver.getCurrentUrl());
+
+        // close browser
+        driver.quit();
     }
 }
