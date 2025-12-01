8ebdca9548e37193c7729ae965d16efcbf81bdc6
ViktorijaHolina <viktorija.holina@gmail.com>
2025-12-01 12:01:37 +0200

    Task3

--- src/test/java/selenium/sample/Sample3Task.java
+++ src/test/java/selenium/sample/Sample3Task.java
@@ -3,8 +3,14 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample3Task {
     WebDriver driver;
@@ -27,6 +33,9 @@ public class Sample3Task {
 
     @Test
     public void assertEqualsTask() throws Exception {
+        List<WebElement> elemList = driver.findElements(By.className("test"));
+        assertEquals(5, elemList.size());
+        assertEquals("This is also a button", driver.findElements(By.id("buttonId")).getFirst().getDomAttribute("value"));
 //         TODO:
 //          check how many element with class "test" there are on page (5)
 //          check that value of second button is "This is also a button"
@@ -34,6 +43,7 @@ public class Sample3Task {
 
     @Test
     public void assertTrueTask() throws Exception {
+        assertTrue(driver.findElement(By.id("buttonId")).getDomAttribute("value").equalsIgnoreCase("this is Also a Button"), "custom error message");
 //         TODO:
 //          check that it is True that value of second button is
 //          "this is Also a Button" if you ignore Caps Locks
@@ -42,12 +52,20 @@ public class Sample3Task {
 
     @Test
     public void assertFalseTask() throws Exception {
+        assertFalse(driver.findElement(By.id("buttonId")).getDomAttribute("value").equals("This is a button"));
 //         TODO:
 //          check that it is False that value of second button is "This is a button"
     }
 
     @Test
     public void failTask() throws Exception {
+        List<WebElement> elemList = driver.findElements(By.className("test"));
+       for(WebElement elem : elemList){
+           if(elem.getText().contains("190")){
+               fail();
+
+           }
+       }
 //        TODO:
 //         check that none of items with class "test"
 //         contain number 190
