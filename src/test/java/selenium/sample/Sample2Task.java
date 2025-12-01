dd0d3717ad9c47685a3dac2f92d6c728488b5fad
ViktorijaHolina <viktorija.holina@gmail.com>
2025-12-01 11:05:27 +0200

    /

--- src/test/java/selenium/sample/Sample2Task.java
+++ src/test/java/selenium/sample/Sample2Task.java
@@ -3,7 +3,9 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
 
 public class Sample2Task {
@@ -27,24 +29,36 @@ public class Sample2Task {
 
     @Test
     public void findElementByID() throws Exception {
+        System.out.println(driver.findElement(By.id("heading_2")).getText());
 //         TODO:
 //          get text "Heading 2 text" using id
     }
 
     @Test
     public void findElementByName() throws Exception {
+        System.out.println(driver.findElement(By.name("randomButton2")).getAttribute("value"));
+
+        System.out.println(driver.findElement(By.name("randomButton2")).getDomProperty("id"));
+        System.out.println(driver.findElement(By.name("randomButton2")).getDomProperty("value"));
 //         TODO:
 //          get attribute "id" and "value" of button "This is also a button" using name
     }
 
     @Test
     public void findElementByClassFirst() throws Exception {
+        System.out.println(driver.findElement(By.className("test")).getText());
+        System.out.println(driver.findElements(By.className("test")).get(0).getText());
 //         TODO:
 //          get first text of class "test" (should be "Test Text 1")
     }
 
     @Test
     public void findElementByClassAll() throws Exception {
+        List<WebElement> elemList = driver.findElements(By.className("test"));
+        System.out.println(elemList.size());
+        for(WebElement elem : elemList){
+        }
+        System.out.println(elemList.get(2).getText());
 //         TODO:
 //          get size text of class "test" (should be 5)
 //          get text of class "test"
