a1f730473ab03ac3b5c604c28ee8de8061b27f4d
Mariss Zakaritis <marissz@proton.me>
2025-12-01 10:01:48 +0200

    Switched to Java 21 & updated Sample1Task

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -2,16 +2,23 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import selenium.utility.BootcampUtils;
+
+import java.io.File;
 
 public class Sample1Task {
-
     @Test
     public void goToHomepage() throws Exception {
-//        TODO:
 //         define driver
+        WebDriver driver = BootcampUtils.initializeChromeDriver();
 //         go to https://janisdzalbe.github.io/example-site/index2.html
+        driver.get("https://janisdzalbe.github.io/example-site/index2.html");
 //         get title of page
+        System.out.println(driver.getTitle());
 //         get URL of current page
+        System.out.println(driver.getCurrentUrl());
 //         close browser
+        driver.quit();
     }
 }
