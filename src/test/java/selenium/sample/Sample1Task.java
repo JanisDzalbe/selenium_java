172049d15c43fdfcf69d18375e424c2019b8e46c
Nahid <nahid.clash76@gmail.com>
2025-12-01 09:57:54 +0200

    Sample1Task done

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -2,16 +2,37 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Sample1Task {
 
-    @Test
-    public void goToHomepage() throws Exception {
+//    @Test
+//    public void goToHomepage() throws Exception {
 //        TODO:
 //         define driver
 //         go to https://janisdzalbe.github.io/example-site/index2.html
 //         get title of page
 //         get URL of current page
 //         close browser
+//    }
+    @Test
+    public void goToHomepage() throws Exception {
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.get("https://janisdzalbe.github.io/example-site/index2.html");
+            String title = driver.getTitle();
+            System.out.println("Page title: " + title);
+            String currentUrl = driver.getCurrentUrl();
+            System.out.println("Current URL: " + currentUrl);
+
+
+        } finally {
+
+            driver.quit();
+        }
     }
+
 }
