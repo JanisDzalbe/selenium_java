07c6fb48535922454cfa013f2f66ad6d329db8da
Tatjana <4mistralia@gmail.com>
2025-12-01 11:14:17 +0200

    Sample Tasks 1 and 2

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -2,6 +2,10 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static selenium.sample.Sample1.libWithDriversLocation;
 
 public class Sample1Task {
 
@@ -13,5 +17,13 @@ public class Sample1Task {
 //         get title of page
 //         get URL of current page
 //         close browser
+
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://janisdzalbe.github.io/example-site/index2.html");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
+
     }
 }
