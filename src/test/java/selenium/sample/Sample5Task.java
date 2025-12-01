b161b673928f403d5d4d6bd0bc0b8befc804dc59
Mariss Zakaritis <marissz@proton.me>
2025-12-01 14:49:19 +0200

    Updated Sample5Task

--- src/test/java/selenium/sample/Sample5Task.java
+++ src/test/java/selenium/sample/Sample5Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample5Task {
     WebDriver driver;
@@ -27,22 +31,29 @@ public class Sample5Task {
 
     @Test
     public void goToAlertedPageViaButton() throws Exception {
-//         TODO:
 //          click on "To go to alerted page press Ok. Or stay here" button
+        driver.findElement(By.className("w3-blue")).click();
 //          switch to alert
 //          click ok
+        driver.switchTo().alert().accept();
 //          switch to second alert
+        Alert alert = driver.switchTo().alert();
 //          verify alert text
+        assertEquals("Booooooooo!", alert.getText());
 //          click ok on second alert
+        alert.accept();
 //          verify that the correct page is opened
+        assertEquals("https://janisdzalbe.github.io/example-site/examples/alerted_page", driver.getCurrentUrl());
     }
 
     @Test
     public void doNotGoToAlertedPageViaButton() throws Exception {
-//         TODO:
 //          click on "To go to alerted page press Ok. Or stay here" button
+        driver.findElement(By.className("w3-blue")).click();
 //          switch to alert
 //          click cancel
+        driver.switchTo().alert().dismiss();
 //          verify the text on page
+        assertEquals("So you desided to say? Good!", driver.findElement(By.id("textForAlerts")).getText());
     }
 }
