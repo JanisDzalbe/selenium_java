5e451405c6d02440c9ad5783a4450d08a42b3a47
RoboStriker2k <rlaimite@gmail.com>
2025-12-01 10:49:14 +0200

    Task2 implementation

--- src/test/java/selenium/sample/Sample2Task.java
+++ src/test/java/selenium/sample/Sample2Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
 
 public class Sample2Task {
     WebDriver driver;
@@ -29,18 +33,24 @@ public class Sample2Task {
     public void findElementByID() throws Exception {
 //         TODO:
 //          get text "Heading 2 text" using id
+      System.out.println(driver.findElement(By.id("heading_2")).getText());
     }
 
     @Test
     public void findElementByName() throws Exception {
 //         TODO:
 //          get attribute "id" and "value" of button "This is also a button" using name
+
+
+      System.out.println(driver.findElement(By.name("randomButton2")).getDomProperty("value")); // "This is a button"
+      System.out.println(driver.findElement(By.name("randomButton2")).getDomProperty("id"));
     }
 
     @Test
     public void findElementByClassFirst() throws Exception {
 //         TODO:
 //          get first text of class "test" (should be "Test Text 1")
+      System.out.println(driver.findElement(By.className("test")).getText());
     }
 
     @Test
@@ -49,5 +59,12 @@ public class Sample2Task {
 //          get size text of class "test" (should be 5)
 //          get text of class "test"
 //          get third text of class "test" (should be "Test Text 4")
+
+      System.out.println(driver.findElements(By.className("test")).size());
+
+      List<WebElement> allElementsWithClass = driver.findElements(By.className("test"));
+
+      System.out.println(allElementsWithClass.get(2).getText());
+
     }
 }
