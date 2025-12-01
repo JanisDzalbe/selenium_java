8f8eefef67c5bbf1d94173656f07002d3072800e
sofijaeveratt <sofija.everatt@gmail.com>
2025-12-01 09:58:31 +0200

    sample task1

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -2,6 +2,9 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Sample1Task {
 
@@ -9,9 +12,21 @@ public class Sample1Task {
     public void goToHomepage() throws Exception {
 //        TODO:
 //         define driver
+        System.setProperty("webdriver.chrome.driver", lib.chromedriver.exe + "chromedriver" + new selenium.ChangeToFileExtension().extension());
+
+        WebDriver driver = new ChromeDriver();
+
 //         go to https://janisdzalbe.github.io/example-site/index2.html
+        driver.get("https://janisdzalbe.github.io/example-site/index2.html");
+
 //         get title of page
+        System.out.println(driver.getTitle());
+
 //         get URL of current page
+        System.out.println(driver.findElement(By.id("h1")).getText());
+
 //         close browser
+        driver.quit();
+
     }
 }
