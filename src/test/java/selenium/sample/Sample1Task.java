cfe6fef785c879005f253b95b2931a83d00beb9a
juris_smirnovs <jurcixx21@gmail.com>
2025-12-01 10:07:03 +0200

    Task completed

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -2,16 +2,17 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class Sample1Task {
 
     @Test
     public void goToHomepage() throws Exception {
-//        TODO:
-//         define driver
-//         go to https://janisdzalbe.github.io/example-site/index2.html
-//         get title of page
-//         get URL of current page
-//         close browser
+        System.setProperty("webdriver.chrome","lib/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://janisdzalbe.github.io/example-site/index2.html");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
     }
 }
