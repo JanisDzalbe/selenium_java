ed5f439e6ae91bb577804ab839eeaf9b0cce95b3
Vito-devops <vittallen@gmail.com>
2025-12-01 09:59:30 +0200

    Vitaly_test_commit

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -2,12 +2,31 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
 
 public class Sample1Task {
+   // static String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
 
     @Test
     public void goToHomepage() throws Exception {
-//        TODO:
+//
+//        System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe" );
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://janisdzalbe.github.io/example-site/index2.html");
+//        System.out.println(driver.findElement(By.id("h1")).getText());
+//
+//        System.out.println(driver.getTitle());
+//        System.out.println(driver.getCurrentUrl());
+//
+//        Thread.sleep(10000);
+//
+//        driver.quit();
+
+        //        TODO:
 //         define driver
 //         go to https://janisdzalbe.github.io/example-site/index2.html
 //         get title of page
