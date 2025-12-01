5b8cea58eb02243b8f3975b634bed7f22fa0e034
Krii01 <kzeikare@gmail.com>
2025-12-01 11:05:01 +0200

    Activity 2

--- src/test/java/selenium/sample/Sample2Task.java
+++ src/test/java/selenium/sample/Sample2Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
 
 public class Sample2Task {
     WebDriver driver;
@@ -29,18 +33,26 @@ public class Sample2Task {
     public void findElementByID() throws Exception {
 //         TODO:
 //          get text "Heading 2 text" using id
+        System.out.println(driver.findElement(By.id("heading_2")).getText());
     }
 
     @Test
     public void findElementByName() throws Exception {
 //         TODO:
 //          get attribute "id" and "value" of button "This is also a button" using name
+        System.out.println(driver.findElement(By.name("randomButton2")).getAttribute("id"));
+        System.out.println(driver.findElement(By.name("randomButton2")).getAttribute("value"));
+
+//        System.out.println(driver.findElement(By.name("randomButton2")).getDomAttribute("id"));
+//        System.out.println(driver.findElement(By.name("randomButton2")).getDomAttribute("value"));
     }
 
     @Test
     public void findElementByClassFirst() throws Exception {
 //         TODO:
 //          get first text of class "test" (should be "Test Text 1")
+        // System.out.println(driver.findElement(By.className("test")).getText());
+        System.out.println(driver.findElements(By.className("test")).get(0).getText()); //Better to do it this way if there are multiple elements
     }
 
     @Test
@@ -49,5 +61,11 @@ public class Sample2Task {
 //          get size text of class "test" (should be 5)
 //          get text of class "test"
 //          get third text of class "test" (should be "Test Text 4")
+        List<WebElement> elementsWithClass = driver.findElements(By.className("test"));
+        System.out.println(elementsWithClass.size());
+        for (WebElement elementWithClass : elementsWithClass) {
+            System.out.println(elementWithClass.getText());
+        }
+        System.out.println(driver.findElements(By.className("test")).get(2).getText());
     }
 }
