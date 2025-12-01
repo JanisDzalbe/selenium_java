c4d791504067dcd45e6672a3788c4dfe14d83188
Vito-devops <vittallen@gmail.com>
2025-12-01 11:06:16 +0200

    Vitaly_test_commit_1.1

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -17,7 +17,9 @@ public class Sample1Task {
         System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe" );
         WebDriver driver = new ChromeDriver();
         driver.get("https://janisdzalbe.github.io/example-site/index2.html");
-        System.out.println(driver.findElement(By.tagName("h1")).getText());
+
+     //  System.out.println(driver.findElement(By.tagName("h1")).getText());
+        System.out.println(driver.findElement(By.id("header")).getText());
 
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
