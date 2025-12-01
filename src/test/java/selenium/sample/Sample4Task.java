111af7883d7650f68f8962d72c6a9e79340f0108
Ergashev Nurmukhammad <ergashev.nurik01@mail.ru>
2025-12-01 15:17:49 +0300

    Submitting Third & Forth Assignments

--- src/test/java/selenium/sample/Sample4Task.java
+++ src/test/java/selenium/sample/Sample4Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample4Task {
     WebDriver driver;
@@ -38,6 +42,53 @@ public class Sample4Task {
 //          check that the button "Clear Result" is clickable now
 //          click on "Clear Result"
 //          check that the text is now (""), but it is not displayed
+        WebElement numberInput = driver.findElement(
+                By.xpath("//h2[normalize-space()='Number']/following::input[1]")
+        );
+
+        WebElement resultButton = driver.findElement(
+                By.xpath("//h2[normalize-space()='Number']/following::button[normalize-space()='Result'][1]")
+        );
+
+        WebElement clearButton = driver.findElement(
+                By.xpath("//h2[normalize-space()='Number']/following::button[normalize-space()='Clear Result'][1]")
+        );
+
+        // Result text (Number bölməsində çıxan mətni götürürük)
+        WebElement resultText = driver.findElement(
+                By.xpath("//h2[normalize-space()='Number']/following::p[1]")
+        );
+
+        String numberToEnter = "123";
+
+        numberInput.clear();
+        numberInput.sendKeys(numberToEnter);
+
+        // check that button is not clickable "Clear Result"
+        assertFalse(clearButton.isEnabled());
+
+        // check that text is not displayed
+        assertFalse(resultText.isDisplayed());
+
+        // click on "Result" button
+        resultButton.click();
+
+        // check that text is displayed
+        assertTrue(resultText.isDisplayed());
+
+        // check that the correct Text appears ("You entered number: \"NUMBER YOU ENTERED\"")
+        String expectedText = "You entered number: " + numberToEnter;
+        assertEquals(expectedText, resultText.getText());
+
+        // check that the button "Clear Result" is clickable now
+        assertTrue(clearButton.isEnabled());
+
+        // click on "Clear Result"
+        clearButton.click();
+
+        // check that the text is now (""), but it is not displayed
+        assertFalse(resultText.isDisplayed());
+        assertEquals("", resultText.getText());
     }
 
     @Test
@@ -47,5 +98,16 @@ public class Sample4Task {
 //          click on "This is a link to Homepage"
 //          check that current url is not base_url
 //          verify that current url is homepage
+        // check current url is base_url
+        assertEquals(base_url, driver.getCurrentUrl());
+
+        // click on "This is a link to Homepage"
+        driver.findElement(By.linkText("This is a link to Homepage")).click();
+
+        // check that current url is not base_url
+        assertFalse(driver.getCurrentUrl().equals(base_url));
+
+        // verify that current url is homepage
+        assertEquals("https://janisdzalbe.github.io/example-site/", driver.getCurrentUrl());
     }
 }
