dba093da1bb517569708392777e2f042014ae9fd
AhmedMostafa290 <ahmed.ezz.mostafa@gmail.com>
2025-12-01 15:17:14 +0200

    Add implementation to Sample5Task

--- src/test/java/selenium/sample/Sample5Task.java
+++ src/test/java/selenium/sample/Sample5Task.java
@@ -3,8 +3,14 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Sample5Task {
     WebDriver driver;
@@ -27,22 +33,39 @@ public class Sample5Task {
 
     @Test
     public void goToAlertedPageViaButton() throws Exception {
-//         TODO:
-//          click on "To go to alerted page press Ok. Or stay here" button
-//          switch to alert
-//          click ok
-//          switch to second alert
-//          verify alert text
-//          click ok on second alert
-//          verify that the correct page is opened
+        //TODO:
+        //click on "To go to alerted page press Ok. Or stay here" button
+        WebElement button = driver.findElement(By.cssSelector(".w3-blue"));
+        button.click();
+        //switch to alert
+        Alert firstAlert = driver.switchTo().alert();
+        //click ok
+        firstAlert.accept();
+        //switch to second alert
+        Alert secondAlert = driver.switchTo().alert();
+        //verify alert text
+        assertEquals( "Booooooooo!", secondAlert.getText());
+        //click ok on second alert
+        secondAlert.accept();
+        //verify that the correct page is opened
+        assertEquals("https://janisdzalbe.github.io/example-site/examples/alerted_page", driver.getCurrentUrl());
+        //Verify this text is displayed "This page is alerted" to make sure that the correct page is opened
+        WebElement textHeader = driver.findElement(By.id("heading"));
+        assertTrue(textHeader.isDisplayed());
     }
 
     @Test
     public void doNotGoToAlertedPageViaButton() throws Exception {
-//         TODO:
-//          click on "To go to alerted page press Ok. Or stay here" button
-//          switch to alert
-//          click cancel
-//          verify the text on page
+        //TODO:
+        //click on "To go to alerted page press Ok. Or stay here" button
+        WebElement button = driver.findElement(By.cssSelector(".w3-blue"));
+        button.click();
+        //switch to alert
+        Alert alert = driver.switchTo().alert();
+        //click cancel
+        alert.dismiss();
+        //verify the text on page
+        assertEquals("https://janisdzalbe.github.io/example-site/examples/alerts_popups", driver.getCurrentUrl());
+        assertTrue(button.isDisplayed());
     }
 }
