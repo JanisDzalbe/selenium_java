1fadf72d720b77d2d0d753f2c2add732166940e7
xDoots <gladiatus2001@inbox.lv>
2025-12-01 09:59:17 +0200

    Complete Sample1Task

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -2,8 +2,13 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
 
 public class Sample1Task {
+    static String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
 
     @Test
     public void goToHomepage() throws Exception {
@@ -13,5 +18,13 @@ public class Sample1Task {
 //         get title of page
 //         get URL of current page
 //         close browser
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://janisdzalbe.github.io/example-site/index2.html");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        Thread.sleep(5000);
+        driver.quit();
     }
 }
