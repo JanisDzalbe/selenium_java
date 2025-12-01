0f4096e01b53a5a735dab0b6dc00d5ab927ac05a
ViktorijaHolina <viktorija.holina@gmail.com>
2025-12-01 14:49:20 +0200

    Task5

--- src/test/java/selenium/sample/Sample5Task.java
+++ src/test/java/selenium/sample/Sample5Task.java
@@ -3,8 +3,14 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample5Task {
     WebDriver driver;
@@ -27,6 +33,14 @@ public class Sample5Task {
 
     @Test
     public void goToAlertedPageViaButton() throws Exception {
+        driver.findElement(By.className("w3-blue")).click();
+        driver.switchTo().alert().accept();
+
+        Alert secondAlert = driver.switchTo().alert();
+        assertEquals("Booooooooo!", secondAlert.getText());
+        secondAlert.accept();
+
+        assertEquals("https://janisdzalbe.github.io/example-site/examples/alerted_page", driver.getCurrentUrl());
 //         TODO:
 //          click on "To go to alerted page press Ok. Or stay here" button
 //          switch to alert
@@ -39,6 +53,9 @@ public class Sample5Task {
 
     @Test
     public void doNotGoToAlertedPageViaButton() throws Exception {
+        driver.findElement(By.className("w3-blue")).click();
+        driver.switchTo().alert().dismiss();
+        assertEquals("https://janisdzalbe.github.io/example-site/examples/alerts_popups", driver.getCurrentUrl());
 //         TODO:
 //          click on "To go to alerted page press Ok. Or stay here" button
 //          switch to alert
