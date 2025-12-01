1159fa23378932b015b77bce8f23bacb22b3b09f
beate_scerbane <bscerbane1@gmail.com>
2025-12-01 14:49:05 +0200

    Sample5Task.java

--- src/test/java/selenium/sample/Sample5Task.java
+++ src/test/java/selenium/sample/Sample5Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample5Task {
     WebDriver driver;
@@ -35,6 +39,23 @@ public class Sample5Task {
 //          verify alert text
 //          click ok on second alert
 //          verify that the correct page is opened
+
+        driver.findElement(By.xpath("//button[text()='To go to alerted page press Ok. Or stay here']")).click(); //click on the button by text
+
+        Alert alert1 = driver.switchTo().alert(); //first alert
+
+        alert1.accept(); //we press ok
+
+        Alert alert2 = driver.switchTo().alert(); //second alert
+
+        // Verify alert text is "Booooooooo!"
+        String expectedAlertText = "Booooooooo!";
+        assertEquals(expectedAlertText, alert2.getText());
+
+        alert2.accept(); //click ok
+
+        String expectedUrl = "https://janisdzalbe.github.io/example-site/examples/alerted_page";
+        assertEquals(expectedUrl, driver.getCurrentUrl());      //check if correct page opened
     }
 
     @Test
@@ -44,5 +65,15 @@ public class Sample5Task {
 //          switch to alert
 //          click cancel
 //          verify the text on page
+
+        driver.findElement(By.xpath("//button[text()='To go to alerted page press Ok. Or stay here']")).click(); //click on the button by text
+
+        Alert alert = driver.switchTo().alert(); //alert popup
+
+        alert.dismiss(); //click cancel
+
+        String expectedText = "So you desided to say? Good!";       //normally we would test it for typos but here I will include typo, so test succeeds
+        String actualText = driver.findElement(By.id("textForAlerts")).getText();
+        assertEquals(expectedText, actualText);
     }
 }
