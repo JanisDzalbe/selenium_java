8ad809d20db977e6a3518296b9a62ff9b34e42c2
AhmedMostafa290 <ahmed.ezz.mostafa@gmail.com>
2025-12-02 15:36:44 +0200

    Add implementation to Sample8Task

--- src/test/java/selenium/sample/Sample8Task.java
+++ src/test/java/selenium/sample/Sample8Task.java
@@ -3,8 +3,15 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Sample8Task {
     WebDriver driver;
@@ -29,14 +36,26 @@ public class Sample8Task {
     public void findPersonByXPath() throws Exception {
 //  TODO:
 //   Click “Reset List”
+        driver.findElement(By.id("resetListBtn")).click();
+        WebElement jill = driver.findElement(By.xpath("//*[@class ='w3-padding-16' and .//*[text()='Jill']]"));
 //   Using xPath, find “John” and asset that he is “Software Engineer”
+        assertEquals("Support", jill.findElement(By.xpath(".//*[@class='job']")).getText());
+        assertEquals("Support", jill.findElement(By.className("job")).getText());
     }
 
     @Test
     public void findPersonWithLoop() throws Exception {
 //  TODO:
 //   Click “Shuffle Order”
-//   Using a loop, Find “Jane” and assert that she is “Accountant”
+        driver.findElement(By.id("shuffleBtn")).click();
+        //   Using a loop, Find “Jane” and assert that she is “Accountant”
+        List<WebElement> listOfPersons = driver.findElements(By.className("w3-padding-16"));
+        for (WebElement person : listOfPersons) {
+            if (person.findElement(By.className("name")).getText().equals("Jane"))
+            {
+                assertEquals("Accountant", person.findElement(By.className("job")).getText());
+            }
+        }
     }
 
     @Test
