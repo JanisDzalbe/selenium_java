596861788547749ab8a6264b9b90a852dabd3548
julijakuznecova <cicadoidea@gmail.com>
2025-12-02 10:19:21 +0200

    6 Task

--- src/test/java/selenium/sample/Sample6Task.java
+++ src/test/java/selenium/sample/Sample6Task.java
@@ -34,46 +34,46 @@ public class Sample6Task {
 
 
         System.out.println("\t Heading 2 text v1 is '" +
-                driver.findElement(By.xpath("//*[text()='Heading 2 text']")).getText() + "'");
+                driver.findElement(By.xpath("//*[text()='Heading 2 text']")).getText());
 
         System.out.println("\t Heading 2 text v2 is '" +
-                driver.findElement(By.xpath("//*[@id='heading_2']")).getText() + "'");
+                driver.findElement(By.xpath("//*[@id='heading_2']")).getText());
 
         System.out.println("\t Test Text 1 v1 is '" +
-                driver.findElement(By.xpath("//*[text()='Test Text 1']")).getText() + "'");
+                driver.findElement(By.xpath("//*[text()='Test Text 1']")).getText());
 
         System.out.println("\t Test Text 1 v2 is '" +
-                driver.findElement(By.xpath("//*[@id='test1']/p[1]")).getText() + "'");
+                driver.findElement(By.xpath("//*[@id='test1']/p[1]")).getText());
 
         System.out.println("\t Test Text 2 v1 is '" +
-                driver.findElement(By.xpath("//*[text()='Test Text 2']")).getText() + "'");
+                driver.findElement(By.xpath("//*[text()='Test Text 2']")).getText());
 
         System.out.println("\t Test Text 2 v2 is '" +
-                driver.findElement(By.xpath("//*[@id='test1']/p[2]")).getText() + "'");
+                driver.findElement(By.xpath("//*[@id='test1']/p[2]")).getText());
 
         System.out.println("\t Test Text 3 v1 is '" +
-                driver.findElement(By.xpath("//*[text()='Test Text 3']")).getText() + "'");
+                driver.findElement(By.xpath("//*[text()='Test Text 3']")).getText());
 
         System.out.println("\t Test Text 3 v2 is '" +
-                driver.findElement(By.xpath("//*[@id='test3']/p[1]")).getText() + "'");
+                driver.findElement(By.xpath("//*[@id='test3']/p[1]")).getText());
 
         System.out.println("\t Test Text 4 v1 is '" +
-                driver.findElement(By.xpath("//*[text()='Test Text 4']")).getText() + "'");
+                driver.findElement(By.xpath("//*[text()='Test Text 4']")).getText());
 
         System.out.println("\t Test Text 4 v2 is '" +
-                driver.findElement(By.xpath("//*[@id='test3']/p[2]")).getText() + "'");
+                driver.findElement(By.xpath("//*[@id='test3']/p[2]")).getText());
 
         System.out.println("\t Test Text 5 v1 is '" +
-                driver.findElement(By.xpath("//*[text()='Test Text 5']")).getText() + "'");
+                driver.findElement(By.xpath("//*[text()='Test Text 5']")).getText());
 
         System.out.println("\t Test Text 5 v2 is '" +
-                driver.findElement(By.xpath("//*[@id='test2']/p[1]")).getText() + "'");
+                driver.findElement(By.xpath("//*[@id='test2']/p[1]")).getText());
 
         System.out.println("\t This is also a button v1 is '" +
-                driver.findElement(By.xpath("//*[@id='buttonId']")).getDomProperty("value") + "'");
+                driver.findElement(By.xpath("//*[@id='buttonId']")).getDomProperty("value") );
 
         System.out.println("\t This is also a button v2 is '" +
-                driver.findElement(By.xpath("//*[@value='This is also a button']")).getDomProperty("value") + "'");
+                driver.findElement(By.xpath("//*[@value='This is also a button']")).getDomProperty("value") );
     }
 
 
@@ -82,30 +82,30 @@ public class Sample6Task {
         System.out.println("Find elements by text using CSS:");
 
         System.out.println("\t Heading 2 text v1 is '" +
-                driver.findElement(By.cssSelector("#heading_2")).getText() + "'");
+                driver.findElement(By.cssSelector("#heading_2")).getText() );
 
         System.out.println("\t Heading 2 text v2 is '" +
-                driver.findElement(By.cssSelector("h2#heading_2")).getText() + "'");
+                driver.findElement(By.cssSelector("h2#heading_2")).getText());
 
         System.out.println("\t Test Text 1 v1 is '" +
-                driver.findElement(By.cssSelector("#test1 p:nth-of-type(1)")).getText() + "'");
+                driver.findElement(By.cssSelector("#test1 p:nth-of-type(1)")).getText());
 
         System.out.println("\t Test Text 2 v1 is '" +
-                driver.findElement(By.cssSelector("#test1 .twoTest")).getText() + "'");
+                driver.findElement(By.cssSelector("#test1 .twoTest")).getText());
 
         System.out.println("\t Test Text 2 v2 is '" +
-                driver.findElement(By.cssSelector("#test1 p:nth-of-type(2)")).getText() + "'");
+                driver.findElement(By.cssSelector("#test1 p:nth-of-type(2)")).getText());
 
         System.out.println("\t Test Text 3 v1 is '" +
                 driver.findElement(By.cssSelector("#test3 p.test")).getText() + "'");
 
         System.out.println("\t Test Text 3 v2 is '" +
-                driver.findElement(By.cssSelector("#test3 p:nth-of-type(1)")).getText() + "'");
+                driver.findElement(By.cssSelector("#test3 p:nth-of-type(1)")).getText());
 
         System.out.println("\t This is also a button v1 is '" +
-                driver.findElement(By.cssSelector("#buttonId")).getDomProperty("value") + "'");
+                driver.findElement(By.cssSelector("#buttonId")).getDomProperty("value"));
 
         System.out.println("\t This is also a button v2 is '" +
-                driver.findElement(By.cssSelector("[value='This is also a button']")).getDomProperty("value") + "'");
+                driver.findElement(By.cssSelector("[value='This is also a button']")).getDomProperty("value"));
     }
 }
--- src/test/java/selenium/sample/Sample7Task.java
+++ src/test/java/selenium/sample/Sample7Task.java
@@ -3,24 +3,26 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Sample7Task {
+
     WebDriver driver;
     String base_url = "https://janisdzalbe.github.io/example-site/examples/actions";
 
-    // method which is being run before each test
     @BeforeEach
     public void startingTests() throws Exception {
-        // Initialize driver
         driver = BootcampUtils.initializeChromeDriver();
-
-        //open page:
         driver.get(base_url);
     }
 
-    // method which is being run after each test
     @AfterEach
     public void endingTests() throws Exception {
         driver.quit();
@@ -28,51 +30,100 @@ public class Sample7Task {
 
     @Test
     public void selectCheckBox() throws Exception {
-//         TODO:
-//          check that none of the checkboxes are ticked
-//          tick  "Option 2"
-//          check that "Option 1" and "Option 3" are not ticked, but "Option 2" is ticked
-//          tick  "Option 3"
-//          click result
-//          check that text 'You selected value(s): Option 2, Option 3' is being displayed
+
+        List<WebElement> checkBoxes =
+                driver.findElements(By.cssSelector(".w3-check[type='checkbox']"));
+
+        for (WebElement cb : checkBoxes) {
+            assertFalse(cb.isSelected());
+        }
+
+        WebElement option2 =
+                driver.findElement(By.cssSelector(".w3-check[value='Option 2'][type='checkbox']"));
+        option2.click();
+
+        WebElement option1 =
+                driver.findElement(By.cssSelector(".w3-check[value='Option 1'][type='checkbox']"));
+        WebElement option3 =
+                driver.findElement(By.cssSelector(".w3-check[value='Option 3'][type='checkbox']"));
+
+        assertFalse(option1.isSelected());
+        assertTrue(option2.isSelected());
+        assertFalse(option3.isSelected());
+
+        option3.click();
+
+        driver.findElement(By.id("result_button_checkbox")).click();
+
+        WebElement result = driver.findElement(By.id("result_checkbox"));
+        assertEquals("You selected value(s): Option 2, Option 3", result.getText());
     }
 
 
     @Test
     public void selectRadioButton() throws Exception {
-//         TODO:
-//          check that none of the radio are selected
-//          select  "Option 3"
-//          check that "Option 1" and "Option 2' are not select, but "Option 3" is selected
-//          select  "Option 1"
-//          check that "Option 2" and "Option 3' are not select, but "Option 1" is selected
-//          click result
-//          check that 'You selected option: Option 1' text is being displayed
-    }
 
-    @Test
-    public void selectOption() throws Exception {
-//         TODO:
+        List<WebElement> radios =
+                driver.findElements(By.cssSelector(".w3-check[type='radio']"));
+
+        for (WebElement r : radios) {
+            assertFalse(r.isSelected());
+        }
+
+        WebElement option3 =
+                driver.findElement(By.cssSelector(".w3-check[value='Option 3'][type='radio']"));
+        option3.click();
+
+        WebElement option1 =
+                driver.findElement(By.cssSelector(".w3-check[value='Option 1'][type='radio']"));
+        WebElement option2 =
+                driver.findElement(By.cssSelector(".w3-check[value='Option 2'][type='radio']"));
+
+        assertFalse(option1.isSelected());
+        assertFalse(option2.isSelected());
+        assertTrue(option3.isSelected());
+
+        option1.click();
+
+        assertTrue(option1.isSelected());
+        assertFalse(option2.isSelected());
+        assertFalse(option3.isSelected());
+
+        driver.findElement(By.id("result_button_radio")).click();
+
+        WebElement result = driver.findElement(By.id("result_radio"));
+        assertEquals("You selected option: Option 1", result.getText());
+    }
+}
+
+
+
+
+
+//    @Test
+//    public void selectOption() throws Exception {
+
 //          select "Option 3" in Select
 //          check that selected option is "Option 3"
 //          select "Option 2" in Select
 //          check that selected option is "Option 2"
 //          click result
 //          check that 'You selected option: Option 2' text is being displayed
-    }
+//    }
 
 // ** Bonus tasks **
-    @Test
-    public void chooseDateViaCalendarBonus() throws Exception {
-//         TODO:
-//          enter date '4 of July 2007' using calendar widget
-//          check that correct date is added
-    }
+//    @Test
+//    public void chooseDateViaCalendarBonus() throws Exception {
+////         TODO:
+////          enter date '4 of July 2007' using calendar widget
+////          check that correct date is added
+//    }
+//
+//    @Test
+//    public void chooseDateViaTextBoxBonus() throws Exception {
+////         TODO:
+////          enter date '2 of May 1959' using text
+////          check that correct date is added
+//    }
 
-    @Test
-    public void chooseDateViaTextBoxBonus() throws Exception {
-//         TODO:
-//          enter date '2 of May 1959' using text
-//          check that correct date is added
-    }
-}
+
