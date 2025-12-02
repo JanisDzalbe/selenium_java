f48fb2ac5429e6a304a39c8e6123d99d0ad3cb0a
Mariss Zakaritis <marissz@proton.me>
2025-12-02 17:59:44 +0200

    Progress of Task2

--- src/test/java/selenium/tasks/Task2.java
+++ src/test/java/selenium/tasks/Task2.java
@@ -3,13 +3,23 @@ package selenium.tasks;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
+import java.time.Duration;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Task2 {
     WebDriver driver;
+    private static WebDriverWait wait;
 
     @BeforeEach
     public void openPage() {
@@ -17,6 +27,8 @@ public class Task2 {
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
         driver = new ChromeDriver();
         driver.get("https://janisdzalbe.github.io/example-site/tasks/list_of_people_with_jobs");
+
+        wait = (WebDriverWait) new WebDriverWait(driver, Duration.ofSeconds(10)).ignoring(StaleElementReferenceException.class);
     }
 
     @AfterEach
@@ -24,30 +36,61 @@ public class Task2 {
         driver.quit();
     }
 
+    private static Map<String, String> getInitialList() {
+        // should convert to List and implement duplicate checks accordingly
+        // as this will not work if the name repeats - key must be unique
+        Map<String, String> initialList = new HashMap<>();
+        initialList.put("Mike", "Web Designer");
+        initialList.put("Jill", "Support");
+        initialList.put("Jane", "Accountant");
+        initialList.put("John", "Software Engineer");
+        initialList.put("Sarah", "Product Manager");
+        initialList.put("Carlos", "Data Analyst");
+        initialList.put("Emily", "UX Designer");
+        initialList.put("David", "Project Manager");
+        initialList.put("Maria", "QA Engineer");
+        initialList.put("Alex", "DevOps Engineer");
+        return initialList;
+    }
+
     @Test
     public void initialPeopleList() throws Exception {
-//         TODO:
+        Map<String, String> initialList = getInitialList();
+
 //          check that "Add person" and "Reset List" buttons are displayed and enabled
+        assertTrue(driver.findElement(By.xpath("//button[text()='Reset List']")).isEnabled());
+        assertTrue(driver.findElement(By.xpath("//button[text()='Add person']")).isEnabled());
+
 //          check list of people contains 10 entries with correct names and jobs
-//        Mike, Web Designer
-//        Jill, Support
-//        Jane, Accountant
-//        John, Software Engineer
-//        Sarah, Product Manager
-//        Carlos, Data Analyst
-//        Emily, UX Designer
-//        David, Project Manager
-//        Maria, QA Engineer
-//        Alex, DevOps Engineer
+        List<WebElement> allPeople = driver.findElements(By.xpath("//li[contains(@id, 'person')]"));
+        assertEquals(initialList.size(), allPeople.size());
+
+        for(WebElement person : allPeople){
+            String name = person.findElement(By.xpath(".//*[contains(@class, 'name')]")).getText();
+            String job = person.findElement(By.xpath(".//*[@class='job']")).getText();
+
+            assertTrue(initialList.containsKey(name));
+            assertEquals(initialList.get(name), job);
+        }
     }
 
     @Test
     public void addNewPerson() throws Exception {
-//         TODO:
 //          click "Add person"
+        driver.findElement(By.xpath("//button[text()='Add person']")).click();
+        wait.until(ExpectedConditions.urlToBe("https://janisdzalbe.github.io/example-site/tasks/enter_a_new_person_with_a_job.html"));
 //          fill "Name" and "Job" fields
+        String name = "Mariss";
+        String job = "Test Engineer";
+        driver.findElement(By.id("name")).sendKeys(name);
+        driver.findElement(By.id("job")).sendKeys(job);
 //          click "Add"
+        driver.findElement(By.xpath("//button[text()='Add']")).click();
 //          check that new person is added to the list with correct name and job
+        wait.until(ExpectedConditions.urlToBe("https://janisdzalbe.github.io/example-site/tasks/list_of_people_with_jobs.html"));
+        WebElement newEntry = driver.findElement(By.xpath("//li[*/text()='"+ name +"']"));
+        assertEquals(name, newEntry.findElement(By.xpath(".//*[contains(@class, 'name')]")).getText());
+        assertEquals(job, newEntry.findElement(By.xpath(".//*[@class='job']")).getText());
     }
 
     @Test
@@ -62,17 +105,64 @@ public class Task2 {
 
     @Test
     public void removeExistingPerson() throws Exception {
-//         TODO:
 //          click cross (x) icon for an existing person
+        Map<String, String> peopleList = getInitialList();
+        String nameToRemove = peopleList.keySet().stream()  // randomly selects a name from the people list
+                .skip(new Random().nextInt(peopleList.size()))
+                .findFirst().get();
+        System.out.println(nameToRemove);
+
+        WebElement entryToDelete = driver.findElement(By.xpath("//li[*/text()='"+ nameToRemove +"']"));
+        entryToDelete.findElement(By.xpath(".//*[contains(text(), '×')]")).click();
 //          check that the person is removed from the list
+        // could just check if findElement throws an NoSuchElementException?
+
+        peopleList.remove(nameToRemove);
+
+        wait.until(ExpectedConditions.stalenessOf(entryToDelete));
+        List<WebElement> allPeople = driver.findElements(By.xpath("//li[contains(@id, 'person')]"));
+        assertEquals(peopleList.size(), allPeople.size());
+
+        for(WebElement person : allPeople){
+            String name = person.findElement(By.xpath(".//*[contains(@class, 'name')]")).getText();
+            String job = person.findElement(By.xpath(".//*[@class='job']")).getText();
+
+            assertTrue(peopleList.containsKey(name));
+            assertEquals(peopleList.get(name), job);
+        }
     }
 
     @Test
     public void resetList() throws Exception {
-//         TODO:
 //          modify the list in any way (add, edit or remove a person)
+        Map<String, String> initialList = getInitialList();
+        String nameToRemove = initialList.keySet().stream()  // randomly selects a name from the people list
+                .skip(new Random().nextInt(initialList.size()))
+                .findFirst().get();
+        System.out.println(nameToRemove);
+
+        WebElement entryToDelete = driver.findElement(By.xpath("//li[*/text()='"+ nameToRemove +"']"));
+        entryToDelete.findElement(By.xpath(".//*[contains(text(), '×')]")).click();
+
 //          check that the list is modified
+        wait.until(ExpectedConditions.stalenessOf(entryToDelete));
+        List<WebElement> allPeople = driver.findElements(By.xpath("//li[contains(@id, 'person')]"));
+        assertNotEquals(initialList.size(), allPeople.size());
+
 //          click "Reset List"
+        driver.findElement(By.xpath("//button[text()='Reset List']")).click();
+
 //          check that the list is back to initial state with 10 original entries
+        wait.until(ExpectedConditions.stalenessOf(allPeople.getFirst()));
+        allPeople = driver.findElements(By.xpath("//li[contains(@id, 'person')]"));
+        assertEquals(initialList.size(), allPeople.size());
+
+        for(WebElement person : allPeople){
+            String name = person.findElement(By.xpath(".//*[contains(@class, 'name')]")).getText();
+            String job = person.findElement(By.xpath(".//*[@class='job']")).getText();
+
+            assertTrue(initialList.containsKey(name));
+            assertEquals(initialList.get(name), job);
+        }
     }
 }
