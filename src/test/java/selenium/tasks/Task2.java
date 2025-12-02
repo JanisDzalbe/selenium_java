75d9560e252c237e806e27a52e3c9242d586b168
Vito-devops <vittallen@gmail.com>
2025-12-02 23:29:25 +0200

    test_Task2_finished

--- src/test/java/selenium/tasks/Task2.java
+++ src/test/java/selenium/tasks/Task2.java
@@ -7,9 +7,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.w3c.dom.ls.LSOutput;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.File;
 
 public class Task2 {
@@ -44,6 +47,29 @@ public class Task2 {
 //        Maria, QA Engineer
 //        Alex, DevOps Engineer
 
+        List<String> namesList = new ArrayList<>();
+        namesList.add("Mike");
+        namesList.add("Jill");
+        namesList.add("Jane");
+        namesList.add("John");
+        namesList.add("Sarah");
+        namesList.add("Carlos");
+        namesList.add("Emily");
+        namesList.add("David");
+        namesList.add("Maria");
+                                                                                namesList.add("Alex");
+        List<String> jobsList = new ArrayList<>();
+        jobsList.add("Web Designer");
+        jobsList.add("Support");
+        jobsList.add("Accountant");
+        jobsList.add("Software Engineer");
+        jobsList.add("Product Manager");
+        jobsList.add("Data Analyst");
+        jobsList.add("UX Designer");
+        jobsList.add("Project Manager");
+        jobsList.add("QA Engineer");
+        jobsList.add("DevOps Engineer");
+
         WebElement buttonAdd = driver.findElement(By.xpath("//*[@id='addPersonBtn' and contains(@class,'w3-btn') and contains(text(),'Add person')]"));
         assertTrue(buttonAdd.isDisplayed());
         assertTrue(buttonAdd.isEnabled());
@@ -53,14 +79,31 @@ public class Task2 {
         assertTrue(buttonResetList.isEnabled());
 
         String url = driver.getCurrentUrl();
+        assertEquals(url, "https://janisdzalbe.github.io/example-site/tasks/list_of_people_with_jobs");
 
+     //   List<WebElement> people = driver.findElements(By.xpath("//*[@id='listOfPeople']"));
+        List<WebElement> allPeople = driver.findElements(By.className("w3-padding-16"));
 
+        String xName = "";
+        String xJob = "";
+        for (int i = 0; i < allPeople.size(); i++) {
+            WebElement person = allPeople.get(i);
+              xName = person.findElement(By.className("w3-xlarge")).getText();
+              xJob = person.findElement(By.className("job")).getText();
 
+              assertEquals(xName, namesList.get(i));
+              assertEquals(xJob, jobsList.get(i));
 
-
-    //      List<WebElement> people = driver.findElements(By.xpath("//*[@id='listOfPeople']"));
-
+//            Just for visual troubleshooting and debugging;
+//            assertEquals(xJob, jobsList.get(i+1));
+//            System.out.println("name: " + i + " -" + xName + "-");
+//            System.out.println("name: " + i + " -" + namesList.get(i) + "-");
+//            System.out.println("job: " + i + " -" + xJob + "-");
+//            System.out.println("job: " + i + " -" + jobsList.get(i) + "-");
+        }
     }
+
+
 
     @Test
     public void addNewPerson() throws Exception {
@@ -69,15 +112,50 @@ public class Task2 {
 //          fill "Name" and "Job" fields
 //          click "Add"
 //          check that new person is added to the list with correct name and job
-        //    System.out.println(driver.getCurrentUrl());
-        WebElement buttonAdd = driver.findElement(By.xpath("//*[@id='addPersonBtn' and contains(@class,'w3-btn') and contains(text(),'Add person')]"));
-        buttonAdd.click();
-        String url = driver.getCurrentUrl();
 
+        List<String> namesList = new ArrayList<>();
+        namesList.add("Mike");
+        namesList.add("Jill");
+        namesList.add("Jane");
+        namesList.add("John");
+        namesList.add("Sarah");
+        namesList.add("Carlos");
+        namesList.add("Emily");
+        namesList.add("David");
+        namesList.add("Maria");
+        namesList.add("Alex");
+
+        List<String> jobsList = new ArrayList<>();
+        jobsList.add("Web Designer");
+        jobsList.add("Support");
+        jobsList.add("Accountant");
+        jobsList.add("Software Engineer");
+        jobsList.add("Product Manager");
+        jobsList.add("Data Analyst");
+        jobsList.add("UX Designer");
+        jobsList.add("Project Manager");
+        jobsList.add("QA Engineer");
+        jobsList.add("DevOps Engineer");
+
+        WebElement buttonAdd = driver.findElement(By.xpath("//*[@id='addPersonBtn' and contains(@class,'w3-btn') and contains(text(),'Add person')]"));
+        assertTrue(buttonAdd.isDisplayed());
+        assertTrue(buttonAdd.isEnabled());
+
+        WebElement buttonResetList = driver.findElement(By.xpath("//*[@id='addPersonBtn' and contains(@class,'w3-btn') and contains(text(),'Reset List')]"));
+        assertTrue(buttonResetList.isDisplayed());
+        assertTrue(buttonResetList.isEnabled());
+
+        String url = driver.getCurrentUrl();
+        assertEquals(url, "https://janisdzalbe.github.io/example-site/tasks/list_of_people_with_jobs");
+        //    System.out.println(driver.getCurrentUrl());
+
+        buttonAdd.click();
+
+        url = driver.getCurrentUrl();
         //      System.out.println(driver.getCurrentUrl());
         assertEquals(url,"https://janisdzalbe.github.io/example-site/tasks/enter_a_new_person_with_a_job.html");
 
-        driver.findElement(By.xpath("//*[@id=\"addPersonBtn\"]")).click();
+       //  driver.findElement(By.xpath("//*[@id=\"addPersonBtn\"]")).click();
 
         WebElement fieldName =  driver.findElement(By.xpath("//*[@id='name']"));
         WebElement fieldJob = driver.findElement(By.xpath("//*[@id='job']"));
@@ -87,6 +165,9 @@ public class Task2 {
 
         String nameKey = "Boris";
         String jobKey = "Team lead";
+
+        namesList.add(nameKey);
+        jobsList.add(jobKey);
 
         fieldName.sendKeys(nameKey);
         fieldJob.sendKeys(jobKey);
@@ -100,22 +181,21 @@ public class Task2 {
 
         assertEquals(url,"https://janisdzalbe.github.io/example-site/tasks/list_of_people_with_jobs.html");
 
+        List<WebElement> allPeople = driver.findElements(By.className("w3-padding-16"));
 
+        String xName = "";
+        String xJob = "";
+        for (int i = 0; i < allPeople.size(); i++) {
+            WebElement person = allPeople.get(i);
+            xName = person.findElement(By.className("w3-xlarge")).getText();
+            xJob = person.findElement(By.className("job")).getText();
 
+            assertEquals(xName, namesList.get(i));
+            assertEquals(xJob, jobsList.get(i));
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        assertEquals("Boris", namesList.get(allPeople.size() - 1));
+        assertEquals("Team lead",jobsList.get(allPeople.size() - 1 ));
     }
 
 
@@ -127,6 +207,54 @@ public class Task2 {
 //          change "Job" field
 //          click "Edit"
 //          check that the person is updated in the list with new job
+        List<WebElement> allPeople = driver.findElements(By.className("w3-padding-16"));
+
+        //Change 3rd person value
+        allPeople.get(2).findElement(By.cssSelector(".fa.fa-pencil")).click();
+
+        String url = "https://janisdzalbe.github.io/example-site/tasks/enter_a_new_person_with_a_job.html?id=2";
+        String currentUrl = driver.getCurrentUrl();
+        assertEquals(url, currentUrl);
+
+        WebElement fieldName =  driver.findElement(By.xpath("//*[@id='name']"));
+        WebElement fieldJob = driver.findElement(By.xpath("//*[@id='job']"));
+
+        assertEquals("Jane", fieldName.getAttribute("value"));
+        assertEquals("Accountant",  fieldJob.getAttribute("value"));
+
+
+        fieldName.clear();
+        fieldJob.clear();
+        assertEquals("", fieldName.getAttribute("value"));
+        assertEquals("",  fieldJob.getAttribute("value"));
+
+        String  nameKey = "Jack";
+        String jobKey = "HR";
+        fieldName.sendKeys(nameKey);
+        fieldJob.sendKeys(jobKey);
+        assertEquals("Jack", fieldName.getAttribute("value"));
+        assertEquals("HR",  fieldJob.getAttribute("value"));
+
+
+        WebElement buttonEdit = driver.findElement(By.xpath("//*[@id='modal_button'  and contains(text(),'Edit')]"));
+        assertTrue(buttonEdit.isDisplayed());
+        assertTrue(buttonEdit.isEnabled());
+
+        buttonEdit.click();
+
+        List<WebElement> updatedPeople = driver.findElements(By.className("w3-padding-16"));
+
+        url = "https://janisdzalbe.github.io/example-site/tasks/list_of_people_with_jobs.html";
+        currentUrl = driver.getCurrentUrl();
+        assertEquals(url, currentUrl);
+
+        WebElement updatedPerson = updatedPeople.get(2);
+        String xName = updatedPerson.findElement(By.xpath("//*[contains(@class,'w3-xlarge') and contains(text(),'Jack')]")).getText();
+        String xJob = updatedPerson.findElement(By.xpath("//*[contains(@class,'job') and contains(text(),'HR')]")).getText();
+
+        assertEquals("Jack", xName);
+        assertEquals("HR", xJob);
+
     }
 
     @Test
@@ -134,6 +262,64 @@ public class Task2 {
 //         TODO:
 //          click cross (x) icon for an existing person
 //          check that the person is removed from the list
+
+        List<String> namesList = new ArrayList<>();
+        namesList.add("Mike");
+        namesList.add("Jill");
+        namesList.add("Jane");
+        namesList.add("John");
+        namesList.add("Sarah");
+        namesList.add("Carlos");
+        namesList.add("Emily");
+        namesList.add("David");
+        namesList.add("Maria");
+        namesList.add("Alex");
+
+        List<String> jobsList = new ArrayList<>();
+        jobsList.add("Web Designer");
+        jobsList.add("Support");
+        jobsList.add("Accountant");
+        jobsList.add("Software Engineer");
+        jobsList.add("Product Manager");
+        jobsList.add("Data Analyst");
+        jobsList.add("UX Designer");
+        jobsList.add("Project Manager");
+        jobsList.add("QA Engineer");
+        jobsList.add("DevOps Engineer");
+
+        List<WebElement> allPeople = driver.findElements(By.className("w3-padding-16"));
+
+        //Remove 2nd person //w3-closebtn × "Jill" "Support"
+        String deleteName = "Jill";
+        String deleteJob = "Support";
+
+        allPeople.get(1).findElement(By.xpath(".//*[contains(@class,'w3-closebtn') and contains(text(),'×')]")).click();
+        jobsList.remove(1);
+
+        String url = "https://janisdzalbe.github.io/example-site/tasks/list_of_people_with_jobs";
+        String currentUrl = driver.getCurrentUrl();
+        assertEquals(url, currentUrl);
+
+        String xName = "";
+        String xJob = "";
+
+        List<WebElement> updatedAllPeople = driver.findElements(By.className("w3-padding-16"));
+
+        //List debug
+//        for (int i = 0; i < updatedAllPeople.size(); i++) {
+//            System.out.println(updatedAllPeople.get(i).getText());
+//        }
+
+        for (int i = 0; i < updatedAllPeople.size(); i++) {
+            WebElement person = updatedAllPeople.get(i);
+            xName = person.findElement(By.className("w3-xlarge")).getText();
+            xJob = person.findElement(By.className("job")).getText();
+
+                assertNotEquals(xName, deleteName);
+                assertNotEquals(xJob, deleteJob);
+        }
+
+
     }
 
     @Test
@@ -143,5 +329,143 @@ public class Task2 {
 //          check that the list is modified
 //          click "Reset List"
 //          check that the list is back to initial state with 10 original entries
+
+        List<String> namesList = new ArrayList<>();
+        namesList.add("Mike");
+        namesList.add("Jill");
+        namesList.add("Jane");
+        namesList.add("John");
+        namesList.add("Sarah");
+        namesList.add("Carlos");
+        namesList.add("Emily");
+        namesList.add("David");
+        namesList.add("Maria");
+        namesList.add("Alex");
+
+        List<String> jobsList = new ArrayList<>();
+        jobsList.add("Web Designer");
+        jobsList.add("Support");
+        jobsList.add("Accountant");
+        jobsList.add("Software Engineer");
+        jobsList.add("Product Manager");
+        jobsList.add("Data Analyst");
+        jobsList.add("UX Designer");
+        jobsList.add("Project Manager");
+        jobsList.add("QA Engineer");
+        jobsList.add("DevOps Engineer");
+
+        WebElement buttonAdd = driver.findElement(By.xpath("//*[@id='addPersonBtn' and contains(@class,'w3-btn') and contains(text(),'Add person')]"));
+        assertTrue(buttonAdd.isDisplayed());
+        assertTrue(buttonAdd.isEnabled());
+
+        WebElement buttonResetList = driver.findElement(By.xpath("//*[@id='addPersonBtn' and contains(@class,'w3-btn') and contains(text(),'Reset List')]"));
+        assertTrue(buttonResetList.isDisplayed());
+        assertTrue(buttonResetList.isEnabled());
+
+        String url = driver.getCurrentUrl();
+        assertEquals(url, "https://janisdzalbe.github.io/example-site/tasks/list_of_people_with_jobs");
+        //    System.out.println(driver.getCurrentUrl());
+
+        buttonAdd.click();
+
+        url = driver.getCurrentUrl();
+        //      System.out.println(driver.getCurrentUrl());
+        assertEquals(url,"https://janisdzalbe.github.io/example-site/tasks/enter_a_new_person_with_a_job.html");
+
+        //  driver.findElement(By.xpath("//*[@id=\"addPersonBtn\"]")).click();
+
+        WebElement fieldName =  driver.findElement(By.xpath("//*[@id='name']"));
+        WebElement fieldJob = driver.findElement(By.xpath("//*[@id='job']"));
+
+        assertEquals("", fieldName.getText());
+        assertEquals("", fieldJob.getText());
+
+        String nameKey = "Boris";
+        String jobKey = "Team lead";
+
+        namesList.add(nameKey);
+        jobsList.add(jobKey);
+
+        fieldName.sendKeys(nameKey);
+        fieldJob.sendKeys(jobKey);
+
+        assertEquals("Boris", fieldName.getAttribute("value"));
+        assertEquals("Team lead",  fieldJob.getAttribute("value"));
+
+        driver.findElement(By.xpath("//*[@id='modal_button' and contains(text(),'Add')]")).click();
+
+        url = driver.getCurrentUrl();
+
+        assertEquals(url,"https://janisdzalbe.github.io/example-site/tasks/list_of_people_with_jobs.html");
+
+        List<WebElement> allPeople = driver.findElements(By.className("w3-padding-16"));
+
+        String xName = "";
+        String xJob = "";
+        for (int i = 0; i < allPeople.size(); i++) {
+            WebElement person = allPeople.get(i);
+            xName = person.findElement(By.className("w3-xlarge")).getText();
+            xJob = person.findElement(By.className("job")).getText();
+
+            assertEquals(xName, namesList.get(i));
+            assertEquals(xJob, jobsList.get(i));
+        }
+
+        assertEquals("Boris", namesList.get(allPeople.size() - 1));
+        assertEquals("Team lead",jobsList.get(allPeople.size() - 1 ));
+
+        allPeople = driver.findElements(By.className("w3-padding-16"));
+        //Remove 11th person // "Boris" "Team lead"
+        String deleteName = "Boris";
+        String deleteJob = "Team lead";
+        allPeople.get(10).findElement(By.xpath(".//*[contains(@class,'w3-closebtn') and contains(text(),'×')]")).click();
+        jobsList.remove(10);
+
+        url = "https://janisdzalbe.github.io/example-site/tasks/list_of_people_with_jobs.html";
+        String currentUrl = driver.getCurrentUrl();
+        assertEquals(url, currentUrl);
+
+         xName = "";
+         xJob = "";
+
+        List<WebElement> updatedAllPeople1 = driver.findElements(By.className("w3-padding-16"));
+
+        for (int i = 0; i < updatedAllPeople1.size(); i++) {
+            WebElement person = updatedAllPeople1.get(i);
+            xName = person.findElement(By.className("w3-xlarge")).getText();
+            xJob = person.findElement(By.className("job")).getText();
+
+            assertNotEquals(xName, deleteName);
+            assertNotEquals(xJob, deleteJob);
+        }
+
+        //Double check
+//        for (int i = 0; i < updatedAllPeople1.size(); i++) {
+//            System.out.println(updatedAllPeople1.get(i).getText());
+//        }
+
+        // click on Button Reset List
+        buttonResetList = driver.findElement(By.xpath("//*[@id='addPersonBtn' and contains(@class,'w3-btn') and contains(text(),'Reset List')]"));
+        assertTrue(buttonResetList.isDisplayed());
+        assertTrue(buttonResetList.isEnabled());
+        buttonResetList.click();
+
+
+        List<WebElement> updatedAllPeople2 = driver.findElements(By.className("w3-padding-16"));
+
+        for (int i = 0; i < updatedAllPeople2.size(); i++) {
+            WebElement person = updatedAllPeople2.get(i);
+            xName = person.findElement(By.className("w3-xlarge")).getText();
+            xJob = person.findElement(By.className("job")).getText();
+
+            assertEquals(xName, namesList.get(i));
+            assertEquals(xJob, jobsList.get(i));
+
+        }
     }
+
+
+
+
 }
+
