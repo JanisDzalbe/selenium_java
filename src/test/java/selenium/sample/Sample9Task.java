2eb73046bb2fc3be7c08c11feb69a0e8b5f620e4
julijakuznecova <cicadoidea@gmail.com>
2025-12-02 15:33:21 +0200

    9 task

--- src/test/java/selenium/sample/Sample9Task.java
+++ src/test/java/selenium/sample/Sample9Task.java
@@ -3,8 +3,17 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import selenium.utility.BootcampUtils;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Sample9Task {
     WebDriver driver;
@@ -25,6 +34,18 @@ public class Sample9Task {
 
     @Test
     public void loadGreenSleep() throws Exception {
+
+
+            driver.findElement(By.id("start_green")).click();
+            assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+            assertTrue(driver.findElement(By.id("loading_green")).isDisplayed());
+
+            Thread.sleep(6000);
+
+            assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+            assertFalse(driver.findElement(By.id("loading_green")).isDisplayed());
+            assertTrue(driver.findElement(By.id("finish_green")).isDisplayed());
+
 //         TODO:
 //          * 1) click on start loading green button
 //          * 2) check that button does not appear,
@@ -36,6 +57,18 @@ public class Sample9Task {
 
     @Test
     public void loadGreenImplicit() throws Exception {
+
+
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+            driver.findElement(By.id("start_green")).click();
+            assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+            assertTrue(driver.findElement(By.id("loading_green")).isDisplayed());
+
+            assertTrue(driver.findElement(By.id("finish_green")).isDisplayed());
+            assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+            assertFalse(driver.findElement(By.id("loading_green")).isDisplayed());
+        }
 //         TODO:
 //          * 1) click on start loading green button
 //          * 2) check that button does not appear,
@@ -43,10 +76,23 @@ public class Sample9Task {
 //          * 3) check that both button
 //          * and loading text is not seen,
 //          * success is seen instead "Green Loaded"
-    }
+
 
     @Test
     public void loadGreenExplicitWait() throws Exception {
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+            driver.findElement(By.id("start_green")).click();
+            assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+            assertTrue(driver.findElement(By.id("loading_green")).isDisplayed());
+
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.id("finish_green")));
+
+            assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+            assertFalse(driver.findElement(By.id("loading_green")).isDisplayed());
+            assertTrue(driver.findElement(By.id("finish_green")).isDisplayed());
+        }
 //         TODO:
 //          * 1) click on start loading green button
 //          * 2) check that button does not appear,
@@ -56,16 +102,6 @@ public class Sample9Task {
 //          * success is seen instead "Green Loaded"
     }
 
-    @Test
-    public void loadGreenAndBlueBonus() {
-//         TODO:
-//          * 0) wait until button to load green and blue appears
-//          * 1) click on start loading green and blue button
-//          * 2) check that button does not appear, but loading text is seen instead for green
-//          * 3) check that button does not appear, but loading text is seen instead for green and blue
-//          * 4) check that button and loading green does not appear,
-//          * but loading text is seen instead for blue and success for green is seen
-//          * 5) check that both button and loading text is not seen, success is seen instead
-    }
 
-}
+
+
