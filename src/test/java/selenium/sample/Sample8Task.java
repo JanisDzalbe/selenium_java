fa162dc6f816f2eb0bf89677cabdad5015de8b98
ViktorijaHolina <viktorija.holina@gmail.com>
2025-12-02 12:49:37 +0200

    Task8

--- src/test/java/selenium/sample/Sample8Task.java
+++ src/test/java/selenium/sample/Sample8Task.java
@@ -3,8 +3,19 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class Sample8Task {
     WebDriver driver;
@@ -27,6 +38,12 @@ public class Sample8Task {
 
     @Test
     public void findPersonByXPath() throws Exception {
+            driver.findElement(By.id("resetListBtn")).click();
+            WebElement john = driver.findElement(By.xpath("//*[@class=\"w3-padding-16\" and .//*[text()='John']]"));
+            assertEquals("Software Engineer",
+                    john.findElement(By.xpath(".//*[@class=\"job\"]")).getText());
+            assertEquals("Software Engineer",
+                    john.findElement(By.className("job")).getText());
 //  TODO:
 //   Click “Reset List”
 //   Using xPath, find “John” and asset that he is “Software Engineer”
@@ -34,6 +51,13 @@ public class Sample8Task {
 
     @Test
     public void findPersonWithLoop() throws Exception {
+        driver.findElement(By.id("shuffleBtn")).click();
+            List<WebElement> allPersons = driver.findElements(By.className("w3-padding-16"));
+            for (WebElement person : allPersons) {
+                if (person.findElement(By.className("name")).getText().equals("Jane")) {
+                    assertEquals("Accountant", person.findElement(By.className("job")).getText());
+                }
+            }
 //  TODO:
 //   Click “Shuffle Order”
 //   Using a loop, Find “Jane” and assert that she is “Accountant”
