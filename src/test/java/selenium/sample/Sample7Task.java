7f4f414a1ee83dbe21f6e6a255e41f358db76b71
xDoots <gladiatus2001@inbox.lv>
2025-12-02 11:41:31 +0200

    Complete Sample7

--- src/test/java/selenium/sample/Sample7Task.java
+++ src/test/java/selenium/sample/Sample7Task.java
@@ -3,8 +3,18 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import selenium.utility.BootcampUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample7Task {
     WebDriver driver;
@@ -35,6 +45,21 @@ public class Sample7Task {
 //          tick  "Option 3"
 //          click result
 //          check that text 'You selected value(s): Option 2, Option 3' is being displayed
+        List<WebElement> checkBoxes = driver.findElements(By.cssSelector(".w3-check[type='checkbox']"));
+        WebElement option1 = driver.findElement(By.cssSelector(".w3-check[value='Option 1'][type='checkbox'"));
+        WebElement option2 = driver.findElement(By.cssSelector(".w3-check[value='Option 2'][type='checkbox'"));
+        WebElement option3 = driver.findElement(By.cssSelector(".w3-check[value='Option 3'][type='checkbox'"));
+
+        for (WebElement checkBox : checkBoxes) {
+            assertFalse(checkBox.isSelected());
+        }
+        option2.click();
+        assertFalse(option1.isSelected());
+        assertTrue(option2.isSelected());
+        assertFalse(option3.isSelected());
+        option3.click();
+        driver.findElement(By.cssSelector("#result_button_checkbox")).click();
+        assertTrue(driver.findElement(By.cssSelector("#result_checkbox")).isDisplayed());
     }
 
 
@@ -48,6 +73,24 @@ public class Sample7Task {
 //          check that "Option 2" and "Option 3' are not select, but "Option 1" is selected
 //          click result
 //          check that 'You selected option: Option 1' text is being displayed
+        List<WebElement> radioButtons = driver.findElements(By.cssSelector(".w3-check[type='radio']"));
+        WebElement option1 = driver.findElement(By.cssSelector(".w3-check[value='Option 1'][type='radio'"));
+        WebElement option2 = driver.findElement(By.cssSelector(".w3-check[value='Option 2'][type='radio'"));
+        WebElement option3 = driver.findElement(By.cssSelector(".w3-check[value='Option 3'][type='radio'"));
+
+        for (WebElement radioButton : radioButtons) {
+            assertFalse(radioButton.isSelected());
+        }
+        option3.click();
+        assertFalse(option1.isSelected());
+        assertFalse(option2.isSelected());
+        assertTrue(option3.isSelected());
+        option1.click();
+        assertTrue(option1.isSelected());
+        assertFalse(option2.isSelected());
+        assertFalse(option3.isSelected());
+        driver.findElement(By.cssSelector("#result_button_ratio")).click();
+        assertTrue(driver.findElement(By.cssSelector("#result_radio")).isDisplayed());
     }
 
     @Test
@@ -59,6 +102,13 @@ public class Sample7Task {
 //          check that selected option is "Option 2"
 //          click result
 //          check that 'You selected option: Option 2' text is being displayed
+        Select dropdown = new Select(driver.findElement(By.id("vfb-12")));
+        dropdown.selectByVisibleText("Option 3");
+        assertEquals("Option 3", dropdown.getFirstSelectedOption().getText());
+        dropdown.selectByIndex(2);
+        assertEquals("Option 2", dropdown.getFirstSelectedOption().getText());
+        driver.findElement(By.cssSelector("#result_button_select")).click();
+        assertTrue(driver.findElement(By.cssSelector("#result_select")).isDisplayed());
     }
 
 // ** Bonus tasks **
@@ -67,6 +117,28 @@ public class Sample7Task {
 //         TODO:
 //          enter date '4 of July 2007' using calendar widget
 //          check that correct date is added
+        Calendar cal = Calendar.getInstance();
+        // 18 * 12 + 5 = 221  - this is the amount of month difference, used in for loop
+        int monthDif = Math.abs(2007 - cal.get(Calendar.YEAR)) * 12 + Math.abs(6 - cal.get(Calendar.MONTH));
+
+        // month index starts from 0
+        cal.add(Calendar.YEAR, 2007 - cal.get(Calendar.YEAR));
+        cal.add(Calendar.MONTH, 6 - cal.get(Calendar.MONTH));
+        String result = new SimpleDateFormat("MM/04/yyyy").format(cal.getTime());
+
+        WebElement dateBox = driver.findElement(By.id("vfb-8"));
+        assertEquals("", dateBox.getDomProperty("value"));
+
+        dateBox.click();
+        WebElement dateWidget = driver.findElement(By.id("ui-datepicker-div"));
+
+        for (int i = 0; i < monthDif; i++) {
+            dateWidget.findElement(By.className("ui-datepicker-prev")).click();
+        }
+        dateWidget.findElement(By.xpath("//a[text()='4']")).click();
+
+        assertEquals(result, dateBox.getDomProperty("value"));
+        dateBox.clear();
     }
 
     @Test
@@ -74,5 +146,13 @@ public class Sample7Task {
 //         TODO:
 //          enter date '2 of May 1959' using text
 //          check that correct date is added
+        String dateToEnter = "05/02/1959";
+
+        WebElement dateBox = driver.findElement(By.id("vfb-8"));
+        assertEquals("", dateBox.getDomProperty("value"));
+
+        dateBox.clear();
+        dateBox.sendKeys(dateToEnter);
+        assertEquals(dateToEnter, dateBox.getDomProperty("value"));
     }
 }
