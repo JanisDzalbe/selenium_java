d17dd730bb98be45d2a4c7566ad0911dca6107f4
Tatjana <4mistralia@gmail.com>
2025-12-02 10:38:56 +0200

    Task 6

--- src/test/java/selenium/sample/Sample6Task.java
+++ src/test/java/selenium/sample/Sample6Task.java
@@ -3,8 +3,11 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample6Task {
     WebDriver driver;
@@ -35,6 +38,18 @@ public class Sample6Task {
 //          1-2 ways to find text: "Test Text 4"
 //          1-2 ways to find text: "Test Text 5"
 //          1-2 ways to find text: "This is also a button"
+
+        assertEquals("Heading 2 text", driver.findElement(By.xpath("//*[@id=\"heading_2\"]")).getText());
+        assertEquals("Heading 2 text", driver.findElement(By.xpath("//h2[contains(text(), \"2\")]")).getText());
+
+        assertEquals("Test Text 1", driver.findElement(By.xpath("//*[@id=\"test1\"]/*[@class=\"test\"]")).getText());
+        assertEquals("Test Text 2", driver.findElement(By.xpath("//p[@class='twoTest']")).getText());
+        assertEquals("Test Text 3", driver.findElement(By.xpath("//*[@id=\"test3\"]/*[1]")).getText());
+        assertEquals("Test Text 4", driver.findElement(By.xpath("//*[@id=\"test3\"]/*[text()='Test Text 4']")).getText());
+        assertEquals("Test Text 5", driver.findElement(By.xpath("//*[@class='Test']")).getText());
+
+        assertEquals("This is also a button", driver.findElement(By.xpath("//*[@id='buttonId']")).getDomAttribute("value"));
+        assertEquals("This is also a button", driver.findElement(By.xpath("//input[@name='randomButton2']")).getDomAttribute("value"));
     }
 
     @Test
@@ -45,5 +60,15 @@ public class Sample6Task {
 //          1-2 ways to find text: "Test Text 2"
 //          1-2 ways to find text: "Test Text 3"
 //          1-2 ways to find text: "This is also a button"
+
+        assertEquals("Heading 2 text", driver.findElement(By.cssSelector("#heading_2")).getText());
+        assertEquals("Heading 2 text", driver.findElement(By.cssSelector("h2:nth-of-type(2)")).getText());
+
+        assertEquals("Test Text 1", driver.findElement(By.cssSelector("#test1 .test")).getText());
+        assertEquals("Test Text 2", driver.findElement(By.cssSelector(".twoTest")).getText());
+        assertEquals("Test Text 3", driver.findElement(By.cssSelector("#test3 .test:nth-of-type(1)")).getText());
+
+        assertEquals("This is also a button", driver.findElement(By.cssSelector("#buttonId")).getDomAttribute("value"));
+        assertEquals("This is also a button", driver.findElement(By.cssSelector("input[name='randomButton2']")).getDomAttribute("value"));
     }
 }
