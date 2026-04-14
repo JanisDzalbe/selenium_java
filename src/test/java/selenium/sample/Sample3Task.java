4419475a906cfd274304595aac7b4424fa1313b7
Andrejs Ivanovs <ivanovs.andrejs@inbox.lv>
2026-04-14 23:51:31 +0300

    All tasks from day 1

--- src/test/java/selenium/sample/Sample3Task.java
+++ src/test/java/selenium/sample/Sample3Task.java
@@ -3,8 +3,14 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample3Task {
     WebDriver driver;
@@ -30,6 +36,10 @@ public class Sample3Task {
 //         TODO:
 //          check how many element with class "test" there are on page (5)
 //          check that value of second button is "This is also a button"
+        int expected = 5;
+        int actual = driver.findElements(By.className("test")).size();
+        assertEquals(expected, actual);
+        assertEquals("This is also a button", driver.findElement(By.id("buttonId")).getDomAttribute("value"));
     }
 
     @Test
@@ -38,12 +48,16 @@ public class Sample3Task {
 //          check that it is True that value of second button is
 //          "this is Also a Button" if you ignore Caps Locks
 //          fail with custom error message:
+        String expected = "this is Also a Button";
+        String actual = driver.findElement(By.name("randomButton2")).getDomAttribute("value");
+        assertTrue(expected.equalsIgnoreCase(actual), "Custom error message");
     }
 
     @Test
     public void assertFalseTask() throws Exception {
 //         TODO:
 //          check that it is False that value of second button is "This is a button"
+        assertFalse(driver.findElement(By.id("buttonId")).getDomAttribute("value").equals("This is a button"));
     }
 
     @Test
@@ -51,5 +65,11 @@ public class Sample3Task {
 //        TODO:
 //         check that none of items with class "test"
 //         contain number 190
+        List<WebElement> elements = driver.findElements(By.className("test"));
+        for (WebElement e : elements) {
+            if (e.getText().contains("190")) {
+                fail("contain number 190");
+            }
+        }
     }
 }
--- src/test/java/selenium/tasks/Task1.java
+++ src/test/java/selenium/tasks/Task1.java
@@ -3,10 +3,17 @@ package selenium.tasks;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
+import java.text.DecimalFormat;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Task1 {
     WebDriver driver;
@@ -29,18 +36,40 @@ public class Task1 {
     public void errorOnText() {
 //        TODO
 //         enter a text instead of a number, check that correct error is shown
+        WebElement inputNumber =driver.findElement(By.id("numb"));
+        WebElement submitButton = driver.findElement(By.className("w3-btn"));
+        WebElement errorText = driver.findElement(By.id("ch1_error"));
+        inputNumber.sendKeys("abc");
+        submitButton.click();
+        assertTrue(errorText.isDisplayed());
+        assertEquals("Please enter a number", errorText.getText());
+
     }
 
     @Test
     public void errorOnNumberTooSmall() {
 //        TODO
 //         enter number which is too small (positive number below 50), check that correct error is shown
+        WebElement inputNumber =driver.findElement(By.id("numb"));
+        WebElement submitButton = driver.findElement(By.className("w3-btn"));
+        WebElement errorText = driver.findElement(By.id("ch1_error"));
+        inputNumber.sendKeys("33");
+        submitButton.click();
+        assertTrue(errorText.isDisplayed());
+        assertEquals("Number is too small", errorText.getText());
     }
 
     @Test
     public void errorOnNumberTooBig() {
 //        TODO
 //         enter number which is too big (above 100), check that correct error is shown
+        WebElement inputNumber =driver.findElement(By.id("numb"));
+        WebElement submitButton = driver.findElement(By.className("w3-btn"));
+        WebElement errorText = driver.findElement(By.id("ch1_error"));
+        inputNumber.sendKeys("300");
+        submitButton.click();
+        assertTrue(errorText.isDisplayed());
+        assertEquals("Number is too big", errorText.getText());
     }
 
     @Test
@@ -49,5 +78,15 @@ public class Task1 {
 //         enter a number between 50 and 100 digit in the input, then press submit
 //         and check that no error is shown and that square root is calculated correctly
 //         NOTE: input and assertion values have to be defined as variables
+        int inputText = 64;
+        WebElement inputNumber =driver.findElement(By.id("numb"));
+        WebElement submitButton = driver.findElement(By.className("w3-btn"));
+        inputNumber.sendKeys(String.valueOf(inputText));
+        submitButton.click();
+        Alert alert = driver.switchTo().alert();
+        double squareText = Math.sqrt(inputText);
+        DecimalFormat df = new DecimalFormat("#.00");
+        String expectedAlertText = "Square root of " + inputText + " is " + df.format(squareText);
+        assertEquals(expectedAlertText, alert.getText());
     }
 }
