8d4e3dae11ee3a7c31d7f76939ad2223cb6d5142
Sonakshi <sonakshigupta.135@gmail.com>
2026-04-14 13:31:34 +0300

    Sample 1 Task

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -2,8 +2,13 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import selenium.utility.BootcampUtils;
 
 public class Sample1Task {
+
+
 
     @Test
     public void goToHomepage() throws Exception {
@@ -13,5 +18,18 @@ public class Sample1Task {
 //         get title of page
 //         get URL of current page
 //         close browser
+
+        WebDriver driver = BootcampUtils.initializeChromeDriver();
+        driver.get("https://janisdzalbe.github.io/example-site/");
+        String title= driver.getTitle();
+        String url = driver.getCurrentUrl();
+
+        System.out.println(title);
+        System.out.println(url);
+
+        Thread.sleep(2000);
+
+        // Close browser
+        driver.quit();
     }
 }
