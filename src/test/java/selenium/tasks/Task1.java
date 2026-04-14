0fb090208035d0d9ec5efcf6f37f741d8c9b34fe
Meganath Nandhakumar <meganathrtu@gmail.com>
2026-04-14 16:40:12 +0300

    Task1

--- src/test/java/selenium/tasks/Task1.java
+++ src/test/java/selenium/tasks/Task1.java
@@ -3,17 +3,22 @@ package selenium.tasks;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Task1 {
+
     WebDriver driver;
 
     @BeforeEach
     public void openPage() {
-
         String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
         driver = new ChromeDriver();
@@ -27,27 +32,55 @@ public class Task1 {
 
     @Test
     public void errorOnText() {
-//        TODO
-//         enter a text instead of a number, check that correct error is shown
+        WebElement input = driver.findElement(By.tagName("input"));
+        input.sendKeys("abc");
+        driver.findElement(By.tagName("button")).click();
+
+        WebElement error = driver.findElement(By.className("error"));
+        assertTrue(error.isDisplayed(), "Error message should be visible");
+        assertFalse(error.getText().isEmpty(), "Error message should not be empty");
     }
 
     @Test
     public void errorOnNumberTooSmall() {
-//        TODO
-//         enter number which is too small (positive number below 50), check that correct error is shown
+        WebElement input = driver.findElement(By.tagName("input"));
+        input.sendKeys("10");
+        driver.findElement(By.tagName("button")).click();
+
+        WebElement error = driver.findElement(By.className("error"));
+        assertTrue(error.isDisplayed(), "Error message should be visible for number too small");
+        assertFalse(error.getText().isEmpty(), "Error message should not be empty");
     }
 
     @Test
     public void errorOnNumberTooBig() {
-//        TODO
-//         enter number which is too big (above 100), check that correct error is shown
+        WebElement input = driver.findElement(By.tagName("input"));
+        input.sendKeys("200");
+        driver.findElement(By.tagName("button")).click();
+
+        WebElement error = driver.findElement(By.className("error"));
+        assertTrue(error.isDisplayed(), "Error message should be visible for number too big");
+        assertFalse(error.getText().isEmpty(), "Error message should not be empty");
     }
 
     @Test
     public void correctSquareRoot() {
-//        TODO
-//         enter a number between 50 and 100 digit in the input, then press submit
-//         and check that no error is shown and that square root is calculated correctly
-//         NOTE: input and assertion values have to be defined as variables
+        int inputNumber = 64;
+        double expectedSquareRoot = Math.round(Math.sqrt(inputNumber) * 100.0) / 100.0;
+        // For 64, expectedSquareRoot = 8.0, displayed as "8.00"
+        String expectedText = String.format("Square root of %d is %.2f", inputNumber, expectedSquareRoot);
+
+        WebElement input = driver.findElement(By.tagName("input"));
+        input.sendKeys(String.valueOf(inputNumber));
+        driver.findElement(By.tagName("button")).click();
+
+
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+
+        assertEquals(expectedText, alertText,
+                "Alert should show the correct square root result");
+
+        alert.accept(); // dismiss the alert
     }
 }
