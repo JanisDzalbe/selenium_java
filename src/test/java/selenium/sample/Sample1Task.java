82a2ccd13343cc782ef16d35c78395ffae17a4ec
Meganath Nandhakumar <meganathrtu@gmail.com>
2026-04-14 10:55:04 +0300

    Sample1Task

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -1,17 +1,38 @@
 package selenium.sample;
 
-
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
 
 public class Sample1Task {
 
+    static String libWithDriversLocation = System.getProperty("user.dir")
+            + File.separator + "lib" + File.separator;
+
     @Test
     public void goToHomepage() throws Exception {
-//        TODO:
-//         define driver
-//         go to https://janisdzalbe.github.io/example-site/index2.html
-//         get title of page
-//         get URL of current page
-//         close browser
+        // set path to chromedriver
+        System.setProperty("webdriver.chrome.driver",
+                libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
+
+        // define driver
+        WebDriver driver = new ChromeDriver();
+
+        // go to page
+        driver.get("https://janisdzalbe.github.io/example-site/index2.html");
+
+        // get title of page
+        System.out.println(driver.getTitle());
+
+        // get URL of current page
+        System.out.println(driver.getCurrentUrl());
+
+        // optional wait (just like in sample)
+        Thread.sleep(5000);
+
+        // close browser
+        driver.quit();
     }
 }
