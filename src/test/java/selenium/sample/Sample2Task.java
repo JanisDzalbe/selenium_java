09d92601c4ffa2f480bec5c5149ee28ff169f5be
Sonakshi <sonakshigupta.135@gmail.com>
2026-04-14 15:20:42 +0300

    Commit sample task

--- src/test/java/selenium/sample/Sample2Task.java
+++ src/test/java/selenium/sample/Sample2Task.java
@@ -3,8 +3,14 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample2Task {
     WebDriver driver;
@@ -29,18 +35,28 @@ public class Sample2Task {
     public void findElementByID() throws Exception {
 //         TODO:
 //          get text "Heading 2 text" using id
+        String text = driver.findElement(By.id("heading2")).getText();
+        System.out.println(text);
     }
 
     @Test
     public void findElementByName() throws Exception {
 //         TODO:
 //          get attribute "id" and "value" of button "This is also a button" using name
+        WebElement button = driver.findElement(By.name("randomButton2"));
+        String id = button.getAttribute("id");
+        String value = button.getAttribute("value");
+
+        System.out.println(id);
+        System.out.println(value);
     }
 
     @Test
     public void findElementByClassFirst() throws Exception {
 //         TODO:
 //          get first text of class "test" (should be "Test Text 1")
+        String text = driver.findElement(By.className("test")).getText();
+        System.out.println(text);
     }
 
     @Test
@@ -49,5 +65,16 @@ public class Sample2Task {
 //          get size text of class "test" (should be 5)
 //          get text of class "test"
 //          get third text of class "test" (should be "Test Text 4")
+        List<WebElement> elements = driver.findElements(By.className("test"));
+        int size = elements.size();
+        System.out.println("Size: " + size);
+        assertEquals(5, size);
+
+        for (WebElement el : elements) {
+            System.out.println(el.getText());
+        }
+        String thirdText = elements.get(2).getText();
+        System.out.println("Third: " + thirdText);
+        assertEquals("Test Text 4", thirdText);
     }
 }
--- src/test/java/selenium/sample/Sample3Task.java
+++ src/test/java/selenium/sample/Sample3Task.java
@@ -3,8 +3,14 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample3Task {
     WebDriver driver;
@@ -30,6 +36,10 @@ public class Sample3Task {
 //         TODO:
 //          check how many element with class "test" there are on page (5)
 //          check that value of second button is "This is also a button"
+         int count= driver.findElements(By.className("test")).size();
+        assertEquals(5, count);
+          String text =  driver.findElement(By.id("buttonId")).getAttribute("value");
+        assertEquals("This is also a button", text);
     }
 
     @Test
@@ -38,12 +48,23 @@ public class Sample3Task {
 //          check that it is True that value of second button is
 //          "this is Also a Button" if you ignore Caps Locks
 //          fail with custom error message:
+        String text = driver.findElement(By.id("buttonId")).getAttribute("value");
+        assertTrue(
+                text.equalsIgnoreCase("this is also a button"),
+                "Button text is not correct!"
+        );
     }
 
     @Test
     public void assertFalseTask() throws Exception {
 //         TODO:
 //          check that it is False that value of second button is "This is a button"
+        String text = driver.findElement(By.id("buttonId")).getAttribute("value");
+
+        assertFalse(
+                text.equals("This is a button"),
+                "Second button should NOT be 'This is a button'"
+        );
     }
 
     @Test
@@ -51,5 +72,11 @@ public class Sample3Task {
 //        TODO:
 //         check that none of items with class "test"
 //         contain number 190
+        Thread.sleep(1000);
+        driver.findElements(By.className("test")).forEach(e-> System.out.println(e.getText()));
+       java.util.Optional<WebElement> element = driver.findElements(By.className("test")).stream().filter(e->
+                e.getText().contains("190")).findAny();
+       assertTrue(element.isEmpty());
+
     }
 }
