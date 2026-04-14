88476cb406f17605986b69b70b7969e891420608
Ilmārs Cīrulis <6750944+IlmarsCirulis@users.noreply.github.com>
2026-04-14 19:25:02 +0300

    Update extra3Task.java
    Partial progress, but not sure and will ask questions

--- src/test/java/selenium/sample/extra/extra3Task.java
+++ src/test/java/selenium/sample/extra/extra3Task.java
@@ -3,8 +3,15 @@ package selenium.sample.extra;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+import org.openqa.selenium.support.Color;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class extra3Task {
     WebDriver driver;
@@ -27,9 +34,19 @@ public class extra3Task {
 
     @Test
     public void styleChecks() throws Exception {
+        WebElement header = driver.findElement(By.id("header"));
+        WebElement underHeader = driver.findElement(By.xpath("//div[h1]"));
+        WebElement h1 = driver.findElement(By.tagName("h1"));
+        Color black = Color.fromString("rgba(0, 0, 0, 0)");
+        Color lightGray = Color.fromString("#f1f1f1");
+
+        assertEquals(black, Color.fromString(header.getCssValue("background-color")));
+        assertEquals(lightGray, Color.fromString(underHeader.getCssValue("background-color")));
+        assertEquals("64px", h1.getCssValue("font-size"));
+
 //       TODO:
 //        check the background of top 2 sections
-//        rgba(255, 221, 221, 1);
+//        rgba(255, 221, 221, 1);  ???? don't understand this part
 //        check h1 element font-size 64px
     }
 }
