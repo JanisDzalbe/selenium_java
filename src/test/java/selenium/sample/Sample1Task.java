d32b3ccaf130efd838a45fcf7fe44be2e3393171
Ilmārs Cīrulis <6750944+IlmarsCirulis@users.noreply.github.com>
2026-04-14 11:13:09 +0300

    Added System.setProperty line

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static selenium.sample.Sample1.libWithDriversLocation;
+
 public class Sample1Task {
 
     @Test
     public void goToHomepage() throws Exception {
 
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
         WebDriver driver = new ChromeDriver();
         driver.get("https://janisdzalbe.github.io/example-site/index2.html");
         System.out.println(driver.getTitle());
