f1b3caa0065a0e4b1b8cc142d9e9400831d57d11
oleksandr_shu <shushin2000@gmail.com>
2026-04-14 10:59:42 +0300

    done the Sample1Task file

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -2,6 +2,11 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import selenium.utility.BootcampUtils;
+
 
 public class Sample1Task {
 
@@ -13,5 +18,11 @@ public class Sample1Task {
 //         get title of page
 //         get URL of current page
 //         close browser
+            WebDriver driver;
+            driver = BootcampUtils.initializeChromeDriver();
+            driver.get("https://janisdzalbe.github.io/example-site/index2.html");
+            System.out.println(driver.getTitle());
+            System.out.println(driver.getCurrentUrl());
+            driver.quit();
     }
 }
