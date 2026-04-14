abcb35e29e239073182f7126f219fb0903f7b979
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2026-04-14 14:22:47 +0300

    feat: implement assertions for button values and element count in Sample3Task

--- src/test/java/selenium/sample/Sample3Task.java
+++ src/test/java/selenium/sample/Sample3Task.java
@@ -3,8 +3,14 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample3Task {
     WebDriver driver;
@@ -27,29 +33,39 @@ public class Sample3Task {
 
     @Test
     public void assertEqualsTask() throws Exception {
-//         TODO:
-//          check how many element with class "test" there are on page (5)
-//          check that value of second button is "This is also a button"
+        int expectedResultForTestsClasses = 5;
+        int actualResultForTestsClasses = driver.findElements(By.className("test")).size();
+
+        String expectedResultForButton = "This is also a button";
+        String actualResultForButton = driver.findElement(By.name("randomButton2")).getDomAttribute("value");
+
+        assertEquals(expectedResultForTestsClasses, actualResultForTestsClasses);
+        assertEquals(expectedResultForButton, actualResultForButton);
     }
 
     @Test
     public void assertTrueTask() throws Exception {
-//         TODO:
-//          check that it is True that value of second button is
-//          "this is Also a Button" if you ignore Caps Locks
-//          fail with custom error message:
+        String customExpectedResult = "this is Also a Button";
+        String elementTextOnPage = driver.findElement(By.name("randomButton2")).getDomAttribute("value");
+        assertTrue(elementTextOnPage.equalsIgnoreCase(customExpectedResult), "There is a mistake here btw");
     }
 
     @Test
     public void assertFalseTask() throws Exception {
-//         TODO:
-//          check that it is False that value of second button is "This is a button"
+        String customExpectedResult = "This is a Button";
+        String elementTextOnPage = driver.findElement(By.name("randomButton2")).getDomAttribute("value");
+        assertFalse(elementTextOnPage.equals(customExpectedResult), "There is a mistake here btw");
+        assertNotEquals(customExpectedResult, elementTextOnPage, "There is a mistake here btw");
     }
 
     @Test
     public void failTask() throws Exception {
-//        TODO:
-//         check that none of items with class "test"
-//         contain number 190
+        List<WebElement> webElements = driver.findElements(By.className("test"));
+        String numberNotToBeContained = "190";
+        for (WebElement webElement : webElements) {
+            if (webElement.getText().contains("190")) {
+                fail(); 
+            }
+        }
     }
 }
