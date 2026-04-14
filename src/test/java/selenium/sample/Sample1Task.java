1ed093a7601ea7fbc485534a77a64863e8ab9f7e
naujokasnikita-ctrl <naujokasnikita@gmail.com>
2026-04-14 11:16:06 +0300

    updated config

--- src/test/java/selenium/sample/Sample1Task.java
+++ src/test/java/selenium/sample/Sample1Task.java
@@ -1,12 +1,23 @@
 package selenium.sample;
-
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 public class Sample1Task {
+    static String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
 
     @Test
     public void goToHomepage() throws Exception {
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://github.com/naujokasnikita-ctrl/java-course");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
+//
 //        TODO:
 //         define driver
 //         go to https://janisdzalbe.github.io/example-site/index2.html
