c6955254e6086cdb0e54039cc224f3bc9ecdb8c5
Meganath Nandhakumar <meganathrtu@gmail.com>
2026-04-14 15:58:55 +0300

    Sample5Task

--- src/test/java/selenium/sample/Sample5Task.java
+++ src/test/java/selenium/sample/Sample5Task.java
@@ -3,23 +3,22 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample5Task {
     WebDriver driver;
 
-    // method which is being run before each test
     @BeforeEach
     public void startingTests() throws Exception {
-        // Initialize driver
         driver = BootcampUtils.initializeChromeDriver();
-
-        //open page:
         driver.get("https://janisdzalbe.github.io/example-site/examples/alerts_popups");
     }
 
-    // method which is being run after each test
     @AfterEach
     public void endingTests() throws Exception {
         driver.quit();
@@ -27,22 +26,34 @@ public class Sample5Task {
 
     @Test
     public void goToAlertedPageViaButton() throws Exception {
-//         TODO:
-//          click on "To go to alerted page press Ok. Or stay here" button
-//          switch to alert
-//          click ok
-//          switch to second alert
-//          verify alert text
-//          click ok on second alert
-//          verify that the correct page is opened
+        // Click on "To go to alerted page press Ok. Or stay here" button
+        driver.findElement(By.className("w3-blue")).click();
+
+        // Switch to first alert and click OK
+        Alert alert1 = driver.switchTo().alert();
+        alert1.accept();
+
+        // Switch to second alert and verify its text
+        Alert alert2 = driver.switchTo().alert();
+        assertEquals("Booooooooo!", alert2.getText());
+
+        // Click OK on second alert
+        alert2.accept();
+
+        // Verify that the correct page is opened
+        assertEquals("This page is alerted", driver.findElement(By.id("heading")).getText());
     }
 
     @Test
     public void doNotGoToAlertedPageViaButton() throws Exception {
-//         TODO:
-//          click on "To go to alerted page press Ok. Or stay here" button
-//          switch to alert
-//          click cancel
-//          verify the text on page
+        // Click on "To go to alerted page press Ok. Or stay here" button
+        driver.findElement(By.className("w3-blue")).click();
+
+        // Switch to alert and click Cancel
+        Alert alert = driver.switchTo().alert();
+        alert.dismiss();
+
+        // Verify the text on page (note: typo "desided" is intentional, it's what the page shows)
+        assertEquals("So you desided to say? Good!", driver.findElement(By.id("textForAlerts")).getText());
     }
 }
