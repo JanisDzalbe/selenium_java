e4bce348357fae80a5dc1cf8aace3e5e70fba48a
naujokasnikita-ctrl <naujokasnikita@gmail.com>
2026-04-14 22:35:52 +0300

    sampel1-5task_and_task1

--- src/test/java/selenium/tasks/Task1.java
+++ src/test/java/selenium/tasks/Task1.java
@@ -1,8 +1,10 @@
 package selenium.tasks;
-
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -29,18 +31,27 @@ public class Task1 {
     public void errorOnText() {
 //        TODO
 //         enter a text instead of a number, check that correct error is shown
+        driver.findElement(By.id("numb")).sendKeys("sad");
+        driver.findElement(By.className("w3-orange")).click();
+        assertEquals("Please enter a number", driver.findElement(By.id("ch1_error")).getText());
     }
 
     @Test
     public void errorOnNumberTooSmall() {
 //        TODO
 //         enter number which is too small (positive number below 50), check that correct error is shown
+        driver.findElement(By.id("numb")).sendKeys("49");
+        driver.findElement(By.className("w3-orange")).click();
+        assertEquals("Number is too small", driver.findElement(By.id("ch1_error")).getText());
     }
 
     @Test
     public void errorOnNumberTooBig() {
 //        TODO
 //         enter number which is too big (above 100), check that correct error is shown
+        driver.findElement(By.id("numb")).sendKeys("101");
+        driver.findElement(By.className("w3-orange")).click();
+        assertEquals("Number is too big", driver.findElement(By.id("ch1_error")).getText());
     }
 
     @Test
@@ -49,5 +60,10 @@ public class Task1 {
 //         enter a number between 50 and 100 digit in the input, then press submit
 //         and check that no error is shown and that square root is calculated correctly
 //         NOTE: input and assertion values have to be defined as variables
+        driver.findElement(By.id("numb")).sendKeys("51");
+        driver.findElement(By.className("w3-orange")).click();
+        Alert alert = driver.switchTo().alert();
+        assertEquals("Square root of 51 is 7.14", alert.getText());
+        alert.accept();
     }
 }
