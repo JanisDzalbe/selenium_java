e7ccf752903d2e7e544fb1b2afa884148ed2c997
Andrejs Ivanovs <ivanovs.andrejs@inbox.lv>
2026-04-16 01:21:43 +0300

    task2

--- src/test/java/selenium/tasks/Task2.java
+++ src/test/java/selenium/tasks/Task2.java
@@ -3,10 +3,17 @@ package selenium.tasks;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Task2 {
     WebDriver driver;
@@ -39,6 +46,36 @@ public class Task2 {
 //        David, Project Manager
 //        Maria, QA Engineer
 //        Alex, DevOps Engineer
+        WebElement addPersonButton = driver.findElement(By.xpath("//*[@id='addPersonBtn' and contains(text(),'Add person')]"));
+        WebElement resetListButton = driver.findElement(By.xpath("//*[@id='addPersonBtn' and contains(text(),'Reset List')]"));
+        assertTrue(addPersonButton.isDisplayed());
+        assertTrue(addPersonButton.isEnabled());
+        assertTrue(resetListButton.isDisplayed());
+        assertTrue(resetListButton.isEnabled());
+
+        List<String> expectedPeople = Arrays.asList(
+                "Mike, Web Designer",
+                "Jill, Support",
+                "Jane, Accountant",
+                "John, Software Engineer",
+                "Sarah, Product Manager",
+                "Carlos, Data Analyst",
+                "Emily, UX Designer",
+                "David, Project Manager",
+                "Maria, QA Engineer",
+                "Alex, DevOps Engineer"
+        );
+        List<WebElement> people = driver.findElements(By.cssSelector("li[id^='person']"));
+        assertEquals(10, people.size());
+        List<String> actualPeople = new ArrayList<>();
+
+        for (WebElement person : people) {
+            String name = person.findElement(By.className("name")).getText().trim();
+            String job = person.findElement(By.className("job")).getText().trim();
+
+            actualPeople.add(name + ", " + job);
+        }
+        assertEquals(expectedPeople, actualPeople);
     }
 
     @Test
@@ -48,6 +85,29 @@ public class Task2 {
 //          fill "Name" and "Job" fields
 //          click "Add"
 //          check that new person is added to the list with correct name and job
+        WebElement addPersonButton = driver.findElement(By.xpath("//*[@id='addPersonBtn' and contains(text(),'Add person')]"));
+        addPersonButton.click();
+
+        String name = "Joseph";
+        WebElement nameInput = driver.findElement(By.cssSelector(".w3-input[id='name']"));
+        nameInput.sendKeys(name);
+
+        String job = "Actor";
+        WebElement jobInput = driver.findElement(By.cssSelector(".w3-input[id='job']"));
+        jobInput.sendKeys(job);
+
+        WebElement addButton = driver.findElement(By.id("modal_button"));
+        addButton.click();
+
+        List<WebElement> employees = driver.findElements(By.xpath("//li[contains(@id,'person')]"));
+        for(WebElement person : employees) {
+            String personName = person.findElement(By.xpath("./span[contains(@class,'name')]")).getText();
+            if (personName.equals(name)) {
+                String personJob = person.findElement(By.xpath(".//span[contains(@class, 'job')]")).getText();
+                assertEquals(job, personJob);
+                break;
+            }
+        }
     }
 
     @Test
@@ -58,6 +118,16 @@ public class Task2 {
 //          change "Job" field
 //          click "Edit"
 //          check that the person is updated in the list with new job
+        driver.findElement(By.xpath("//span[contains(@onclick,'openModalForEditPersonWithJob(0)')]")).click();
+        WebElement name = driver.findElement(By.id("name"));
+        WebElement job = driver.findElement(By.id("job"));
+        assertEquals("Mike", name.getDomProperty("value"));
+        assertEquals("Web Designer", job.getDomProperty("value"));
+        driver.findElement(By.id("job")).clear();
+        driver.findElement(By.id("job")).sendKeys("Pilot");
+        driver.findElement(By.xpath("//button[text()='Edit']")).click();
+        WebElement updatedjob = driver.findElement(By.id("person0"));
+        assertEquals("Pilot", updatedjob.findElement(By.className("job")).getText());
     }
 
     @Test
@@ -65,6 +135,10 @@ public class Task2 {
 //         TODO:
 //          click cross (x) icon for an existing person
 //          check that the person is removed from the list
+        driver.findElements(By.xpath("//span[text()='Maria']"));
+        driver.findElement(By.xpath("//span[contains(@onclick,'deletePerson(0)')]")).click();
+        assertFalse(driver.findElements(By.xpath("//span[text()='Maria']")).isEmpty());
+
     }
 
     @Test
@@ -73,6 +147,33 @@ public class Task2 {
 //          modify the list in any way (add, edit or remove a person)
 //          check that the list is modified
 //          click "Reset List"
-//          check that the list is back to initial state with 10 original entries
+//          check that the list is back to initial state with 10 original entries\
+        List<WebElement> people = driver.findElements(By.cssSelector("li[id^='person']"));
+        List<String> actualPeople = new ArrayList<>();
+
+        for (WebElement person : people) {
+            String name = person.findElement(By.className("name")).getText().trim();
+            String job = person.findElement(By.className("job")).getText().trim();
+
+            actualPeople.add(name + ", " + job);
+        }
+        int list =  driver.findElements(By.xpath("//li[starts-with(@id,'person')]")).size();
+        driver.findElement(By.xpath("//span[contains(@onclick,'deletePerson(0)')]")).click();
+        int listWithdDelete =  driver.findElements(By.xpath("//li[starts-with(@id,'person')]")).size();
+        assertNotEquals(list, listWithdDelete);
+        driver.findElement(By.xpath("//button[text()='Reset List']")).click();
+        int listAfterReset = driver.findElements(By.xpath("//li[starts-with(@id,'person')]")).size();
+        assertEquals(list, listAfterReset);
+        List<WebElement> persons = driver.findElements(By.cssSelector("li[id^='person']"));
+        assertEquals(10, persons.size());
+        List<String> newActualPeople = new ArrayList<>();
+
+        for (WebElement person : persons) {
+            String name = person.findElement(By.className("name")).getText().trim();
+            String job = person.findElement(By.className("job")).getText().trim();
+
+            newActualPeople.add(name + ", " + job);
+        }
+        assertEquals(actualPeople,newActualPeople);
     }
 }
