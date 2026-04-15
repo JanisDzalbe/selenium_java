eadd6744eb313788c522479efa788a6145aaabf3
Meganath Nandhakumar <meganathrtu@gmail.com>
2026-04-15 10:02:36 +0300

    Sample6Task

--- src/test/java/selenium/sample/Sample6Task.java
+++ src/test/java/selenium/sample/Sample6Task.java
@@ -3,23 +3,19 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import selenium.utility.BootcampUtils;
 
 public class Sample6Task {
     WebDriver driver;
 
-    // method which is being run before each test
     @BeforeEach
     public void startingTests() throws Exception {
-        // Initialize driver
         driver = BootcampUtils.initializeChromeDriver();
-
-        //open page:
         driver.get("https://janisdzalbe.github.io/example-site/examples/locators");
     }
 
-    // method which is being run after each test
     @AfterEach
     public void endingTests() throws Exception {
         driver.quit();
@@ -27,23 +23,56 @@ public class Sample6Task {
 
     @Test
     public void findElementByXPath() throws Exception {
-//         TODO:
-//          1-2 ways to find text: "Heading 2 text":
-//          1-2 ways to find text: "Test Text 1"
-//          1-2 ways to find text: "Test Text 2"
-//          1-2 ways to find text: "Test Text 3"
-//          1-2 ways to find text: "Test Text 4"
-//          1-2 ways to find text: "Test Text 5"
-//          1-2 ways to find text: "This is also a button"
+
+        // Heading 2 text
+        System.out.println(driver.findElement(By.xpath("//h2")).getText());
+        System.out.println(driver.findElement(By.xpath("//*[text()='Heading 2 text']")).getText());
+
+        // Test Text 1
+        System.out.println(driver.findElement(By.xpath("//*[text()='Test Text 1']")).getText());
+        System.out.println(driver.findElement(By.xpath("(//p)[1]")).getText());
+
+        // Test Text 2
+        System.out.println(driver.findElement(By.xpath("//*[text()='Test Text 2']")).getText());
+        System.out.println(driver.findElement(By.xpath("(//p)[2]")).getText());
+
+        // Test Text 3
+        System.out.println(driver.findElement(By.xpath("//*[text()='Test Text 3']")).getText());
+        System.out.println(driver.findElement(By.xpath("(//p)[3]")).getText());
+
+        // Test Text 4
+        System.out.println(driver.findElement(By.xpath("//*[text()='Test Text 4']")).getText());
+        System.out.println(driver.findElement(By.xpath("(//p)[4]")).getText());
+
+        // Test Text 5
+        System.out.println(driver.findElement(By.xpath("//*[text()='Test Text 5']")).getText());
+        System.out.println(driver.findElement(By.xpath("(//p)[5]")).getText());
+
+        // This is also a button
+        System.out.println(driver.findElement(By.xpath("//*[@value='This is also a button']")).getDomProperty("value"));
+        System.out.println(driver.findElement(By.xpath("//input[contains(@value,'also')]")).getDomProperty("value"));
     }
 
     @Test
     public void findElementByCssName() throws Exception {
-//         TODO:
-//          1-2 ways to find text: "Heading 2 text"
-//          1-2 ways to find text: "Test Text 1"
-//          1-2 ways to find text: "Test Text 2"
-//          1-2 ways to find text: "Test Text 3"
-//          1-2 ways to find text: "This is also a button"
+
+        // Heading 2 text
+        System.out.println(driver.findElement(By.cssSelector("h2")).getText());
+        System.out.println(driver.findElement(By.cssSelector("#heading_2")).getText());
+
+        // Test Text 1
+        System.out.println(driver.findElement(By.cssSelector("p:nth-of-type(1)")).getText());
+        System.out.println(driver.findElement(By.cssSelector(".text")).getText());
+
+        // Test Text 2
+        System.out.println(driver.findElement(By.cssSelector("p:nth-of-type(2)")).getText());
+        System.out.println(driver.findElement(By.cssSelector(".text.unbelievable")).getText());
+
+        // Test Text 3
+        System.out.println(driver.findElement(By.cssSelector("p:nth-of-type(3)")).getText());
+
+        // This is also a button
+        System.out.println(driver.findElement(By.cssSelector("[value='This is also a button']")).getDomProperty("value"));
+        System.out.println(driver.findElement(By.cssSelector("input[value='This is also a button']")).getDomProperty("value"));
     }
 }
