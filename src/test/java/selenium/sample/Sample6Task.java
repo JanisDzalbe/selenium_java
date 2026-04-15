7892c8a9a48dc643d5aa9dad6e7186d1f598df5c
broather <markussgr@gmail.com>
2026-04-15 18:34:44 +0300

    third commit
    tasks 6 and 7

--- src/test/java/selenium/sample/Sample6Task.java
+++ src/test/java/selenium/sample/Sample6Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample6Task {
     WebDriver driver;
@@ -25,25 +29,71 @@ public class Sample6Task {
         driver.quit();
     }
 
+    /**
+     * @description textMatchMode uses xpath's literal text() match, setting it to false uses more creative expressions
+     * @throws Exception
+     */
     @Test
     public void findElementByXPath() throws Exception {
-//         TODO:
-//          1-2 ways to find text: "Heading 2 text":
-//          1-2 ways to find text: "Test Text 1"
-//          1-2 ways to find text: "Test Text 2"
-//          1-2 ways to find text: "Test Text 3"
-//          1-2 ways to find text: "Test Text 4"
-//          1-2 ways to find text: "Test Text 5"
-//          1-2 ways to find text: "This is also a button"
+        boolean textMatchMode = true;
+        WebElement foundElement;
+        // 1-2 ways to find text: "Heading 2 text":
+        foundElement = textMatchMode ? driver.findElement(By.xpath("//*[text()='Heading 2 text']")) :
+                driver.findElement(By.xpath("//*[@id='heading_2']"));
+        assertEquals("Heading 2 text", foundElement.getText());
+        // 1-2 ways to find text: "Test Text 1"
+        foundElement = textMatchMode ? driver.findElement(By.xpath("//*[text()='Test Text 1']")) :
+                driver.findElement(By.xpath("//*[@id='test1']/p"));
+        assertEquals("Test Text 1", foundElement.getText());
+        // 1-2 ways to find text: "Test Text 2"
+        foundElement = textMatchMode ? driver.findElement(By.xpath("//*[text()='Test Text 2']")) :
+                driver.findElement(By.xpath("//*[@class='twoTest']"));
+        assertEquals("Test Text 2", foundElement.getText());
+        // 1-2 ways to find text: "Test Text 3"
+        foundElement = textMatchMode ? driver.findElement(By.xpath("//*[text()='Test Text 3']")) :
+                driver.findElement(By.xpath("//*[@id='test3']/p"));
+        assertEquals("Test Text 3", foundElement.getText());
+        // 1-2 ways to find text: "Test Text 4"
+        foundElement = textMatchMode ? driver.findElement(By.xpath("//*[text()='Test Text 4']")) :
+                driver.findElements(By.xpath("//*[@id='test3']/p")).get(1);
+        assertEquals("Test Text 4", foundElement.getText());
+        // 1-2 ways to find text: "Test Text 5"
+        foundElement = textMatchMode ? driver.findElement(By.xpath("//*[text()='Test Text 5']")) :
+                driver.findElement(By.xpath("//*[@id='test2']/p"));
+        assertEquals("Test Text 5", foundElement.getText());
+        // 1-2 ways to find text: "This is also a button"
+        foundElement = textMatchMode ? driver.findElement(By.xpath("//*[@value='This is also a button']")) :
+                driver.findElements(By.xpath("//input")).get(1);
+        assertEquals("This is also a button", foundElement.getDomAttribute("value"));
+
     }
 
+    /**
+     * @description flip `alternateMode` between true and false to use different css selectors for some elements
+     * @throws Exception
+     */
     @Test
     public void findElementByCssName() throws Exception {
-//         TODO:
-//          1-2 ways to find text: "Heading 2 text"
-//          1-2 ways to find text: "Test Text 1"
-//          1-2 ways to find text: "Test Text 2"
-//          1-2 ways to find text: "Test Text 3"
-//          1-2 ways to find text: "This is also a button"
+        boolean alternateMode = true;
+        WebElement foundElement;
+//      1-2 ways to find text: "Heading 2 text"
+        foundElement = driver.findElement(By.cssSelector("#heading_2"));
+        assertEquals("Heading 2 text", foundElement.getText());
+//      1-2 ways to find text: "Test Text 1"
+        foundElement = alternateMode ? driver.findElement(By.cssSelector("div#test1 > p")) :
+                driver.findElement(By.cssSelector(".test"));
+        assertEquals("Test Text 1", foundElement.getText());
+//      1-2 ways to find text: "Test Text 2"
+        foundElement = alternateMode ? driver.findElements(By.cssSelector("div#test1 > p")).get(1) :
+                driver.findElement(By.cssSelector(".twoTest"));
+        assertEquals("Test Text 2", foundElement.getText());
+//      1-2 ways to find text: "Test Text 3"
+        foundElement = alternateMode ? driver.findElement(By.cssSelector("div#test3 > p")) :
+                driver.findElements(By.cssSelector(".test")).get(1);
+        assertEquals("Test Text 3", foundElement.getText());
+//      1-2 ways to find text: "This is also a button"
+        foundElement = alternateMode ? driver.findElements(By.cssSelector("input")).get(1) :
+                driver.findElement(By.cssSelector("#buttonId"));
+        assertEquals("This is also a button", foundElement.getDomAttribute("value"));
     }
 }
--- src/test/java/selenium/sample/Sample7Task.java
+++ src/test/java/selenium/sample/Sample7Task.java
@@ -3,8 +3,18 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.w3c.dom.html.HTMLInputElement;
 import selenium.utility.BootcampUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample7Task {
     WebDriver driver;
@@ -28,37 +38,84 @@ public class Sample7Task {
 
     @Test
     public void selectCheckBox() throws Exception {
-//         TODO:
+        WebElement option1 = driver.findElement((By.cssSelector(".w3-check[value='Option 1'][type='checkbox']")));
+        WebElement option2 = driver.findElement((By.cssSelector(".w3-check[value='Option 2'][type='checkbox']")));
+        WebElement option3 = driver.findElement((By.cssSelector(".w3-check[value='Option 3'][type='checkbox']")));
+        List<WebElement> checkBoxes = Arrays.asList(option1, option2, option3);
+        WebElement resultButton = driver.findElement((By.id("result_button_checkbox")));
+        WebElement resultText = driver.findElement((By.id("result_checkbox")));
 //          check that none of the checkboxes are ticked
+        for(WebElement cb : checkBoxes){
+            assertFalse(cb.isSelected());
+        }
 //          tick  "Option 2"
+        option2.click();
 //          check that "Option 1" and "Option 3" are not ticked, but "Option 2" is ticked
+        assertFalse(option1.isSelected() && option3.isSelected());
+        assertTrue(option2.isSelected());
 //          tick  "Option 3"
+        option3.click();
 //          click result
+        resultButton.click();
 //          check that text 'You selected value(s): Option 2, Option 3' is being displayed
-    }
 
+//      filter the list of checkBoxes so that only the selected checkBoxes remain
+        Stream<WebElement> selectedCheckBoxes = checkBoxes.stream().filter(c -> c.isSelected());
+//      map each checkbox to it's `value` attribute
+        List<String> checkBoxStrings = selectedCheckBoxes.map(c -> c.getDomProperty("value")).toList();
+//      create the expected string like on the website
+        assertEquals("You selected value(s): "+ String.join(", ", checkBoxStrings), resultText.getText());
+    }
 
     @Test
     public void selectRadioButton() throws Exception {
-//         TODO:
+        WebElement option1 = driver.findElement((By.cssSelector(".w3-check[value='Option 1'][type='radio']")));
+        WebElement option2 = driver.findElement((By.cssSelector(".w3-check[value='Option 2'][type='radio']")));
+        WebElement option3 = driver.findElement((By.cssSelector(".w3-check[value='Option 3'][type='radio']")));
+        List<WebElement> radioButtons = Arrays.asList(option1, option2, option3);
+        WebElement resultButton = driver.findElement((By.id("result_button_radio")));
+        WebElement resultText = driver.findElement((By.id("result_radio")));
 //          check that none of the radio are selected
+        for(WebElement rb : radioButtons){
+            assertFalse(rb.isSelected());
+        }
 //          select  "Option 3"
+        option3.click();
 //          check that "Option 1" and "Option 2' are not select, but "Option 3" is selected
+        assertFalse(option1.isSelected() && option2.isSelected());
+        assertTrue(option3.isSelected());
 //          select  "Option 1"
+        option1.click();
 //          check that "Option 2" and "Option 3' are not select, but "Option 1" is selected
+        assertFalse(option2.isSelected() && option3.isSelected());
+        assertTrue(option1.isSelected());
 //          click result
+        resultButton.click();
 //          check that 'You selected option: Option 1' text is being displayed
+//      filter radioButtons so that the selected one remains and save it to a variable
+        WebElement selectedOption = radioButtons.stream().filter(c -> c.isSelected()).toList().getFirst();
+//      create the expected string like on the website
+        assertEquals("You selected value(s): "+ selectedOption.getDomProperty("value"), resultText.getText());
+
     }
 
     @Test
     public void selectOption() throws Exception {
-//         TODO:
+        Select dropdown = new Select(driver.findElement(By.id("vfb-12")));
+        WebElement resultButton = driver.findElement(By.id("result_button_select"));
+        WebElement resultText = driver.findElement(By.id("result_select"));
 //          select "Option 3" in Select
+        dropdown.selectByVisibleText("Option 3");
 //          check that selected option is "Option 3"
+        assertEquals("Option 3", dropdown.getFirstSelectedOption().getText());
 //          select "Option 2" in Select
+        dropdown.selectByVisibleText("Option 2");
 //          check that selected option is "Option 2"
+        assertEquals("Option 2", dropdown.getFirstSelectedOption().getText());
 //          click result
+        resultButton.click();
 //          check that 'You selected option: Option 2' text is being displayed
+        assertEquals("You selected option: Option 2", resultText.getText());
     }
 
 // ** Bonus tasks **
