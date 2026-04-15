d39a8d3d25504db19a9fa14281d1b53782e8d08f
Ilmārs Cīrulis <6750944+IlmarsCirulis@users.noreply.github.com>
2026-04-15 10:26:04 +0300

    Sample6Task done

--- src/test/java/selenium/sample/Sample6Task.java
+++ src/test/java/selenium/sample/Sample6Task.java
@@ -3,8 +3,11 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample6Task {
     WebDriver driver;
@@ -18,7 +21,6 @@ public class Sample6Task {
         //open page:
         driver.get("https://janisdzalbe.github.io/example-site/examples/locators");
     }
-
     // method which is being run after each test
     @AfterEach
     public void endingTests() throws Exception {
@@ -27,6 +29,25 @@ public class Sample6Task {
 
     @Test
     public void findElementByXPath() throws Exception {
+        System.out.println("Using xPath:");
+
+        System.out.println(driver.findElement(By.xpath("//*[@id='heading_2']")).getText());
+        System.out.println(driver.findElements(By.xpath("//h2")).get(1).getText());
+
+        System.out.println(driver.findElement(By.xpath("//*[@id='test1']/*[@class='test']")).getText());
+        System.out.println(driver.findElements(By.xpath("//div[@id='test1']/p")).getFirst().getText());
+
+        System.out.println(driver.findElement(By.xpath("//*[@id='test1']/p[@class='twoTest']")).getText());
+        System.out.println(driver.findElements(By.xpath("//div[@id='test1']/p")).get(1).getText());
+
+        System.out.println(driver.findElements(By.xpath("//div[@id='test3']/p")).getFirst().getText());
+
+        System.out.println(driver.findElements(By.xpath("//div[@id='test3']/p")).get(1).getText());
+
+        System.out.println(driver.findElement(By.xpath("//div[@id='test2']/p[@class='Test']")).getText());
+
+        System.out.println(driver.findElement(By.xpath("//input[@id='buttonId']")).getDomProperty("value"));
+
 //         TODO:
 //          1-2 ways to find text: "Heading 2 text":
 //          1-2 ways to find text: "Test Text 1"
@@ -39,6 +60,26 @@ public class Sample6Task {
 
     @Test
     public void findElementByCssName() throws Exception {
+
+        System.out.println("Using css selectors:");
+
+        System.out.println(driver.findElement(By.cssSelector("#heading_2")).getText());
+        System.out.println(driver.findElements(By.cssSelector("h2")).get(1).getText());
+
+        System.out.println(driver.findElement(By.cssSelector("#test1 > .test")).getText());
+        System.out.println(driver.findElements(By.cssSelector("#test1 > p")).getFirst().getText());
+
+        System.out.println(driver.findElement(By.cssSelector("#test1 > .twoTest")).getText());
+        System.out.println(driver.findElements(By.cssSelector("#test1 > p")).get(1).getText());
+
+        System.out.println(driver.findElements(By.cssSelector("#test3 > p")).getFirst().getText());
+
+        System.out.println(driver.findElements(By.cssSelector("#test3 > p")).get(1).getText());
+
+        System.out.println(driver.findElement(By.cssSelector("#test2 > p.Test")).getText());
+
+        System.out.println(driver.findElement(By.cssSelector("#buttonId")).getDomProperty("value"));
+
 //         TODO:
 //          1-2 ways to find text: "Heading 2 text"
 //          1-2 ways to find text: "Test Text 1"
