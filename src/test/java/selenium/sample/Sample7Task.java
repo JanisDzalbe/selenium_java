166d819224692de07feca7c86f8dd1541f259650
Kira Solovjova <solovjova467@gmail.com>
2026-04-15 11:12:44 +0300

    Sample 7 Task completed.

--- src/test/java/selenium/sample/Sample7Task.java
+++ src/test/java/selenium/sample/Sample7Task.java
@@ -3,24 +3,26 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample7Task {
     WebDriver driver;
     String base_url = "https://janisdzalbe.github.io/example-site/examples/actions";
 
-    // method which is being run before each test
     @BeforeEach
     public void startingTests() throws Exception {
-        // Initialize driver
         driver = BootcampUtils.initializeChromeDriver();
-
-        //open page:
         driver.get(base_url);
     }
 
-    // method which is being run after each test
     @AfterEach
     public void endingTests() throws Exception {
         driver.quit();
@@ -28,51 +30,83 @@ public class Sample7Task {
 
     @Test
     public void selectCheckBox() throws Exception {
-//         TODO:
-//          check that none of the checkboxes are ticked
-//          tick  "Option 2"
-//          check that "Option 1" and "Option 3" are not ticked, but "Option 2" is ticked
-//          tick  "Option 3"
-//          click result
-//          check that text 'You selected value(s): Option 2, Option 3' is being displayed
-    }
+        List<WebElement> checkBoxes = driver.findElements(By.cssSelector(".w3-check[type='checkbox']"));
 
+        for (WebElement cb : checkBoxes) {
+            assertFalse(cb.isSelected(), "Checkbox should not be selected initially");
+        }
+
+        WebElement opt2 = driver.findElement(By.cssSelector("input[value='Option 2']"));
+        opt2.click();
+
+        assertTrue(opt2.isSelected());
+        assertFalse(driver.findElement(By.cssSelector("input[value='Option 1']")).isSelected());
+        assertFalse(driver.findElement(By.cssSelector("input[value='Option 3']")).isSelected());
+
+        driver.findElement(By.cssSelector("input[value='Option 3']")).click();
+        driver.findElement(By.id("result_button_checkbox")).click();
+        String resultText = driver.findElement(By.id("result_checkbox")).getText();
+        assertEquals("You selected value(s): Option 2, Option 3", resultText);
+    }
 
     @Test
     public void selectRadioButton() throws Exception {
-//         TODO:
-//          check that none of the radio are selected
-//          select  "Option 3"
-//          check that "Option 1" and "Option 2' are not select, but "Option 3" is selected
-//          select  "Option 1"
-//          check that "Option 2" and "Option 3' are not select, but "Option 1" is selected
-//          click result
-//          check that 'You selected option: Option 1' text is being displayed
+        List<WebElement> radios = driver.findElements(By.cssSelector(".w3-check[type='radio']"));
+        for (WebElement rb : radios) {
+            assertFalse(rb.isSelected());
+        }
+
+        WebElement opt3 = driver.findElement(By.id("vfb-7-3"));
+        opt3.click();
+        assertTrue(opt3.isSelected());
+        assertFalse(driver.findElement(By.id("vfb-7-1")).isSelected());
+        assertFalse(driver.findElement(By.id("vfb-7-2")).isSelected());
+
+        WebElement opt1 = driver.findElement(By.id("vfb-7-1"));
+        opt1.click();
+        assertTrue(opt1.isSelected());
+        assertFalse(opt3.isSelected());
+        driver.findElement(By.id("result_button_ratio")).click();
+        assertEquals("You selected option: Option 1", driver.findElement(By.id("result_radio")).getText());
     }
 
     @Test
     public void selectOption() throws Exception {
-//         TODO:
-//          select "Option 3" in Select
-//          check that selected option is "Option 3"
-//          select "Option 2" in Select
-//          check that selected option is "Option 2"
-//          click result
-//          check that 'You selected option: Option 2' text is being displayed
+        Select dropdown = new Select(driver.findElement(By.id("vfb-12")));
+        dropdown.selectByVisibleText("Option 3");
+        assertEquals("Option 3", dropdown.getFirstSelectedOption().getText());
+        dropdown.selectByValue("value2");
+        assertEquals("Option 2", dropdown.getFirstSelectedOption().getText());
+        driver.findElement(By.id("result_button_select")).click();
+        assertEquals("You selected option: Option 2", driver.findElement(By.id("result_select")).getText());
     }
 
-// ** Bonus tasks **
     @Test
     public void chooseDateViaCalendarBonus() throws Exception {
-//         TODO:
-//          enter date '4 of July 2007' using calendar widget
-//          check that correct date is added
+        WebElement dateBox = driver.findElement(By.id("vfb-8"));
+        dateBox.click();
+
+        WebElement dateWidget = driver.findElement(By.id("ui-datepicker-div"));
+
+        while (!driver.findElement(By.className("ui-datepicker-year")).getText().equals("2007")) {
+            driver.findElement(By.className("ui-datepicker-prev")).click();
+        }
+        while (!driver.findElement(By.className("ui-datepicker-month")).getText().equals("July")) {
+            driver.findElement(By.className("ui-datepicker-prev")).click();
+        }
+
+        driver.findElement(By.xpath("//a[text()='4']")).click();
+        assertEquals("07/04/2007", dateBox.getDomProperty("value"));
     }
 
     @Test
     public void chooseDateViaTextBoxBonus() throws Exception {
-//         TODO:
-//          enter date '2 of May 1959' using text
-//          check that correct date is added
+        String targetDate = "05/02/1959";
+        WebElement dateBox = driver.findElement(By.id("vfb-8"));
+
+        dateBox.clear();
+        dateBox.sendKeys(targetDate);
+
+        assertEquals(targetDate, dateBox.getDomProperty("value"));
     }
 }
