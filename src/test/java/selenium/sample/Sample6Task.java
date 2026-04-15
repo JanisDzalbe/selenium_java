018b8d75f319ab26ca99cc8c83fd3c0ea6dddcd5
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2026-04-15 10:28:53 +0300

    feat: implement XPath and CSS selectors for text retrieval in Sample6Task

--- src/test/java/selenium/sample/Sample6Task.java
+++ src/test/java/selenium/sample/Sample6Task.java
@@ -3,6 +3,7 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import selenium.utility.BootcampUtils;
 
@@ -27,23 +28,51 @@ public class Sample6Task {
 
     @Test
     public void findElementByXPath() throws Exception {
-//         TODO:
-//          1-2 ways to find text: "Heading 2 text":
-//          1-2 ways to find text: "Test Text 1"
-//          1-2 ways to find text: "Test Text 2"
-//          1-2 ways to find text: "Test Text 3"
-//          1-2 ways to find text: "Test Text 4"
-//          1-2 ways to find text: "Test Text 5"
-//          1-2 ways to find text: "This is also a button"
+        System.out.println("\"Heading 2 text\"");
+        System.out.println(driver.findElement(By.xpath("//*[@id='heading_2']")).getText());
+        System.out.println(driver.findElement(By.xpath("//h2[@id='heading_2']")).getText());
+
+        System.out.println("\"Test Text 1\"");
+        System.out.println(driver.findElement(By.xpath("//div[@id='test1']/p[@class='test']")).getText());
+        System.out.println(driver.findElement(By.xpath("//div/p[@class='test'][1]")).getText());
+
+        System.out.println("\"Test Text 2\"");
+        System.out.println(driver.findElement(By.xpath("//div[@id='test1']/p[@class='twoTest']")).getText());
+        System.out.println(driver.findElement(By.xpath("//*[@class='twoTest']")).getText());
+
+        System.out.println("\"Test Text 3\"");
+        System.out.println(driver.findElement(By.xpath("//*[contains(text(), 'Test Text 3')]")).getText());
+        System.out.println(driver.findElement(By.xpath("//*[@id='test3']/*[contains(text(), '3')]")).getText());
+
+        System.out.println("\"Test Text 4\"");
+        System.out.println(driver.findElement(By.xpath("//*[contains(text(), 'Test Text 4')]")).getText());
+
+        System.out.println("\"Test Text 5\"");
+        System.out.println(driver.findElement(By.xpath("//*[contains(text(), 'Test Text 5')]")).getText());
+
+        System.out.println("\"This is also a button\"");
+        System.out.println(driver.findElement(By.xpath("//input[@id='buttonId']")).getDomAttribute("value"));
+        System.out.println(driver.findElement(By.xpath("//*[@name='randomButton2']")).getDomAttribute("value"));
     }
 
     @Test
     public void findElementByCssName() throws Exception {
-//         TODO:
-//          1-2 ways to find text: "Heading 2 text"
-//          1-2 ways to find text: "Test Text 1"
-//          1-2 ways to find text: "Test Text 2"
-//          1-2 ways to find text: "Test Text 3"
-//          1-2 ways to find text: "This is also a button"
+        System.out.println("\"Heading 2 text\"");
+        System.out.println(driver.findElement(By.cssSelector("#heading_2")).getText());
+
+        System.out.println("\"Test Text 1\"");
+        System.out.println(driver.findElement(By.cssSelector("#test1 .test")).getText());
+        System.out.println(driver.findElement(By.cssSelector("#test1 > p.test")).getText());
+
+        System.out.println("\"Test Text 2\"");
+        System.out.println(driver.findElement(By.cssSelector(".twoTest")).getText());
+        System.out.println(driver.findElement(By.cssSelector("#test1 p.twoTest")).getText());
+
+        System.out.println("\"Test Text 3\"");
+        System.out.println(driver.findElements(By.cssSelector("#test3 p.test")).getFirst().getText());
+
+        System.out.println("\"This is also a button\"");
+        System.out.println(driver.findElement(By.cssSelector("#buttonId")).getDomAttribute("value"));
+        System.out.println(driver.findElement(By.cssSelector("input[name='randomButton2']")).getDomAttribute("value"));
     }
 }
