73da6750ea76667955ae50ec2612d9693a874b5b
Sonakshi <sonakshigupta.135@gmail.com>
2026-04-15 11:36:47 +0300

    Commit sample task

--- src/test/java/selenium/sample/Sample7Task.java
+++ src/test/java/selenium/sample/Sample7Task.java
@@ -3,8 +3,15 @@ package selenium.sample;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import selenium.utility.BootcampUtils;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Sample7Task {
     WebDriver driver;
@@ -35,6 +42,36 @@ public class Sample7Task {
 //          tick  "Option 3"
 //          click result
 //          check that text 'You selected value(s): Option 2, Option 3' is being displayed
+
+        List<WebElement> checkBoxes = driver.findElements(By.cssSelector(".w3-check[type='checkbox']"));
+
+        for (WebElement checkBox : checkBoxes) {
+            assertFalse(checkBox.isSelected()); // checkboxes are NOT selected
+            checkBox.click();
+            assertTrue(checkBox.isSelected()); // checkboxes are selected
+            checkBox.click();
+            assertFalse(checkBox.isSelected()); // checkboxes are NOT selected
+        }
+        WebElement option2 = driver.findElement(By.cssSelector(".w3-check[value='Option 2'][type='checkbox']"));
+        assertFalse(option2.isSelected());
+        option2.click();
+        assertTrue(option2.isSelected());
+
+
+        WebElement option1 = driver.findElement(By.cssSelector("input[value='Option 1']"));
+        WebElement Option2 = driver.findElement(By.cssSelector("input[value='Option 2']"));
+        WebElement option3 = driver.findElement(By.cssSelector("input[value='Option 3']"));
+        assertFalse(option1.isSelected());
+        assertTrue(Option2.isSelected());
+        assertFalse(option3.isSelected());
+        option3.click();
+        assertTrue(option3.isSelected());
+
+        WebElement resultBtn = driver.findElement(By.id("result_button_checkbox"));
+        resultBtn.click();
+
+        WebElement resultText = driver.findElement(By.id("result_checkbox"));
+        assertEquals("You selected value(s): Option 2, Option 3", resultText.getText());
     }
 
 
@@ -48,6 +85,31 @@ public class Sample7Task {
 //          check that "Option 2" and "Option 3' are not select, but "Option 1" is selected
 //          click result
 //          check that 'You selected option: Option 1' text is being displayed
+
+
+
+        List<WebElement> radioButtons = driver.findElements(By.cssSelector("input[type='radio']"));
+
+        for (WebElement radioButton : radioButtons) {
+            assertFalse(radioButton.isSelected());
+        }
+
+        radioButtons.get(2).click();
+
+        assertFalse(radioButtons.get(0).isSelected());
+        assertFalse(radioButtons.get(1).isSelected());
+        assertTrue(radioButtons.get(2).isSelected());
+
+        radioButtons.get(0).click();
+
+        assertTrue(radioButtons.get(0).isSelected());
+        assertFalse(radioButtons.get(1).isSelected());
+        assertFalse(radioButtons.get(2).isSelected());
+
+        driver.findElement(By.id("result_button_ratio")).click();
+
+        assertEquals("You selected option: Option 1",
+                driver.findElement(By.id("result_radio")).getText());
     }
 
     @Test
@@ -59,6 +121,19 @@ public class Sample7Task {
 //          check that selected option is "Option 2"
 //          click result
 //          check that 'You selected option: Option 2' text is being displayed
+
+        Select select = new Select(driver.findElement(By.id("vfb-12")));
+
+        select.selectByVisibleText("Option 3");
+        assertEquals("Option 3", select.getFirstSelectedOption().getText());
+
+        select.selectByVisibleText("Option 2");
+        assertEquals("Option 2", select.getFirstSelectedOption().getText());
+
+        driver.findElement(By.id("result_button_select")).click();
+
+        assertEquals("You selected option: Option 2",
+                driver.findElement(By.id("result_select")).getText());
     }
 
 // ** Bonus tasks **
