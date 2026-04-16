a3a1ac85d8af535aca075823dc06a59b6aa7da57
Kira Solovjova <solovjova467@gmail.com>
2026-04-16 09:29:51 +0300

    Extra Task 1 completed.

--- src/test/java/selenium/sample/extra/extra1Task.java
+++ src/test/java/selenium/sample/extra/extra1Task.java
@@ -3,25 +3,24 @@ package selenium.sample.extra;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class extra1Task {
     WebDriver driver;
     String base_url = "https://janisdzalbe.github.io/example-site/examples/act";
-    String new_url = "https://janisdzalbe.github.io/example-site/examples/link1";
+    String po_url = "https://janisdzalbe.github.io/example-site/examples/po";
+    String po1_url = "https://janisdzalbe.github.io/example-site/examples/po1";
 
-    // method which is being run before each test
     @BeforeEach
     public void startingTests() throws Exception {
-        // Initialize driver
         driver = BootcampUtils.initializeChromeDriver();
-
-        //open page:
-        driver.get(base_url);
     }
 
-    // method which is being run after each test
     @AfterEach
     public void endingTests() throws Exception {
         driver.quit();
@@ -29,31 +28,67 @@ public class extra1Task {
 
     @Test
     public void navigateBack() throws Exception {
-//        TODO
-//         open page with url "https://janisdzalbe.github.io/example-site/examples/po"
-//         click "More > " for the top left element
-//         check that the url now "https://janisdzalbe.github.io/example-site/examples/po1"
-//         using driver navigation go back to "https://janisdzalbe.github.io/example-site/examples/po"
-//         check that the page now is "https://janisdzalbe.github.io/example-site/examples/po"
+        // open page with url po
+        driver.get(po_url);
+
+        // click "More > " for the top left element (using linkText for clarity)
+        driver.findElement(By.linkText("More >>")).click();
+
+        // check that the url now po1
+        assertEquals(po1_url, driver.getCurrentUrl());
+
+        // using driver navigation go back
+        driver.navigate().back();
+
+        // check that the page now is po
+        assertEquals(po_url, driver.getCurrentUrl());
     }
 
     @Test
     public void navigateForward() throws Exception {
-//        TODO
-//         open page with url "https://janisdzalbe.github.io/example-site/examples/po"
-//         click "More > " for the top left element
-//         using driver navigation go back to "https://janisdzalbe.github.io/example-site/examples/po"
-//         using driver navigation go forward to "https://janisdzalbe.github.io/example-site/examples/po1"
-//         check that the page now is "https://janisdzalbe.github.io/example-site/examples/po1"
+        // open page with url po
+        driver.get(po_url);
+
+        // click "More > " for the top left element
+        driver.findElement(By.linkText("More >>")).click();
+
+        // using driver navigation go back
+        driver.navigate().back();
+        assertEquals(po_url, driver.getCurrentUrl());
+
+        // using driver navigation go forward to po1
+        driver.navigate().forward();
+
+        // check that the page now is po1
+        assertEquals(po1_url, driver.getCurrentUrl());
     }
 
     @Test
     public void refresh() throws Exception {
-//        TODO
-//         open page "https://janisdzalbe.github.io/example-site/examples/act"
-//         click on "Show" button in 'Button' section
-//         check that text "I am here!" is seen
-//         refresh page
-//         check that text "I am here!" is not seen
+        driver.get(base_url);
+
+        // 1. Find the button
+        WebElement showButton = driver.findElement(By.id("show_text"));
+
+        // 2. EXTRA STEP: If the button is disabled, we enable it so we can click
+        if (!showButton.isEnabled()) {
+            ((org.openqa.selenium.JavascriptExecutor) driver)
+                    .executeScript("arguments[0].removeAttribute('disabled')", showButton);
+        }
+
+        // 3. Click the button
+        showButton.click();
+
+        // 4. Check that text "I am here!" is seen (ID is show_me)
+        WebElement message = driver.findElement(By.id("show_me"));
+        assertTrue(message.isDisplayed(), "Message should be visible");
+        assertEquals("I am here!", message.getText());
+
+        // 5. Refresh page
+        driver.navigate().refresh();
+
+        // 6. Check that the text is gone (or the page reset)
+        // Re-find the element after refresh to avoid StaleElementReferenceException
+        assertFalse(driver.findElement(By.id("show_me")).isDisplayed(), "Message should be hidden after refresh");
     }
 }
