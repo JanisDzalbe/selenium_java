8dec78734247984ee1b3d600ab33fdd8f9a85b9b
Kira Solovjova <solovjova467@gmail.com>
2026-04-16 09:31:57 +0300

    Extra Task 1 modified.

--- src/test/java/selenium/sample/extra/extra1Task.java
+++ src/test/java/selenium/sample/extra/extra1Task.java
@@ -28,19 +28,13 @@ public class extra1Task {
 
     @Test
     public void navigateBack() throws Exception {
-        // open page with url po
         driver.get(po_url);
-
-        // click "More > " for the top left element (using linkText for clarity)
         driver.findElement(By.linkText("More >>")).click();
 
-        // check that the url now po1
         assertEquals(po1_url, driver.getCurrentUrl());
 
-        // using driver navigation go back
         driver.navigate().back();
 
-        // check that the page now is po
         assertEquals(po_url, driver.getCurrentUrl());
     }
 
@@ -49,17 +43,12 @@ public class extra1Task {
         // open page with url po
         driver.get(po_url);
 
-        // click "More > " for the top left element
         driver.findElement(By.linkText("More >>")).click();
-
-        // using driver navigation go back
         driver.navigate().back();
         assertEquals(po_url, driver.getCurrentUrl());
 
-        // using driver navigation go forward to po1
         driver.navigate().forward();
 
-        // check that the page now is po1
         assertEquals(po1_url, driver.getCurrentUrl());
     }
 
@@ -67,28 +56,18 @@ public class extra1Task {
     public void refresh() throws Exception {
         driver.get(base_url);
 
-        // 1. Find the button
         WebElement showButton = driver.findElement(By.id("show_text"));
 
-        // 2. EXTRA STEP: If the button is disabled, we enable it so we can click
         if (!showButton.isEnabled()) {
             ((org.openqa.selenium.JavascriptExecutor) driver)
                     .executeScript("arguments[0].removeAttribute('disabled')", showButton);
         }
-
-        // 3. Click the button
         showButton.click();
-
-        // 4. Check that text "I am here!" is seen (ID is show_me)
         WebElement message = driver.findElement(By.id("show_me"));
         assertTrue(message.isDisplayed(), "Message should be visible");
         assertEquals("I am here!", message.getText());
 
-        // 5. Refresh page
         driver.navigate().refresh();
-
-        // 6. Check that the text is gone (or the page reset)
-        // Re-find the element after refresh to avoid StaleElementReferenceException
         assertFalse(driver.findElement(By.id("show_me")).isDisplayed(), "Message should be hidden after refresh");
     }
 }
