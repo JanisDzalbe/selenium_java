cc4c3ba18ea7c73083eef770e900e54dd442439a
Kira Solovjova <solovjova467@gmail.com>
2026-04-16 09:42:24 +0300

    Extra Task 3 completed.

--- src/test/java/selenium/sample/extra/extra3Task.java
+++ src/test/java/selenium/sample/extra/extra3Task.java
@@ -3,23 +3,22 @@ package selenium.sample.extra;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import selenium.utility.BootcampUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class extra3Task {
     WebDriver driver;
 
-    // method which is being run before each test
     @BeforeEach
     public void startingTests() throws Exception {
-        // Initialize driver
         driver = BootcampUtils.initializeChromeDriver();
-
-        //open page:
         driver.get("https://janisdzalbe.github.io/example-site/examples/po");
     }
 
-    // method which is being run after each test
     @AfterEach
     public void endingTests() throws Exception {
         driver.quit();
@@ -27,9 +26,14 @@ public class extra3Task {
 
     @Test
     public void styleChecks() throws Exception {
-//       TODO:
-//        check the background of top 2 sections
-//        rgba(255, 221, 221, 1);
-//        check h1 element font-size 64px
+        WebElement redBox = driver.findElement(By.id("red_box"));
+        assertEquals("rgba(255, 221, 221, 1)", redBox.getCssValue("background-color"));
+
+        WebElement yellowBox = driver.findElement(By.id("yellow_box"));
+        assertEquals("rgba(255, 255, 204, 1)", yellowBox.getCssValue("background-color"));
+
+        // 3. Check h1 element font-size 64px
+        WebElement h1 = driver.findElement(By.tagName("h1"));
+        assertEquals("64px", h1.getCssValue("font-size"));
     }
 }
